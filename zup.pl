b0e7ca3cd52e3b3ade41db8e4f79bb4208c5ecc3
andrewschultz <schultz.andrew@sbcglobal.net>
2015-09-23 20:29:59 -0500

    Added ZUP to files

---
+++ zup.pl
@@ -0,0 +1,82 @@
+open(A, "zup.txt");
+
+while ($count <= $#ARGV)
+{
+  if ($count =~ /,/)
+  {
+    @commas = split(/,/, $count);
+	for (@commas) { $here{$_} = 1; }
+  }
+  else
+  {
+  $here{@ARGV[$count]} = 1;
+  }
+  $count++;
+}
+
+$count = 0;
+
+while ($a = <A>)
+{
+  chomp($a);
+
+  #print "$a: ";
+
+  if ($a =~ /^name=/i)
+  {
+    $a =~ s/^name=//gi;
+    @b = split(/,/, $a);
+	for $idx(@b)
+	{
+	  print "$idx\n";
+	  if ($here{$idx}==1)
+	  {
+	    $triedSomething = 1;
+	    $zipUp = 1;
+	    $zipcmd = "7z a";
+	  }
+	}
+  }
+  if ($a =~ /^;/) { last; }
+  if (!$zipUp) { next; }
+
+  for ($a)
+  {
+
+  /^!/ && do { processCmd($zipcmd); $zipUp = 0; next; };
+  /^out=/ && do { $a =~ s/^out=//g; $zipcmd .= " \"$a\""; next; };
+  /^>>/ && do { $cmd = $a; $cmd =~ s/^>>//g; `$cmd`; print "Running $cmd\n"; next; };
+  /^>/ && do { $fileName = $a; $fileName =~ s/^>//g; $zipcmd = "$zipcmd "; $needFile = 1; next; };
+  /^F=/i && do
+  {
+    $a =~ s/^F=//gi;
+    #$fileName =~ s/\./_release_$a\./g;
+    $needFile = 0;
+	if (! -f "$a") { print "No file $a.\n"; }
+	else
+	{
+    $zipcmd .= " \"$a\"";
+	}
+	#print "Add $a, **$zipcmd\n";
+    next;
+  };
+  /^c:/ && do
+  {
+    $cmd .= " \"$a\"";
+    if (! -f "$a") { print "WARNING: $a doesn't exist.\n"; }
+    next;
+  };
+  /^;/ && do { last; next; };
+  }
+  #print "Cur cmd $cmd\n";
+}
+
+close(A);
+
+if (!$triedSomething) { print "Didn't find anything in @ARGV.\n"; }
+
+sub processCmd
+{
+  print "$_[0]\n";
+  `$_[0]`;
+}
