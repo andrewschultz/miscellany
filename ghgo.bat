2e93f1ee685506e81cac6419fa6d72b1978183d3
andrewschultz <schultz.andrew@sbcglobal.net>
2016-04-29 16:41:00 -0500

    add GHGO to check what needs to be checked in

---
+++ ghgo.bat
@@ -0,0 +1,32 @@
+@echo off
+cd \users\andrew\documents\github
+echo THREEDIOPOLIS
+cd threediopolis
+git status -s
+echo FOURDIOPOLIS
+cd ..\fourdiopolis
+git status -s
+echo STALE TALES SLATE
+cd ..\stale-tales-slate
+git status -s
+echo PROBLEMS COMPOUND
+cd ..\the-problems-compound
+git status -s
+echo SLICKER CITY
+cd ..\slicker-city
+git status -s
+echo MISC
+cd ..\misc
+git status -s
+echo WEIRD
+cd ..\ugly-oafs
+git status -s
+cd ..\ectocomp
+git status -s
+echo TRIZBORT
+cd ..\trizbort
+git status -s
+
+if "%1" NEQ "" (
+cd %1
+)
