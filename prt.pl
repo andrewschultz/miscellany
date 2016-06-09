627e0dc0438bc25446a4c6773f58e873ccdffcc0
andrewschultz <schultz.andrew@sbcglobal.net>
2016-06-08 23:56:47 -0500

    prt.pl robustness plus file-adding

--- prt.pl
+++ prt.pl
@@ -9,9 +9,12 @@ use strict;
 use warnings;
 use i7proj;
 
+my %fileCopy;
+
 findProj();
 
-my $projToRead;
+my $prt = "c:\\games\\inform\\prt";
+my $projToRead = "";
 my $projName = getcwd();
 
 $projName =~ s/\.inform.*//g;
@@ -22,14 +25,56 @@ if (($#ARGV >= 0) && ($proj{$ARGV[0]}))
   $projToRead = $proj{$ARGV[0]};
 }
 elsif ($gotProj{$projName}) { $projToRead = $projName; }
+elsif ($projName) { $projToRead = $projName; }
+
+open(A, "c:/writing/scripts/prt.txt");
+
+while ($a = <A>)
+{
+  if ($a =~ /:/)
+  {
+    chomp($a);
+    my @b = split(/:/, $a);
+	$fileCopy{$b[0]} = $b[1];
+  }
+}
+close(A);
 
 if ($projToRead)
 {
+  my $infBase = "c:\\games\\inform\\$projToRead.inform";
   print "Copying over regression test suite\n";
-  my $q = `copy c:\\games\\inform\\$projToRead.inform\\Source\\reg-*.txt c:\\games\\inform\\prt`;
+  my $q = `copy $infBase\\Source\\reg-*.txt c:\\games\\inform\\prt`;
   print $q;
-  print "Copying over debug files\n";
-  $q = `copy c:\\games\\inform\\$projToRead.inform\\Build\\output.ulx c:\\games\\inform\\prt\\debug-$projToRead.ulx`;
+  print "Looking for build file in $infBase\\Build.\n";
+  if (-f "$infBase\\Build\\output.ulx")
+  {
+  print "Found ULX binary to copy over.\n";
+  $q = `copy $infBase\\Build\\output.ulx $prt\\debug-$projToRead.ulx`;
   print $q;
+  }
+  elsif (-f "$infBase\\Build\\output.z8")
+  {
+  print "Found Z8 binary to copy over.\n";
+  $q = `copy $infBase\\Build\\output.z8 $prt\\debug-$projToRead.z8`;
+  print $q;
+  }
+  elsif (-f "$infBase\\Build\\output.z5")
+  {
+  print "Found Z5 binary to copy over.\n";
+  $q = `copy $infBase\\Build\\output.z5 $prt\\debug-$projToRead.z5`;
+  print $q;
+  }
+  else { print "Couldn't find any output binaries.\n"; }
+  if ($fileCopy{$projToRead})
+  {
+    my @c = split(/,/, $fileCopy{$projToRead});
+    print "Copying over additional specified files.\n";
+	for (@c)
+	{
+	print "$_...\n";
+    `copy $infBase\\Source\\$_ $prt`;
+	}
+	}
 }
 else { print "No project found.\n"; }
