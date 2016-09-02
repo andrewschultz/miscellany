643d88c5437d32b3ce1738914b8bbe3ebabed887
andrewschultz <schultz.andrew@sbcglobal.net>
2016-09-02 17:12:06 -0500

    fixed PY regex

---
+++ ppp/README.md
@@ -0,0 +1,5 @@
+PPP = Perl Python Porting
+
+For projects where I know how to do something in PERL but it'd be nice to learn how to do it in Python.
+
+Old and new projects are eligible.
--- ppp/sok.py
+++ ppp/sok.py
@@ -5,18 +5,14 @@
 #sok.txt is the text file. It has lines like 2U 4L 3D 5R.
 #
 
-from array import *
 import re
 import sys
 
 dir_array = ['U','R','D','L'];
-ord_array = dir_array;
-ord_array = array('i', [0,1,2,3]);
+ord_array = [0, 1, 2, 3];
 
 rotate = 0;
 horiz = 0;
-
-print ord_array;
 
 if (len(sys.argv) > 1):
 	for dir in sys.argv[1]:
@@ -32,24 +28,21 @@ if (len(sys.argv) > 1):
 			rotate = rotate + 3 + 2 * horiz;
 		else:
 			print 'Did not recognize', ld;
-		print dir, ord_array, rotate, horiz;
 	ord_array = ord_array[rotate:4] + ord_array[0:rotate]
-	print ord_array, 'rotate', rotate, 'horiz', horiz;
 	if horiz == 1:
 		ord_array[3], ord_array[1] = ord_array[1], ord_array[3];
 	print ord_array, 'rotate', rotate, 'horiz', horiz;
 
 
 def sokrepl (match):
-	for q in range(0,4):
-		if match.group() == dir_array[q]:
-			return dir_array[ord_array[q]];
-	print match.group();
-	return 'X';
+    for q in range(0,4):
+        if match.group(2).upper() == dir_array[q]:
+            return match.group(1) + dir_array[ord_array[q]]
+    return 'X'
 
 infile = "sok.txt";
 
-q = re.compile(r'[ULDRuldr]\b');
+q = re.compile(r'(\b|[0-9\*]+)([ULDRuldr])\b')
 
 with open(infile) as f:
 	for line in f:
