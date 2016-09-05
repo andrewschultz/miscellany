808d3a150d2e6d5f86a2fe674b56e7e1306db08f
andrewschultz <schultz.andrew@sbcglobal.net>
2016-09-04 20:59:28 -0500

    oops forgot to add the alfit.pl file

---
+++ wordfun/alfit.pl
@@ -0,0 +1,48 @@
+use strict;
+use warnings;
+
+alfnam("firsts");
+alfnam("lasts");
+
+sub alfnam
+{
+my %names;
+
+open(A, "$_[0].txt");
+
+print "Processing $_[0].txt\n";
+
+open(A2, ">$_[0]-nd.txt");
+
+print "Writing $_[0]-nd.txt\n"; 
+
+while (my $line = <A>)
+{
+  chomp($line);
+  if (!defined($names{$line})) { print A2 "$line\n"; }
+  $names{$line} = 1;
+}
+
+close(A2);
+
+print "Writing $_[0]-a.txt\n";
+
+open(B, ">$_[0]-a.txt");
+
+for my $x (sort keys %names) { print B "$x\n"; }
+
+print "Writing $_[0]-l-a.txt\n";
+
+open(C, ">$_[0]-l-a.txt");
+
+for my $x (sort { length($a) <=> length($b) || $a cmp $b } keys %names) { print C "$x\n"; }
+
+close(A);
+close(B);
+close(C);
+
+print "Size of $_[0].txt: " . (-s "$_[0].txt") . "\n";
+print "Size of $_[0]-nd.txt: " . (-s "$_[0]-nd.txt") . "\n";
+print "Size of $_[0]-a.txt: " . (-s "$_[0]-a.txt") . "\n";
+print "Size of $_[0]-l-a.txt: " . (-s "$_[0]-l-a.txt") . "\n";
+}
