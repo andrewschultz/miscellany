c645b6b7a510de4f8c643686c1b3b3c4e8c18490
andrewschultz <schultz.andrew@sbcglobal.net>
2016-10-03 16:35:30 -0500

    show chains in output

--- ppp/fc.py
+++ ppp/fc.py
@@ -7,6 +7,8 @@
 import sys
 from random import shuffle
 
+onoff = ['off', 'on']
+
 suits = ['C', 'd', 'S', 'h']
 
 cards = [' A', ' 2', ' 3', ' 4', ' 5', ' 6', ' 7', ' 8', ' 9', '10', ' J', ' Q', ' K']
@@ -76,7 +78,8 @@ def checkFound():
             if spares[y] > 0:
                 if (spares[y]-1) % 13 == found[(spares[y]-1)/13]:
                     sparesuit = (spares[y]-1)/13
-                    print 'position', y, 'suit' ,suits[(spares[y]-1)/13], 'card' ,tocard(spares[y])
+                    if debug:
+                        print 'position', y, 'suit' ,suits[(spares[y]-1)/13], 'card' ,tocard(spares[y])
                     if found[(sparesuit+3)%4] < found[sparesuit] - 1:
                         break
                     if found[(sparesuit+1)%4] < found[sparesuit] - 1:
@@ -125,10 +128,28 @@ def printCards():
     else:
         printHorizontal()
  
+def chains(myrow):
+    if len(elements[myrow]) == 0:
+        return 0;
+    retval = 1
+    mytemp = len(elements[myrow]) - 1
+    while mytemp > 0:
+        if canPut(elements[myrow][mytemp], elements[myrow][mytemp-1]):
+            retval += 1
+            if retval == 10:
+                return "+"
+            mytemp = mytemp - 1
+        else:
+            return retval
+    return retval
+
 def printVertical():
     count = 0
     for y in range (1,9):
-        sys.stdout.write('  ' + str(y) + ' ')
+        sys.stdout.write('(' + str(chains(y)) + ') ')
+    print
+    for y in range (1,9):
+        sys.stdout.write(' ' + str(y) + ': ')
     print
     oneMoreTry = 1
     while oneMoreTry:
@@ -158,6 +179,7 @@ def printHorizontal():
     printOthers()
     
 def printOthers():
+    checkWin()
     canmove = ''
     for z1 in range (1,9):
         if len(elements[z1]) == 0:
@@ -170,24 +192,30 @@ def printOthers():
                 continue
             if doable(z1,z2,0):
                 canmove = canmove + ' ' + str(z1)+str(z2)
+    for z1 in range (1,9):
+        if len(elements[z1]):
+            for z2 in range (0,4):
+                if canPut(spares[z2], elements[z1][len(elements[z1])-1]):
+                    canmove = canmove + ' ' + chr(z2+97) + str(z1)
     for z1 in range (0,4):
         if spares[z1] == 0:
             canmove = canmove + ' r' + chr(z1+97)
     if canmove:
-        print 'Possible moves:', canmove
+        sys.stdout.write('Possible moves: ' + str(canmove) + ' (' + str(maxmove()) + ')\n')
     else:
         print 'Uh oh. You\'re probably lost.'
     sys.stdout.write('Empty slots:')
     for y in range (0,4):
         sys.stdout.write(' ' + tocard(spares[y]))
     sys.stdout.write('\nFoundation: ')
+    foundscore = 0
     for y in [0, 2, 1, 3]:
+        foundscore += found[y]
         if found[y] == 0:
             sys.stdout.write(' ---');
         else:
             sys.stdout.write(' ' + tocard(found[y] + y * 13))
-    print
-    checkWin()
+    print ' (' + str(foundscore) + ' points)'
 
 def doable (r1, r2, showDeets):
     cardsToMove = 0
@@ -201,12 +229,15 @@ def doable (r1, r2, showDeets):
         locmaxmove /= 2
         if showDeets:
             print 'Only half moves here down to', locmaxmove
-        for n in range(len(elements[r1])-1, 0, -1):
+        for n in range(len(elements[r1])-1, -1, -1):
             fromline += 1
+            #print '1 debug stuff:',tocard(elements[r1][n]),n,fromline
             if n == 0:
                 break
+            #print '2 debug stuff:',tocard(elements[r1][n]),n,fromline
             if canPut(elements[r1][n], elements[r1][n-1]) == 0:
                 break
+            #print '3 debug stuff:',tocard(elements[r1][n]),n,fromline
     else:
         toTopCard = elements[r2][len(elements[r2])-1]
         for n in range(len(elements[r1])-1, -1, -1):
@@ -218,6 +249,10 @@ def doable (r1, r2, showDeets):
             if canPut(elements[r1][n], elements[r1][n-1]) == 0:
                 return 0
     if fromline > locmaxmove:
+        if force == 1:
+            if showDeets:
+                print 'Cutting down to ', locmaxmove
+            return locmaxmove
         if showDeets:
             print 'Not enough open. Have',locmaxmove,'need', fromline
         return -1
@@ -225,8 +260,21 @@ def doable (r1, r2, showDeets):
     return 0
 
 def shiftcards(r1, r2, amt):
+    print r1,r2,amt
     elements[r2].extend(elements[r1][-amt:])
     del elements[r1][-amt:]
+
+def usage():
+    print 'r (1-8a-d) sends that card to the foundation'
+    print '1-8 1-8 = move a row, standard move'
+    print '(1-8a-d) (1-8a-d) move to spares and back'
+    print 'u = usage (this)'
+
+def firstEmpty():
+    for i in range(0,4):
+        if spares[i] == 0:
+            return i
+    return -1
 
 elements = []
 
@@ -244,13 +292,31 @@ initSide()
 initCards()
 printCards()
 
+debug = 0
+
 backup = [row[:] for row in elements]
 
 name = ""
 
 while win == 0:
     checkFound()
+    force = 0
     name = raw_input("Move:")
+    if len(name) == 1 and name.isdigit():
+        name = name + 'e'
+    if len(name) == 0:
+        printCards()
+        continue
+    if name[0] == '/':
+        debug = 1 - debug
+        print 'debug', onoff[debug]
+        continue
+    if name[0] == 'u':
+        usage()
+        continue
+    if name[0] == 'f':
+        name = name.replace("f", "")
+        force = 1
     if name == "-":
         elements = [row[:] for row in backup]
         initSide()
@@ -276,14 +342,10 @@ while win == 0:
     if name[0] == 'r' or name[1] == 'r':
         tofound = name.replace("r", "")
         temprow = -1
-        if name[1].isdigit():
-            temprow = int(name[1])
-        elif name[0].isdigit():
-            temprow = int(name[0])
-        elif (ord(name[0]) > 96) and (ord(name[0]) < 101):
-            tempspare = ord(name[0]) - 97
-        elif (ord(name[1]) > 96) and (ord(name[1]) < 101):
-            tempspare = ord(name[1]) - 97
+        if tofound.isdigit():
+            temprow = int(tofound)
+        elif (ord(tofound) > 96) and (ord(tofound) < 101):
+            tempspare = ord(tofound) - 97
         if temprow > -1:
             if temprow > 8 or temprow < 1:
                 print 'Not a valid row.'
@@ -304,6 +366,8 @@ while win == 0:
                 found[(spares[tempspare]-1)/13]+= 1
                 spares[tempspare] = 0;
                 print 'Moving from spares.'
+                checkFound()
+                printCards()
             else:
                 print 'Can\'t move from spares.' #/? 3s onto 2s with nothing else, all filled
             continue
@@ -317,7 +381,7 @@ while win == 0:
             continue
         tempdoab = doable(t1,t2,1)
         if tempdoab == -1:
-            print 'Not enough space.'
+            #print 'Not enough space.'
             continue
         if tempdoab == 0:
             print 'Those cards don\'t match up.'
@@ -343,10 +407,17 @@ while win == 0:
             continue
         print 'Can\'t put ', spares[mySpare], 'on', elements[myRow][len(elements[myRow])-1]
         continue;
-    if (ord(name[1]) > 96) and (ord(name[1]) < 101):
+    if (ord(name[1]) > 96) and (ord(name[1]) < 102): #1a moves, but also 1e can be A Thing
+        if name[1] == 'e':
+            myToSpare = firstEmpty()
+            if myToSpare == -1:
+                print 'Nothing empty to move to. To which to move.'
+                continue
+        else:
+            myToSpare = ord(name[1]) - 97
         myRow = int(name[0])
-        if spares[ord(name[1]) - 97] > 0:
-            print 'Already filled.'
+        if spares[myToSpare] > 0:
+            print 'Spare', myToSpare, 'already filled.'
             continue
         if myRow < 1 or myRow > 8:
             print 'From row must be between 1 and 8.'
@@ -354,7 +425,12 @@ while win == 0:
         if (len(elements[myRow]) == 0):
             print 'Empty from-row.'
             continue
-        spares[ord(name[1]) - 97] = elements[myRow].pop()
+        spares[myToSpare] = elements[myRow].pop()
         checkFound()
         printCards()
+        continue
+    print name,'not recognized, displaying usage.'
+    usage()
 endwhile
+
+#?? possible moves show restricted, put in new line
