af35bbb86a7cdf27bdcb96ac0bb0e59336d27f12
andrewschultz <schultz.andrew@sbcglobal.net>
2016-11-21 21:13:47 -0600

    can use quarter hours now

--- hrcheck.pl
+++ hrcheck.pl
@@ -13,11 +13,20 @@
 
 use strict;
 use warnings;
+use POSIX;
 
 my $check = "c:\\writing\\scripts\\hrcheck.txt";
 my $code = "c:\\writing\\scripts\\hrcheck.pl";
 
 my ($second, $minute, $hour, $dayOfMonth, $month, $yearOffset, $dayOfWeek, $dayOfYear, $daylightSavings) = localtime(time);
+
+my %browsMap;
+
+$browsMap{"FFX"} = "\"C:\\Program Files (x86)\\Mozilla Firefox\\firefox\"";
+$browsMap{"OPE"} = "\"C:\\Program Files (x86)\\Opera\\launcher.exe\"";
+$browsMap{"CHR"} = "\"C:\\Users\\Andrew\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe\"";
+
+my @quarters  = ("t", "p", "h", "b");
 
 if (defined($ARGV[0]))
 {
@@ -41,11 +50,20 @@ open(A, "$check") || die ("No $check");
 
 my $line;
 
-my $halfHour;
+my @qhr = (0, 0, 0, 0);
+
+my $defaultBrowser = "";
 
 while ($line = <A>)
 {
   chomp($line);
+  @qhr = (1, 0, 0, 0);
+  if ($line =~ /^DEF=/)
+  {
+    $defaultBrowser = $line;
+	$defaultBrowser =~ s/^DEF=//;
+	next;
+  }
   if ($line =~ /^#/) { next; }
   my $cmdCount = 0;
   my $day = -1;
@@ -57,21 +75,43 @@ while ($line = <A>)
     $cmdCount++;
   }
   my $time = $b[$cmdCount];
-  $halfHour = $time =~ tr/h/h/;
-  if ($time =~ /h/) { $time =~ s/h//; }
-  $cmdCount++;
-  if (-f "$b[$cmdCount]" && ($b[$cmdCount] =~ /(txt|otl)$/i)) # skip over empty text file
+
+  ######################quarter hours
+  if ($time =~ /[tphb]$/) { @qhr[0] = 0; }
+  while ($time =~ /[tphb]$/)
   {
-    if (-s "$b[$cmdCount]" == 0) { print "Skipping empty file $b[$cmdCount].\n"; next; }
+    for (0..3) { if ($time =~ /$quarters[$_]$/) { $qhr[$_] = 1; $time =~ s/.$//; } }
   }
+  my $min = floor($minute/15);
+  $cmdCount++;
+
   #print "$b[1]\n"; exit;
-  $b[$cmdCount] =~ s/FFX/start "" "C:\\Program Files (x86)\\Mozilla Firefox\\firefox"/;
-  $b[$cmdCount] =~ s/OPE/start "" "C:\\Program Files (x86)\\Opera\\launcher.exe"/;
-  $b[$cmdCount] =~ s/CHR/start "" "C:\\Users\\Andrew\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe"/;
+  if ($defaultBrowser)
+  {
+  $b[$cmdCount] =~ s/^DEF/$defaultBrowser/;
+  }
+  for my $q (keys %browsMap)
+  {
+  my $name_idx = index $b[$cmdCount], $q;
+  if ($name_idx >= 0)
+  {
+  substr ($b[$cmdCount],
+  $name_idx,
+  length($q),
+  $browsMap{$q} );
+  }
+  }
   if ($time == $hour)
   {
-    if ($halfHour xor ($minute < 30))
-	{ print "Running $b[$cmdCount]\n"; print `$b[$cmdCount]`; }
+    if ($qhr[$min])
+	{
+      if (-f "$b[$cmdCount]" && ($b[$cmdCount] =~ /(txt|otl)$/i)) # skip over empty text file
+      {
+        if (-s "$b[$cmdCount]" == 0) { print "Skipping empty file $b[$cmdCount].\n"; next; }
+      }
+	  print "Running $b[$cmdCount]\n";
+	  print `$b[$cmdCount]`;
+	}
   }
 }
 
