db96ccbaea52545e6edbf7ab754bd2204ea04802
andrewschultz <schultz.andrew@sbcglobal.net>
2016-12-06 06:28:28 -0600

    added diagnose flags and detail, file name not hardcoded

--- trsw.pl
+++ trsw.pl
@@ -1,18 +1,35 @@
+#################################
+#
+# trsw = trizbort switch
+#
+
 if ($#ARGV < 0) { die ("Need CSVs of ids to flip."); }
 
 my $copyBack = 0;
 
 my $count = 0;
 my %matchups;
+my %long;
+
+$long{"btp"} = "buck-the-past";
+$long{"sc"} = "slicker-city";
+$long{"pc"} = "compound-directors-cut";
+
+my $trdr = "c:\\games\\inform\\triz\\mine";
+
+my $file = "buck-the-past.trizbort";
 
 while ($count <= $#ARGV)
 {
   $a1 = $ARGV[$count];
   for ($a1)
   {
-  /^-?d$/ && do { diagnose(); exit; };
-  /^-?c$/ && do { $copyBack = 1; $count++; next; };
+  /^-?d$/ && do { diagnose(); exit(); };
+  /^-?ca?$/ && do { $copyBack = 1; if ($a1 =~ /a/) { $diagAfter = 1; } $count++; next; };
   /^-?n$/ && do { $copyBack = 0; $count++; next; };
+  /^-?o$/ && do { $order = 1; $count++; next; };
+  /^-?da$/ && do { $diagAfter = 1; $count++; next; };
+  if ($long{$a1}) { $file = $long{$a1} . ".trizbort"; $count++; next; }
   /^[0-9,]+$/ && do {
   @j = split(/,/, $a1);
   for (0..$#j)
@@ -20,17 +37,22 @@ while ($count <= $#ARGV)
     $q = ($_+1) % ($#j+1);
     if ($matchups{$j[$_]}) { die("$j[$_] is mapped twice, bailing.\n"); }
     $matchups{$j[$_]} = $j[$q];
-    print "$j[$_] -> $j[$q], from $_ to $q.\n";
+    #print "$j[$_] -> $j[$q], from $_ to $q.\n";
   }
-  $count++;
+  $count++; next;
   };
 
+  print "$a1 is an invalid parameter.\n\n";
   usage();
   }
 }
 
-open(A, "buck-the-past.trizbort");
-open(B, ">buck-the-past-id.trizbort");
+if ($order) { orderTriz(); exit(); }
+
+my $outFile = $file; $outFile =~ s/\./id\./g;
+
+open(A, "$trdr\\$file");
+open(B, ">$trdr\\$outFile");
 
 while ($a = <A>)
 {
@@ -43,16 +65,17 @@ while ($a = <A>)
 close(A);
 close(B);
 
-print "$lineDif different lines, $idDif total changes.\n";
+#print "$lineDif different lines, $idDif total changes.\n";
 
 if ($copyBack)
 {
-print "Copying back.\n";
-`copy /Y buck-the-past-id.trizbort buck-the-past.trizbort`;
+print "Copying back $file.\n";
+`copy /Y $trdr\\$outFile $trdr\\$file`;
+if ($diagAfter) { diagnose(); exit(); }
 } else
 {
-  print "-c to copy back.\n";
-  `wm buck-the-past.trizbort buck-the-past-id.trizbort`;
+  print "-c to copy back $file.\n";
+  `wm $trdr\\$outFile $trdr\\$file`;
 }
 
 sub newNum
@@ -68,13 +91,16 @@ sub newNum
 
 sub diagnose
 {
-  open(A, "buck-the-past.trizbort");
+  my $thisID;
+  my $lastID;
+  open(A, "$trdr\\$file");
   while ($line=<A>)
   {
   if ($line =~ /room id=\"/)
   {
     @q = split(/\"/, $line);
-	push(@printy, "@q[1] -> @q[3]");
+	push(@printy, "@q[1] -> @q[3] (@q[15])");
+	$lastID = @q[1];
   }
   if ($line =~ /line id=\"/)
   {
@@ -82,10 +108,28 @@ sub diagnose
 	push(@mylines, @q[1]);
   }
   }
+  @printy = sort {$a <=> $b} (@printy);
+  $lastID = 0;
+  for (@printy)
+  {
+    $thisID = $_; $thisID =~s/ .*//g;
+	if ($thisID - $lastID != 1) { $_ =~ s/ ->/ \* ->/; }
+	$lastID = $thisID;
+  }
   print join("\n", sort {$a <=> $b} (@printy)) . "\n";
   print "Lines: " . join(", ", sort {$a <=> $b} (@mylines));
 }
 
+sub orderTriz
+{
+}
+
 sub usage
 {
+print<<EOT;
+-c = copy
+-d = diagnose (show all rooms)
+btp pc sc = projects
+EOT
+exit()
 }
