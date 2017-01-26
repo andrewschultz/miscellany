bbb7dfffc76cbed8654d1bd5f3f27d9c8c198887
andrewschultz <schultz.andrew@sbcglobal.net>
2017-01-26 05:17:54 -0600

    warsum.pl now reads in PERL file to tweak as an argument

--- warsum.pl
+++ warsum.pl
@@ -9,6 +9,14 @@
 #tofix.pl 2>&1 | warsum.pl
 #
 #to add: what's in what function(s)
+#
+#ws.bat
+#@echo off
+#set FILE=%1
+#echo Note you need to specify the file with extension as DOS is a bit wobbly
+#set WARSUM=1
+#%FILE% 2>&1 | warsum.pl -annoyingstring
+#set WARSUM=
 
 use warnings;
 use strict;
@@ -16,29 +24,75 @@ use strict;
 my %count;
 my %low;
 my %high;
+my %lastsub;
 
 my $c;
 
-print "Note if you're not, run this with ws.bat to save a few keystrokes.\n";
+my $fileToSearch = "";
 
-while ($a = <STDIN>)
+if (!defined($ARGV[0])) { usage(); }
+if (-f $ARGV[0]) { $fileToSearch = $ARGV[0]; }
+if (! $fileToSearch) { die "No file $ARGV[0]. I could look for it in my path, but I'm too lazy."; }
+
+open(A, "$fileToSearch");
+
+$lastsub{"main"} = 0;
+
+my $line;
+while ($line = <A>)
 {
-  chomp($a);
-  $b = $a;
+  if ($line =~ /^sub/) { $line =~ s/^sub //; chomp($line); $lastsub{$line} = $.; }
+}
+close(A);
+
+my $mystr = `$ARGV[0]  -? 2>&1`;
+
+my @warnlines = split(/[\r\n]+/, $mystr);
+
+my $thisline;
+
+for $thisline (@warnlines)
+{
+  if ($thisline =~ /^Execution of/i) { next; }
+  chomp($thisline);
+  $b = $thisline;
   $b =~ s/.* line //;
   $c = $b; $c =~ s/\.//;
-  $a =~ s/^Global symbol \"//; $a =~ s/\".*//;
-  if (!$low{$a}) { $low{$a} = $c; $high{$a} = $c; }
+  if ($thisline !~ /^Global symbol/i) { next; }
+  $thisline =~ s/^Global symbol \"//; $thisline =~ s/\".*//;
+  if (!$low{$thisline}) { $low{$thisline} = $c; $high{$thisline} = $c; }
   else
   {
-    if ($c < $low{$a}) { $low{$a} = $c; }
-    if ($c > $high{$a}) { $high{$a} = $c; }
+    if ($c < $low{$thisline}) { $low{$thisline} = $c; }
+    if ($c > $high{$thisline}) { $high{$thisline} = $c; }
   }
-  $count{$a}++;
-  #print "$a\n";
+  $count{$thisline}++;
+  #print "$thisline\n";
 }
 
 for my $key (sort keys %low)
 {
-  print "$key covers $low{$key} to $high{$key}, $count{$key} incidences.\n";
+  print "$key covers $low{$key} to $high{$key}, $count{$key} incidences.";
+  if (cursub($low{$key}) ne cursub($high{$key})) { printf (" Separate functions: %s vs %s.", cursub($low{$key}), cursub($high{$key})); }
+  else { printf(" Completely contained in %s.", cursub($low{$key})); }
+  print "\n";
+}
+
+#####################################
+
+sub cursub
+{
+  my $k;
+  for $k (sort {$lastsub{$b} <=> $lastsub{$a}} keys %lastsub)
+  {
+    if ($_[0] > $lastsub{$k}) { return $k; }
+  }
+}
+
+sub usage
+{
+print<<EOT;
+You need to type in the name of a program to check for strict/warnings violations.
+EOT
+exit()
 }
