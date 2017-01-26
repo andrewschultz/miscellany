c8ab541cb39cdabf4d373f3f9d2c682fc4727c0a
andrewschultz <schultz.andrew@sbcglobal.net>
2017-01-26 05:32:58 -0600

    unch.pl now warnings/strict and also runs separate test (how many projs not checked in)

--- unch.pl
+++ unch.pl
@@ -3,9 +3,14 @@
 #un-checked-in changes in GIT
 #run nightly
 
+use warnings;
+use strict;
+
 use POSIX;
 
-@projAry = ("threediopolis", "ectocomp", "fourdiopolis", "stale-tales-slate", "the-problems-compound", "slicker-city", "misc", "ugly-oafs", "dirk", "trizbort", "writing");
+my %exp;
+my %projs;
+my @projAry = ("threediopolis", "ectocomp", "fourdiopolis", "stale-tales-slate", "the-problems-compound", "slicker-city", "misc", "ugly-oafs", "dirk", "trizbort", "writing");
 
 $exp{"3d"} = "threediopolis";
 $exp{"4d"} = "fourdiopolis";
@@ -17,41 +22,32 @@ $exp{"sc"} = "slicker-city";
 $exp{"uo"} = "ugly-oafs";
 $exp{"tr"} = "trizbort";
 
+my $count = 0;
+my $bigString = "";
+
+my $file = 0;
+my $proj = 0;
+my $test = 0;
+
 while ($count <= $#ARGV)
 {
-  $a = @ARGV[$count];
+  $a = $ARGV[$count];
   for ($a)
   {
-    /^-t$/ && do { $test = 1; $count++; next; };
+    /^-?t$/ && do { $test = 1; $count++; next; };
 	/^[a-z]/ && do { @projAry = split(/,/, $b); $count += 2; next; };
 	usage();
   }
 }
 
-$gitRoot = "c:\\users\\andrew\\documents\\github";
+my $gitRoot = "c:\\users\\andrew\\documents\\github";
 
-for $dir (@projAry)
+for my $dir (@projAry)
 {
 checkProject($dir);
 }
 
-sub checkProject
-{
-  $subdir = $_[0];
-  if ($exp{$subdir}) { $subdir = $exp{$subdir}; }
-  if (-d "$gitRoot\\$subdir")
-  {
-  chdir("$gitRoot\\$subdir");
-  }
-  else
-  {
-  print "Couldn't find subdir $subdir.\n";
-  }
-  $tempString = `git status -s`;
-  if ($tempString) { $bigString .= "$subdir:\n$tempString"; }
-}
-
-@errfiles = split(/\n/, $bigString);
+my @errfiles = split(/\n/, $bigString);
 {
   for (@errfiles) { if ($_ =~ /:$/) { $proj++; } else { $file++; } }
 }
@@ -63,7 +59,27 @@ if (!$test)
 else
 {
   $bigString =~ s/\n/<br \/>/g;
-  print "TEST RESULTS:unchecked in files,4,$file,0,$bigString\n";
+  print "TEST RESULTS:unchecked in projects,4,$file,0,$bigString\n";
+  my $projLeft = join("/", sort keys %projs);
+  print "TEST RESULTS:unchecked in files,2,$proj,0,$projLeft\n";
+}
+
+###########################subroutines
+
+sub checkProject
+{
+  my $subdir = $_[0];
+  if ($exp{$subdir}) { $subdir = $exp{$subdir}; }
+  if (-d "$gitRoot\\$subdir")
+  {
+  chdir("$gitRoot\\$subdir");
+  }
+  else
+  {
+  print "Couldn't find subdir $subdir.\n";
+  }
+  my $tempString = `git status -s`;
+  if ($tempString) { $bigString .= "$subdir:\n$tempString"; $projs{$subdir} = 1; }
 }
 
 sub usage
