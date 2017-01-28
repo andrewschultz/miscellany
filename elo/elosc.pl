95fd44f6cd8e010bc086b0b817ec7731d76e2dc9
andrewschultz <schultz.andrew@sbcglobal.net>
2017-01-28 07:36:44 -0600

    warning/strict for elosc

--- elo/elosc.pl
+++ elo/elosc.pl
@@ -8,19 +8,23 @@
 #
 #so anything of the form date \tab Winners xxx, Losers xxx should work
 
-#use strict;
-#use warnings;
+use strict;
+use warnings;
 
 my %rating;
 my %tempRating;
 
-#defaults
-my $count = 0;
+#defaults, can be tweaked with options
 my $dBug = 0;
 my $iterations = 2000;
 my $pointsPerWin = 32;
 my $defaultRating = 2000;
 
+#variables
+my $count = 0;
+my @allGames = ();
+
+#this could be put in a text file, but for now, it's not. Teams/Nicknames could be a hash.
 my @teams = ("Illinois", "Indiana", "Iowa", "Maryland", "Michigan", "Michigan State", "Minnesota", "Nebraska", "Northwestern", "Ohio State", "Penn State", "Purdue", "Rutgers", "Wisconsin");
 
 my %nickname;
@@ -43,8 +47,8 @@ for (@teams) { $rating{$_} = $defaultRating; }
 
 while ($count <= $#ARGV)
 {
-  $a = @ARGV[$count];
-  $b = @ARGV[$count+1];
+  $a = $ARGV[$count];
+  $b = $ARGV[$count+1];
   for ($a)
   {
     /-r/ && do { $defaultRating = $b; $count += 2; next; };
@@ -81,26 +85,31 @@ for ($count = 1; $count <= $iterations; $count++)
 eloIterate();
 }
 
+my $x;
+
 foreach $x (sort keys %rating)
   {
     $rating{$x} = int($rating{$x} + .5);
   }
 
-@sorted = sort { $rating{$b} cmp $rating{$a} } keys %rating;
+ my $rank = 0;
 
-$count=0; $tempCount = 0; $overallCount=0;
+ print "<table>\n";
+foreach $x (sort {$rating{$a} <=> $rating{$b}} keys %rating)
+{
+  $rank++;
+  print "<tr><td>$rank<td>$x<td>$rating{$x}\n";
+}
+print "</table>";
 
-for (@sorted) {
-  $overallCount++;
-  $count += ($draftOrder{$_} ne "c");
-  $tempCount = $count; if ($draftOrder{$_} eq "c") { $tempCount = ""; }
-  #$tempCount = $overallCount;
-  printf "%2s %10s $rating{$_} $draftOrder{$_}\n", $tempCount, $_;
-  }
-
-  print "<table><tr><td>";
+# now to print the table of probabilities
+print "<table><tr><td>";
   
-  for $t1 (sort keys %rating)
+my $t1;
+my $t2;
+my $mult;
+
+for $t1 (sort keys %rating)
   {
     print "<td>";
 	print ifshort($t1);
@@ -123,22 +132,26 @@ for (@sorted) {
 
 sub eloIterate # this takes the ELO rating and
 {
+  my $x;
+  my @b;
+  my $expPoints;
+  my $expWins;
 foreach $x (keys %rating) { $tempRating{$x} = $rating{$x}; }
 
 for (@allGames)
 {
   @b = split(/[,]/, $_);
-  #printf("@b[0] %d vs @b[1] %d\n", $rating{$b[0]}, $rating{$b[1]});
-  unless (defined($rating{@b[0]}) && defined($rating{@b[1]})) { next; }
-  if (!$rating{@b[0]}) { $rating{@b[0]} = $tempRating{@b[0]} = $defaultRating; printDbug("resetting @b[0]\n"); }
-  if (!$rating{@b[1]}) { $rating{@b[1]} = $tempRating{@b[1]} = $defaultRating; printDbug("resetting @b[1]\n"); }
-  $mult = ($rating{@b[1]} - $rating{@b[0]})/400;
+  #printf("$b[0] %d vs $b[1] %d\n", $rating{$b[0]}, $rating{$b[1]});
+  unless (defined($rating{$b[0]}) && defined($rating{$b[1]})) { next; }
+  if (!$rating{$b[0]}) { $rating{$b[0]} = $tempRating{$b[0]} = $defaultRating; printDbug("resetting $b[0]\n"); }
+  if (!$rating{$b[1]}) { $rating{$b[1]} = $tempRating{$b[1]} = $defaultRating; printDbug("resetting $b[1]\n"); }
+  $mult = ($rating{$b[1]} - $rating{$b[0]})/400;
   $expWins = 1/(1+10**($mult));
-  #printDbug("Expected wins for @b[0] vs @b[1] rating $rating{@b[0]} rating $rating{@b[1]} = $expWins\n");
+  #printDbug("Expected wins for $b[0] vs $b[1] rating $rating{$b[0]} rating $rating{$b[1]} = $expWins\n");
   $expPoints = $pointsPerWin*(1 - $expWins * 1);
-  #printDbug("@b[0] gains $expPoints\n@b[1] loses $expPoints\n");
-  $tempRating{@b[0]} += $expPoints;
-  $tempRating{@b[1]} -= $expPoints;
+  #printDbug("$b[0] gains $expPoints\n$b[1] loses $expPoints\n");
+  $tempRating{$b[0]} += $expPoints;
+  $tempRating{$b[1]} -= $expPoints;
 }
 
 my $totalDelt = 0;
@@ -153,18 +166,21 @@ printDbug("$totalDelt total rating shift for run $count.\n");
 
 }
 
+##################this is the ELO formula for expected wins
 sub winPct
 {
   my $exp = ($rating{$_[1]} - $rating{$_[0]}) / 400;
   return 100/(1+10**$exp);
 }
 
+##################if debug flag is on, print what's there. Otherwise, do nothing.
 sub printDbug
 {
   if (!$dBug) { return; }
   print $_[0];
 }
 
+################team's nickname, if they have one. If not, team's name
 sub ifshort
 {
   if (defined($nickname{$_[0]}))
@@ -173,7 +189,6 @@ sub ifshort
   }
   return $_[0];
 }
-
 
 sub usage
 {
@@ -185,3 +200,6 @@ print<<EOT;
 EOT
 exit;
 }
+
+# future options: 1 table or no 2 show debug every X turns
+# also, create a hash of win numbers for each side, as well as their schedule. @allGames contains nonconference games at the moment.
