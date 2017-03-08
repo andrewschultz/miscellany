670c643b59ecc200d8a562abaa2f3d7c0ffd9ca2
andrewschultz <schultz.andrew@sbcglobal.net>
2017-03-08 11:56:54 -0600

    fixed from FAQ markup conversions of 2 small guides

--- wrifi/desp.pl
+++ wrifi/desp.pl
@@ -2,7 +2,8 @@
 #
 # despacer desp.pl
 #
-# removes spaces in a GameFAQs FAQ and also does outline stuff
+# removes spaces in a GameFAQs FAQ and also does outline adjusting stuff
+# changes emails too, etc.
 #
 
 use strict;
@@ -12,14 +13,19 @@ if ($#ARGV == -1) { die ("Need name of file."); }
 
 my $filename = $ARGV[0];
 my $outfile = $filename;
-$outfile =~ s/\./-despace\./g;
+$outfile =~ s/\./-despace\./;
+if ($outfile eq $filename) { $outfile .= "-despace";
 
 my $outFileText = "";
 
 my $lastLineText = 0;
 my $spacesFound = 0;
 
+my $copyBackOver = 1;
+
 my $parzap = 0;
+
+if (!defined($ARGV[0])) { die ("Need a file name to proceed."); }
 
 open(A, "$ARGV[0]") || die ("Can't find $ARGV[0].");
 
@@ -29,6 +35,9 @@ my $gotMarkup = 0;
 
 while (my $line = <A>)
 {
+  $line =~ s/schultz.andrew\@earthlink.net/blurglecruncheno\@gmali.cmo \(anti spam typo\)/g;
+
+  ###############fix numerical outlines to ='s
   if (($line =~ /^(  )*[0-9].*\./) && ($line !~ /[a-z]/))
   {
     chomp($line);
@@ -38,9 +47,11 @@ while (my $line = <A>)
 	$outFileText .= "=" x ($#l + 2) . $line2 . "=" x ($#l + 2) . "\n";
 	next;
   }
+
    if ($line eq ";format:gf-markup\n") { $gotMarkup = 1; }
   $line =~ s/schultz.andrew\@sbcglobal.net/blurglecruncheno\@gmail\.cmo/g; # change email address
-  if (($line =~ /[a-z\. '\\"\*\_]/i) && (length($line) > 65) && ($line !~ /^[\*\|]/))
+  $line =~ s/schultza\@earthlink.net/blurglecruncheno\@gmail\.cmo/g; # change email address
+  if (($line =~ /[a-z\. '\\"\*\_]/i) && (length($line) > 65) && ($line !~ /^[\#\*\|]/))
   {
     if (($parzap == 1) && ($line =~ /^  [A-Z]/)) { $line =~ s/^  //g; }
     $line =~ s/\.  /\. /g;
@@ -63,3 +74,9 @@ print B $outFileText;
 close(B);
 
 printf("%d bytes to %d bytes, total spaces = %d.\n", -s "$filename", -s "$outfile", $spacesFound);
+
+if ($copyBackOver)
+{
+  print "Copying $outfile back over.";
+  copy($outfile, $filename);
+}
