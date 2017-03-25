7742f9a9ee607bce7601351de2a045ad343c424f
andrewschultz <schultz.andrew@sbcglobal.net>
2017-03-24 20:53:27 -0500

    python formatting for Bill James Safe Lead Calculator

--- ppp/bj.py
+++ ppp/bj.py
@@ -21,17 +21,26 @@ def timesplit(x):
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("lead", type=str, help="team's lead")
+parser.add_argument("lead", type=int, help="team's lead")
 parser.add_argument("time", type=str, help="time left (can use colon/period to delimit minutes)")
 
 args = parser.parse_args()
 
 timeint = timesplit(args.time)
 
-def lead(x, y, z):
-	retval = (float(x) - 3.5 + z) ** 2
-	return retval * 100 / y
-	
-print lead(args.lead, timeint, 1), '% safe with ball'
-print lead(args.lead, timeint, 0), '% safe with ball'
+def floatTime(x):
+    return (float(x) - 3.5) ** 2
 
+def lead(x, y, z):
+    retval = (floatTime(x+z))
+    if x < 4:
+        return 0
+    return float(retval * 100) / float(y)
+
+if args.lead < 4:
+    print "Lead is too small to matter."
+else:
+    print("%.2f %% safe with ball" % lead(args.lead, timeint, 1))
+    print("%.2f %% safe without ball" % lead(args.lead, timeint, 0))
+    print("%2d:%02d cutoff time for with the ball" % (floatTime(args.lead+1) / 60, floatTime(args.lead+1) % 60))
+    print("%2d:%02d cutoff time for without the ball" % (floatTime(args.lead) / 60, floatTime(args.lead) % 60))
