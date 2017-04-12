ee7cf6322810b050a66f061b8adafecc1d3381f1
andrewschultz <schultz.andrew@sbcglobal.net>
2017-04-11 23:22:42 -0500

    tweaks to frn.pl

--- frn.pl
+++ frn.pl
@@ -1,9 +1,13 @@
 ################################
 #FRN.PL: Full Room Name
 #
+#command line variables ATM: word1,word2
+#
 
 use strict;
 use warnings;
+
+my $schema = "frn-schema.txt";
 
 ###################hashes
 my %misses;
@@ -11,6 +15,7 @@ my %idx;
 my %need;
 my %adds;
 my %litany;
+my %exceptions;
 
 ###################variables
 my @words;
@@ -19,22 +24,46 @@ my $count = 0;
 my $origLine;
 my $b4;
 my $line;
+my $detail = 0;
 
-open(A, "frm-schema.txt");
+if (defined($ARGV[0]))
+{
+  print "Reading command line for prevword,nextword\n";
+  for ($ARGV[0])
+  {
+    @words=split(/,/, $_);
+	$need{$words[1]} = $words[0];
+  }
+}
+else
+{
+open(A, $schema) || die ("No $schema.");
 
 while ($line=<A>)
 {
+  if ($line =~ /^!/) #in other words, ignore this except for detailed searches
+  {
+    if ($detail == 0) { next; }
+	$line =~ s/^!//;
+  }
   if ($line =~ /^;/) { last; }
   if ($line =~ /^#cut/) { $longComment = !$longComment; next; }
   if ($line =~ /^#/) { next; }
   if ($longComment) { next; }
   chomp($line);
+  if ($line =~ /^-/)
+  {
+    $line =~ s/^-//;
+	$exceptions{$line} = 1;
+	next;
+  }
   $line = lc($line);
   @words = split(/ /, $line);
   if ($#words == 1) { $need{$words[1]} = $words[0]; }
 }
 
 close(A);
+}
 
 open(A, "story.ni");
 
@@ -42,6 +71,7 @@ my $key;
 
 while ($line = <A>)
 {
+  if ($line =~ /^to say/) { next; }
   for $key (keys %need)
   {
     if ($line =~ /\b$key\b/i)
@@ -59,11 +89,11 @@ while ($line = <A>)
       $origLine = $line;
       $line = lc($line);
       $line =~ s/^[\t ]*//; chomp($line);
-      @words = split(/\W+/, $line);
+      @words = split(/[ ;=:,\.\t\"\]\[\?!\(\)]+/, $line);
       for $count (0..$#words)
       {
         #print "$count $words[$count]\n";
-        if ((lc($words[$count]) eq $key) && ($need{$key} ne lc($words[$count-1])))
+        if ((lc($words[$count]) eq $key) && ($need{$key} ne lc($words[$count-1])) && (!defined($exceptions{"$words[$count-1] $words[$count]"})))
         {
           chomp($origLine);
 		  $origLine =~ s/^[\t ]*//;
