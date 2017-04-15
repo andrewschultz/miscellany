f4a09ae34b6fd3650bfb7d8f3f65cc8a506bf449
andrewschultz <schultz.andrew@sbcglobal.net>
2017-04-15 17:22:26 -0500

    ses now launches/other cmd line tweaks

--- ses.pl
+++ ses.pl
@@ -14,6 +14,7 @@ my $sourceFile  = __FILE__;
 my $outputFile = $sourceFile;
 $outputFile =~ s/pl$/txt/i;
 my $npSes = "C:\\Users\\Andrew\\AppData\\Roaming\\Notepad++\\session.xml";
+my $ses = "c:\\writing\\scripts\\ses.htm";
 my $tabMax = 25;
 my $newMax = 15;
 my $tabMin = 10;
@@ -37,6 +38,7 @@ my $count = 0;
 my $toOutput = 0;
 my $analyze = 0;
 my $htmlGen = 0;
+my $launch = 0;
 
 while ($count <= $#ARGV)
 {
@@ -54,6 +56,7 @@ while ($count <= $#ARGV)
   };
   /^-?e$/ && do { `$outputFile`; exit(); };
   /^-?h$/ && do { $htmlGen = 1; $count++; next; };
+  /^-?l$/ && do { $launch = 1; $count++; next; };
   /^-?o$/ && do { $toOutput = 1; $count++; next; };
   /^-?x$/ && do { `$npSes`; exit(); };
   usage();
@@ -113,25 +116,34 @@ if ($analyze)
 	for (@b) { $_ =~ s/ .*//g; }
 	if ($b[0] > $tabMax) { $tabsOverStreak++; } else { $tabsOverStreak = 0; }
 	if ($b[1] > $newMax) { $newOverStreak++; } else { $newOverStreak = 0; }
-	if (($b[0] > $lastTabs) && ($lastTabs > $tabMin)) { $tabsInc++; } else { $tabsInc = 0; }
-	if (($b[1] > $lastNew) && ($lastNew > $newMin)) { $newInc++; } else { $newInc = 0; }
+	if (($b[0] > $lastTabs) && ($lastTabs >= $tabMin)) { $tabsInc++; } else { $tabsInc = 0; }
+	if (($b[1] > $lastNew) && ($lastNew >= $newMin)) { $newInc++; } else { $newInc = 0; }
 	$lastNew = $b[1];
 	$lastTabs = $b[0];
+	print "$b[0] $b[1] $tabsOverStreak $newOverStreak $tabsInc $newInc\n";
   }
 
   my @errs;
-  if ($newOverStreak > 1) { push (@errs, "NEW TABS too big $newOverStreak days in a row."); }
-  if ($newInc > 1) { push (@errs, "NEW TABS grew $newOverStreak days in a row."); }
-  if ($tabsOverStreak > 1) { push (@errs, "OVERALL TABS too big $tabsOverStreak days in a row."); }
-  if ($tabsInc > 1) { push (@errs, "OVERALL TABS grew $tabsOverStreak days in a row."); }
+  if ($newOverStreak > 1) { push (@errs, "NEW TABS too big $newOverStreak times in a row."); }
+  if ($newInc > 1) { push (@errs, "NEW TABS grew $newOverStreak times in a row."); }
+  if ($tabsOverStreak > 1) { push (@errs, "OVERALL TABS too big $tabsOverStreak times in a row."); }
+  if ($tabsInc > 1) { push (@errs, "OVERALL TABS grew $tabsOverStreak times in a row."); }
   if ($#errs > -1)
   {
     if ($htmlGen)
 	{
-	  open(B, ">c:\\writing\\scripts\\ses.htm");
-	  print B "<html><title>Streak Error Stuff</title><body bgcolor=red>\n";
-	  for (@errs) { print B "<font size=+3>$_</font>\n"; }
+	  open(B, ">$ses");
+	  print B "<html><title>Streak Error Stuff</title><body bgcolor=red><center><font size=+5>SES.PL RESULTS:</font></center>\n";
+	  for (@errs) { print B "<center><font size=+3>$_</font></center>\n"; }
 	  close(B);
+	  if ($launch)
+	  {
+	  `$ses`;
+	  }
+	  else
+	  {
+	  print "Use -l to launch.\n";
+	  }
 	}
 	else
 	{
@@ -153,6 +165,7 @@ print<<EOT;
 -c = edit source code
 -e = edit stat file
 -h = to html
+-l = launch HTML file created with -a -h
 -o = output to stat file
 -x = edit XML tabs file
 EOT
