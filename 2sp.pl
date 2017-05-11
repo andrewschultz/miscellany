391bc82e3c20656d7c029e16dca9cb959cacd59d
andrewschultz <schultz.andrew@sbcglobal.net>
2017-05-11 17:43:50 -0500

    2sp add usage, allow printAnyway on ignore

--- 2sp.pl
+++ 2sp.pl
@@ -11,6 +11,7 @@ use warnings;
 my $difference = 0;
 my $copy = 0;
 my $makeDifFile = 1;
+my $printAnyway = 0;
 
 #####################variables
 my $inI6 = 0;
@@ -24,15 +25,24 @@ my %ignoreHash;
 my $myProj = initGuess();
 my $foundDif = 0;
 
+###################constants
+my $ignore = __FILE__;
+$ignore =~ s/pl$/txt/i;
+
+my $in = "$dir\\story.ni";
+my $out = "$dir\\story.nsp";
+my $dif = "$dir\\story.dif.txt";
+
 while ($count <= $#ARGV)
 {
   my $arg = $ARGV[$count];
-  print "$count $arg\n";
   for ($arg)
   {
-  /^-?d$/ && do { $difference = 1; $copy = 0; $makeDifFile = 1; $count++; next; };
+  /^-?e$/ && do { `$ignore`; exit(); };
   /^-?c$/ && do { $difference = 0; $copy = 1; $makeDifFile = 0; $count++; next; };
+  /^-?d$/ && do { $difference = 1; $copy = 0; $makeDifFile = 1; $count++; next; };
   /^-?m$/ && do { $difference = 0; $copy = 0; $makeDifFile = 1; $count++; next; };
+  /^-?p$/ && do { $printAnyway = 1; $count++; next; };
   /^-?\?$/ && do { usage(); };
   if ($projYet) { print "unknown flag/double project $arg\n"; usage(); }
   $myProj = $ARGV[0];
@@ -41,14 +51,6 @@ while ($count <= $#ARGV)
   $count++;
   }
 }
-
-###################constants
-my $ignore = __FILE__;
-$ignore =~ s/pl$/txt/i;
-
-my $in = "$dir\\story.ni";
-my $out = "$dir\\story.nsp";
-my $dif = "$dir\\story.dif.txt";
 
 open(A, $ignore);
 while ($a = <A>)
@@ -59,6 +61,7 @@ while ($a = <A>)
   if ($a =~ /^PROJ=/i) { $projToParse = $a; $projToParse =~ s/^proj=//i; next; }
   $ignoreHash{$projToParse}{$a} = 1;
 }
+if (!defined($ignoreHash{$myProj})) { warn ("No project $myProj"); }
 
 close(A);
 
@@ -68,6 +71,8 @@ if ($makeDifFile)
 {
 open(C, ">$dif");
 }
+
+my $possErr;
 
 while ($a = <A>)
 {
@@ -81,9 +86,14 @@ while ($a = <A>)
 
   if ((!$inI6) && (!$inComment))
   {
-  if (($b =~ /  /) && !ignore($b))
+  $possErr = ($b =~ / (\[(if|one)[^\]]+\])? /);
+  if ($possErr && !ignore($b, $printAnyway))
   {
-  $b =~ s/  +/ /g;
+  if ($printAnyway && ignore($b, 0))
+  {
+  print C "(FLAGGED AS NOT REALLY AN ERROR) ";
+  }
+  $b =~ s/ +/ /g;
   if ($makeDifFile)
   {
   $a =~ s/  /\*\*\*\*\*\*  /;
@@ -101,7 +111,6 @@ close(B);
 if (!$foundDif)
 {
   print "No difference found in story.ni.\n";
-  return;
 }
 
 if ($difference)
@@ -124,11 +133,17 @@ if ($copy)
 ##############################
 #subroutines
 
+##############################
+#$_[0] is the line to read
+#$_[1] is if we should bail anyway
 sub ignore
 {
+  if ($_[1] == 1) { return 0; }
   for my $regex (keys %{$ignoreHash{$myProj}})
   {
+    print "REGEX $regex ~? LINE $_[0]";
     if ($_[0] =~ /$regex/) { return 1; }
+	print "FAILED\n";
   }
   return 0;
 }
@@ -147,6 +162,12 @@ sub initGuess
 sub usage
 {
 print<<EOT;
+-e = edit ignore file
+-c = copy over
+-d = show difference
+-m = make difference file
+-p = print anyway
+(you can also define a project, else story.ni is used)
 EOT
 exit();
 }
