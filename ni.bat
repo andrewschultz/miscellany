2ed00e7cde5c9e55b9c87124e2dfed68db44ce57
andrewschultz <schultz.andrew@sbcglobal.net>
2017-07-07 05:46:45 -0500

    ni.bat now less horrible I think

--- ni.bat
+++ ni.bat
@@ -1,5 +1,9 @@
 @echo off
 
+set OPENSOURCE=0
+set TABLE=0
+set RANDTEXT=0
+set NUDGE=0
 set NOTES=c:\users\andrew\dropbox\notes
 set EXT=.inform
 set PROJ=buck-the-past
@@ -14,122 +18,60 @@ if "%1" EQU "?" (
 goto usage
 )
 
-if "%1" EQU "b" (
-set SOREL=build
-shift /1
-)
+:parse
 
-if "%1" EQU "ta" (
-goto tableread
-)
-
-if "%2" EQU "b" (
-set SOREL=build
-)
-
-if "%2" EQU "m" (
-set EXT= Materials
-set SOREL=release
-)
-
-if "%1" EQU "m" (
-set EXT= Materials
-set SOREL=release
-shift /1
-)
-
-::only add projects below
-
-if "%1" EQU "e11" (
-set PROJ=dash
-goto nodef
-)
-
-if "%1" EQU "e13" (
-set PROJ=ghost
-goto nodef
-)
-
-if "%1" EQU "e14" (
-set PROJ=candy rush saga
-goto nodef
-)
-
-if "%1" EQU "e15" (
-set PROJ=heezy-park
-goto nodef
-)
-
-if "%1" EQU "e16" (
-set PROJ=checkered-haunting
-goto nodef
-)
-
-if "%1" EQU "ni" (
-cd \writing\dict\nightly
-goto end
-)
-
-if "%1" EQU "n" (
-cd \writing\dict\nightly
-goto end
-)
-
-if "%1" EQU "b" (
-set SOREL=Build
-shift /1
-)
-
-c:
-
-if "%1" EQU "z" (
-cd \games\inform\zip
-goto end
-)
-
-if "%1" EQU "w" (
-cd \games\inform\walkthroughs
-goto end
-)
+rem forcing options first
 
 if "%1" EQU "0" (
+echo overriding other arguments, going to Inform base dir
 cd \games\inform
 goto end
 )
 
-if "%1" EQU "tr" (
-cd \games\inform\triz
+if "%1" EQU "n" (
+echo overriding other args, going to nightly build
+cd \writing\dict\nightly
 goto end
 )
 
-if "%1" EQU "t" (
-goto story
+if "%1" EQU "ni" (
+echo overriding other args, going to nightly build
+cd \writing\dict\nightly
+goto end
 )
 
-if "%1" EQU "t2" (
-set PROJ=slicker-city
-goto story
+if "%1" EQU "no" (
+cd \users\andrew\dropbox\notes
+goto end
 )
 
-if "%1" EQU "tp" (
-set PROJ=Compound
-goto story
+if "%1" EQU "tr" (
+echo overriding other arguments, going to trizbort dir
+echo tr.bat allows more options
+cd \games\inform\triz
+shift /1
+goto end
 )
 
-if "%1" EQU "tb" (
-set PROJ=buck-the-past
-goto story
+if "%1" EQU "w" (
+echo overriding other arguments, going to Inform walkthroughs dir
+cd \games\inform\walkthroughs
+goto end
 )
 
-if "%1" EQU "t3" (
-set PROJ=threediopolis
-goto story
+if "%1" EQU "x" (
+  echo X shortcircuits other options. Going to extensions directory.
+  cd "c:\Program Files (x86)\Inform 7\Inform7\Extensions\Andrew Schultz"
+  goto end
 )
 
-if "%1" EQU "t4" (
-set PROJ=fourdiopolis
-goto story
+if "%1" EQU "z" (
+echo overriding other arguments, going to Inform zip dir
+cd \games\inform\zip
+goto end
 )
+
+rem now forcing opening files
 
 if "%1" EQU "g" (
 start "" "C:\Program Files (x86)\Notepad++\notepad++.exe" c:\writing\games.otl
@@ -139,66 +81,6 @@ goto end
 if "%1" EQU "l" (
 start "" "C:\Program Files (x86)\Notepad++\notepad++.exe" c:\writing\limericks.otl
 goto end
-)
-
-if "%1" EQU "gr" (
-set PROJ=grubbyville
-goto nodef
-)
-
-if "%1" EQU "gru" (
-set PROJ=grubbyville
-goto nodef
-)
-
-if "%1" EQU "3" (
-set PROJ=threediopolis
-goto nodef
-)
-
-if "%1" EQU "3d" (
-set PROJ=threediopolis
-goto nodef
-)
-
-if "%1" EQU "4" (
-set PROJ=fourdiopolis
-goto nodef
-)
-
-if "%1" EQU "4d" (
-set PROJ=fourdiopolis
-goto nodef
-)
-
-if "%1" EQU "11" (
-set PROJ=fanint
-goto nodef
-)
-
-if "%1" EQU "12" (
-set PROJ=shuffling
-goto nodef
-)
-
-if "%1" EQU "ek" (
-set PROJ=ektor
-goto nodef
-)
-
-if "%1" EQU "fi" (
-set PROJ=fanint
-goto nodef
-)
-
-if "%1" EQU "op" (
-set PROJ=threediopolis
-goto nodef
-)
-
-if "%1" EQU "r" (
-set PROJ=roiling
-goto nodef
 )
 
 if "%1" EQU "rt" (
@@ -211,159 +93,22 @@ start "" "C:\Program Files (x86)\Notepad++\notepad++.exe" "c:\writing\dict\sts.t
 goto end
 )
 
-if "%1" EQU "ro" (
-set PROJ=roiling
-goto nodef
-)
-
 if "%1" EQU "an" (
 start "" "C:\Program Files (x86)\Notepad++\notepad++.exe" "C:\games\inform\roiling.inform\Source\tosort.txt"
 goto end
 )
 
-if "%1" EQU "roi" (
-set PROJ=roiling
-goto nodef
-)
-
-if "%1" EQU "15" (
-set PROJ=compound
-goto nodef
-)
-
-if "%1" EQU "p" (
-set PROJ=compound
-goto nodef
-)
-
-if "%1" EQU "c" (
-set PROJ=compound
-goto nodef
-)
-
-if "%1" EQU "pc" (
-set PROJ=compound
-goto nodef
-)
-
-if "%1" EQU "sb" (
-set PROJ=see-why-burg
-goto nodef
-)
-
-if "%1" EQU "sc" (
-set PROJ=slicker-city
-goto nodef
-)
-
-if "%1" EQU "16" (
-set PROJ=slicker-city
-goto nodef
-)
-
-if "%1" EQU "17" (
-set PROJ=buck-the-past
-goto nodef
-)
-
-if "%1" EQU "bp" (
-set PROJ=buck-the-past
-goto nodef
-)
-
-if "%1" EQU "btp" (
-set PROJ=buck-the-past
-goto nodef
-)
-
-if "%1" EQU "bs" (
-set PROJ=btp-st
-goto nodef
-)
-
-if "%1" EQU "ss" (
-set PROJ=seeker-status
-goto nodef
-)
-
-if "%1" EQU "bt" (
-set PROJ=buck-the-past
-goto nodef
-)
-
-if "%1" EQU "i" (
-set PROJ=invis
-goto nodef
-)
-
-if "%1" EQU "uo" (
-set PROJ=ugly-oafs
-goto nodef
-)
-
-if "%1" EQU "o" (
-set PROJ=ugly-oafs
-goto nodef
-)
-
-if "%1" EQU "oafs" (
-set PROJ=ugly-oafs
-goto nodef
-)
-
-if "%1" EQU "14" (
-set PROJ=ugly-oafs
-goto nodef
-)
-
-if "%1" EQU "s" (
-set PROJ=shuffling
-goto nodef
-)
-
-if "%1" EQU "sa" (
-set PROJ=shuffling
-goto nodef
-)
-
-if "%1" EQU "ux" (
-set PROJ=uxmulbrufyuz
-goto nodef
-)
-
-if "%1" EQU "f" (
-set PROJ=fetch-quest
-goto nodef
-)
-
-if "%1" EQU "no" (
-cd \users\andrew\dropbox\notes
-goto end
-)
-
-if "%1" EQU "fetch" (
-set PROJ=fetch-quest
-goto nodef
-)
-
-if "%1" EQU "ge" (
-set PROJ=gems
-goto nodef
-)
-
-if "%1" NEQ "" (
-set PROJ=%1
-)
+rem start release file edits
 
 if "%1" EQU "cr" (
 %NOTES%\buck_the_past_release_1_notes.txt
 goto end
 )
 
-::above is current hot project
+::above is current hot project and can change
 
 if "%1" EQU "4r" (
-%NOTES%\fourdiopolis_release_2_notes.txt
+%NOTES%\fourdiopolis_release_3_notes.txt
 goto end
 )
 
@@ -402,19 +147,350 @@ if "%1" EQU "pr" (
 goto end
 )
 
-:nodef
+rem end forcing options
 
-if "%2" EQU "x" (
-"c:\Program Files (x86)\Inform 7\Inform7\Extensions\Andrew Schultz"
+rem start what-file-dir-to-open options
+
+if "%1" EQU "b" (
+set SOREL=build
+shift /1
+goto parse
 )
+
+if "%1" EQU "nu" (
+set NUDGE=1
+shift /1
+goto parse
+)
+
+if "%1" EQU "ra" (
+set RANDTEXT=1
+shift /1
+goto parse
+)
+
+if "%1" EQU "ta" (
+set TABLE=1
+shift /1
+goto parse
+)
+
+if "%1" EQU "m" (
+set EXT= Materials
+set SOREL=release
+shift /1
+goto parse
+)
+
+rem end what-file-dir-to-open options
+
+rem start what-project-to-open options
+
+if "%1" EQU "e11" (
+set PROJ=dash
+shift /1
+goto parse
+)
+
+if "%1" EQU "e13" (
+set PROJ=ghost
+shift /1
+goto parse
+)
+
+if "%1" EQU "e14" (
+set PROJ=candy rush saga
+shift /1
+goto parse
+)
+
+if "%1" EQU "e15" (
+set PROJ=heezy-park
+shift /1
+goto parse
+)
+
+if "%1" EQU "e16" (
+set PROJ=checkered-haunting
+shift /1
+goto parse
+)
+
+if "%1" EQU "t" (
+set OPENSOURCE=1
+shift /1
+goto parse
+)
+
+if "%1" EQU "t2" (
+set PROJ=slicker-city
+shift /1
+goto story
+)
+
+if "%1" EQU "tp" (
+set PROJ=Compound
+shift /1
+goto story
+)
+
+if "%1" EQU "tb" (
+set PROJ=buck-the-past
+shift /1
+goto story
+)
+
+if "%1" EQU "t3" (
+set PROJ=threediopolis
+shift /1
+goto story
+)
+
+if "%1" EQU "t4" (
+set PROJ=fourdiopolis
+shift /1
+goto story
+)
+
+if "%1" EQU "gr" (
+set PROJ=grubbyville
+shift /1
+goto parse
+)
+
+if "%1" EQU "gru" (
+set PROJ=grubbyville
+shift /1
+goto parse
+)
+
+if "%1" EQU "3" (
+set PROJ=threediopolis
+shift /1
+goto parse
+)
+
+if "%1" EQU "3d" (
+set PROJ=threediopolis
+shift /1
+goto parse
+)
+
+if "%1" EQU "4" (
+set PROJ=fourdiopolis
+shift /1
+goto parse
+)
+
+if "%1" EQU "4d" (
+set PROJ=fourdiopolis
+shift /1
+goto parse
+)
+
+if "%1" EQU "11" (
+set PROJ=fanint
+shift /1
+goto parse
+)
+
+if "%1" EQU "12" (
+set PROJ=shuffling
+shift /1
+goto parse
+)
+
+if "%1" EQU "ek" (
+set PROJ=ektor
+shift /1
+goto parse
+)
+
+if "%1" EQU "fi" (
+set PROJ=fanint
+shift /1
+goto parse
+)
+
+if "%1" EQU "op" (
+set PROJ=threediopolis
+shift /1
+goto parse
+)
+
+if "%1" EQU "r" (
+set PROJ=roiling
+shift /1
+goto parse
+)
+
+if "%1" EQU "ro" (
+set PROJ=roiling
+shift /1
+goto parse
+)
+
+if "%1" EQU "roi" (
+set PROJ=roiling
+shift /1
+goto parse
+)
+
+if "%1" EQU "15" (
+set PROJ=compound
+shift /1
+goto parse
+)
+
+if "%1" EQU "p" (
+set PROJ=compound
+shift /1
+goto parse
+)
+
+if "%1" EQU "c" (
+set PROJ=compound
+shift /1
+goto parse
+)
+
+if "%1" EQU "pc" (
+set PROJ=compound
+shift /1
+goto parse
+)
+
+if "%1" EQU "sb" (
+set PROJ=see-why-burg
+shift /1
+goto parse
+)
+
+if "%1" EQU "sc" (
+set PROJ=slicker-city
+shift /1
+goto parse
+)
+
+if "%1" EQU "16" (
+set PROJ=slicker-city
+shift /1
+goto parse
+)
+
+if "%1" EQU "17" (
+set PROJ=buck-the-past
+shift /1
+goto parse
+)
+
+if "%1" EQU "bp" (
+set PROJ=buck-the-past
+shift /1
+goto parse
+)
+
+if "%1" EQU "btp" (
+set PROJ=buck-the-past
+shift /1
+goto parse
+)
+
+if "%1" EQU "bs" (
+set PROJ=btp-st
+shift /1
+goto parse
+)
+
+if "%1" EQU "ss" (
+set PROJ=seeker-status
+shift /1
+goto parse
+)
+
+if "%1" EQU "bt" (
+set PROJ=buck-the-past
+shift /1
+goto parse
+)
+
+if "%1" EQU "i" (
+set PROJ=invis
+shift /1
+goto parse
+)
+
+if "%1" EQU "uo" (
+set PROJ=ugly-oafs
+shift /1
+goto parse
+)
+
+if "%1" EQU "o" (
+set PROJ=ugly-oafs
+shift /1
+goto parse
+)
+
+if "%1" EQU "oafs" (
+set PROJ=ugly-oafs
+shift /1
+goto parse
+)
+
+if "%1" EQU "14" (
+set PROJ=ugly-oafs
+shift /1
+goto parse
+)
+
+if "%1" EQU "s" (
+set PROJ=shuffling
+shift /1
+goto parse
+)
+
+if "%1" EQU "sa" (
+set PROJ=shuffling
+shift /1
+goto parse
+)
+
+if "%1" EQU "ux" (
+set PROJ=uxmulbrufyuz
+shift /1
+goto parse
+)
+
+if "%1" EQU "f" (
+set PROJ=fetch-quest
+shift /1
+goto parse
+)
+
+if "%1" EQU "fetch" (
+set PROJ=fetch-quest
+shift /1
+goto parse
+)
+
+if "%1" EQU "ge" (
+set PROJ=gems
+goto parse
+)
+
+if "%1" NEQ "" (
+set PROJ=%1
+)
+
+:nodef
 
 if "%2" EQU "t" (
 :story
 start "" "C:\Program Files (x86)\Notepad++\notepad++.exe" "c:\games\inform\%PROJ%%EXT%\source\story.ni"
 )
 
-if "%2" EQU "ta" (
-:tableread
+if "%TABLE%" EQU "1" (
 if "%PROJ%" EQU "buck-the-past" (
   start "" "C:\Program Files (x86)\Notepad++\notepad++.exe" "c:\Program Files (x86)\Inform 7\Inform7\Extensions\Andrew Schultz\Buck the Past tables.i7x"
   goto end
@@ -424,25 +500,24 @@ if "%PROJ%" EQU "slicker-city" (
   goto end
   )
 start "" "C:\Program Files (x86)\Notepad++\notepad++.exe" "c:\Program Files (x86)\Inform 7\Inform7\Extensions\Andrew Schultz\%PROJ% tables.i7x"
+)
+
+if "%OPENSOURCE%" EQU "1" (
+start "" "C:\Program Files (x86)\Notepad++\notepad++.exe" "c:\games\inform\%PROJ%.inform\source\story.ni"
 goto end
 )
 
-if "%2" EQU "ra" (
+if "%RANDTEXT%" EQU "1" (
 start "" "C:\Program Files (x86)\Notepad++\notepad++.exe" "c:\Program Files (x86)\Inform 7\Inform7\Extensions\Andrew Schultz\%PROJ% Random Text.i7x"
 goto end
 )
 
-if "%2" EQU "-" (
-start "" "C:\Program Files (x86)\Notepad++\notepad++.exe" "c:\Program Files (x86)\Inform 7\Inform7\Extensions\Andrew Schultz\%PROJ% Random Text.i7x"
-goto end
-)
-
-if "%2" EQU "nu" (
+if "%NUDGE%" EQU "1" (
 start "" "C:\Program Files (x86)\Notepad++\notepad++.exe" "c:\Program Files (x86)\Inform 7\Inform7\Extensions\Andrew Schultz\%PROJ% Nudges.i7x"
 goto end
 )
 
-if "%2" EQU "tc" (
+if "%TESTCASE%" EQU "1" (
 start "" "C:\Program Files (x86)\Notepad++\notepad++.exe" \games\inform\%PROJ%%EXT%\source\testcase.txt
 goto end
 )
