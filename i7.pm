8ce86d12e2fc91a8f13bf4f4dfa6ff0b509e5fc4
andrewschultz <schultz.andrew@sbcglobal.net>
2017-08-05 16:29:51 -0500

    removed gh.txt, added reverse hash for i7x

--- i7.pm
+++ i7.pm
@@ -20,21 +20,35 @@ $VERSION     = 1.00;
 #@EXPORT_OK   = qw(i7x);
 
 our %i7x = ( "12" => "shuffling",
-"sa" => "shuffling",
-"roi" => "roiling",
-"s13" => "roiling",
-"3" => "threediopolis",
-"13" => "threediopolis",
-"14" => "ugly-oafs",
-"oafs" => "ugly-oafs",
-"s15" => "dirk",
-"15" => "compound",
-"4" => "fourdiopolis",
-"s16" => "fourdiopolis",
-"16" => "slicker-city",
-"bs" => "btp-st",
-"s17" => "btp-st",
-"btp" => "buck-the-past"
+  "sa" => "shuffling",
+  "roi" => "roiling",
+  "s13" => "roiling",
+  "3" => "threediopolis",
+  "3d" => "threediopolis",
+  "13" => "threediopolis",
+  "14" => "ugly-oafs",
+  "oafs" => "ugly-oafs",
+  "s15" => "dirk",
+  "15" => "compound",
+  "pc" => "compound",
+  "4" => "fourdiopolis",
+  "4d" => "fourdiopolis",
+  "s16" => "fourdiopolis",
+  "16" => "slicker-city",
+  "bs" => "btp-st",
+  "s17" => "btp-st",
+  "btp" => "buck-the-past"
+);
+
+our %i7xr = ( "shuffling" => "sa",
+  "roiling" => "roi",
+  "threediopolis" => "3d",
+  "fourdiopolis" => "4d",
+  "ugly-oafs" => "uo",
+  "compound" => "pc",
+  "slicker-city" =>"sc" ,
+  "btp-st" =>"bs" ,
+  "btp" => "buck-the-past"
 );
 
 our %xtraFiles;
