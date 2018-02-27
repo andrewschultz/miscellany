c911588b8ffa5a749e843519a6cf978cc8304562
andrewschultz <schultz.andrew@sbcglobal.net>
2018-02-27 02:39:27 -0600

    added table sort checking, plus other rejects, as well as case insensitivity to talf.txt

--- talf.py
+++ talf.py
@@ -6,6 +6,7 @@
 
 from collections import defaultdict
 from shutil import copy
+from filecmp import cmp
 import os
 import sys
 import i7
@@ -14,6 +15,7 @@ import re
 ignore_sort = defaultdict(lambda:defaultdict(str))
 table_sort = defaultdict(lambda:defaultdict(str))
 default_sort = defaultdict(str)
+need_to_catch = defaultdict(lambda:defaultdict(str))
 
 onoff = ['off', 'on']
 
@@ -86,7 +88,11 @@ def read_table_and_default_file():
             if line.startswith('#'): continue
             if line.startswith(';'): break
             if '=' in line:
-                right_side = re.sub(".*=", "", line.strip())
+                if line != line.lower(): print("WARNING", table_default_line, "line", line_count, "has upper case letters but shouldn't.")
+                if '/' in line: print("WARNING", table_default_line, "line", line_count, "has forward slashes but needs backward slashes.")
+                right_side = re.sub(".*=", "", ll)
+                right_side = re.sub("\/", "\\", right_side)
+                right_side =
                 if ll.startswith("f="):
                     cur_file = right_side
                     continue
@@ -94,7 +100,11 @@ def read_table_and_default_file():
                     cur_file = right_side
                     continue
                 if ll.startswith("ignore="):
+                    if right_side in ignore_sort[cur_file].keys():
+                        print("BAILING double assignment of ignore for", right_side, "in", cur_file, "at line", line_count)
+                        exit()
                     ignore_sort[cur_file][right_side] = True
+                    need_to_catch[cur_file][right_side] = True
                     continue
                 if line.lower().startswith("default="):
                     if not cur_file:
@@ -110,8 +120,12 @@ def read_table_and_default_file():
                 exit()
             if ':' in line:
                 ary = ll.split(':')
+                if ary[0] in table_sort[cur_file].keys():
+                    print("BAILING double assignment of table sorting for", right_side, "in", cur_file, "at line", line_count)
+                    exit()
+                need_to_catch[cur_file][ary[0]] = True
                 table_sort[cur_file][ary[0]] = ary[1]
-                print(ary[0], "goes to", ary[1])
+                # print(ary[0], "goes to", ary[1])
             else:
                 print("Line", line_count, "needs :")
 
@@ -122,7 +136,7 @@ def ignorable(a, b, c):
     return False
 
 def table_alf_one_file(f, launch=False, copy_over=False):
-    print(default_sort)
+    cur_table = ''
     if f not in table_sort.keys() and f not in default_sort.keys():
         print(f, "has no table sort keys or default sorts. Returning.")
         return
@@ -151,14 +165,16 @@ def table_alf_one_file(f, launch=False, copy_over=False):
                     row_array.append(line.strip())
                 continue
             if not in_table and line.startswith('table'):
+                cur_table = line.strip()
                 if has_default:
-                    if ignorable(x, f, line):
-                        print("Ignoring default for table", x, "/", line)
+                    if ignorable(cur_table, f, line):
+                        print("Ignoring default for table", cur_table, ("/ " + line if x != line else ""))
                         temp_out.write(line)
+                        need_to_catch[f].pop(x)
                         continue
-                    cur_table = line.strip()
                     what_to_split = default_sort[f]
                     if cur_table in table_sort[f].keys():
+                        need_to_catch[f].pop(cur_table)
                         what_to_split = table_sort[f][cur_table]
                     what_to_sort = what_to_split.split(',')
                     temp_out.write(line)
@@ -179,17 +195,36 @@ def table_alf_one_file(f, launch=False, copy_over=False):
     temp_out.close()
     print("Done writing to", f2)
     if launch:
-        os.system("wm \"{:s}\" \"{:s}\"".format(f, f2))
+        if cmp(f, f2):
+            print("NO DIFFERENCE, NOT LAUNCHING DIFFERENCE")
+        else:
+            print("LAUNCHING DIFFERENCE:")
+            os.system("wm \"{:s}\" \"{:s}\"".format(f, f2))
+    forgot_to_catch = False
+    for x in need_to_catch.keys():
+        if len(need_to_catch[x]) > 0:
+            print(x, "had leftover table-sort key" + ('s' if len(need_to_catch[x]) > 1 else '') + ":", ','.join(sorted(need_to_catch[x].keys())))
+            forgot_to_catch = True
+        else:
+            print(x, "is okay")
+    if forgot_to_catch:
+        if override_omissions:
+            print("Ignoring unaccessed tables.")
+        else:
+            print("You need to sort out the unaccessed tables in talf.txt before I copy back over.")
+            exit()
     if copy_over:
-        if os.stat.getsize(f) != os.stat.getsize(f2):
+        if os.path.getsize(f) != os.path.getsize(f2):
             if override_source_size_differences:
                 print("Different sizes, but copying anyway.")
             else:
-                print(f, '=', os.stat.getsize(f), "bytes")
-                print(f2, '=', os.stat.getsize(f2), "bytes")
+                print(f, '=', os.path.getsize(f), "bytes")
+                print(f2, '=', os.path.getsize(f2), "bytes")
                 print('Use -os to ignore this size differences, but do verify no information was lost, first.')
                 exit()
         copy(f2, f)
+    elif not cmp(f, f2):
+        print("DIFFERENCES FOUND. Run with -c to copy back over.")
 
 count = 1
 projects = []
