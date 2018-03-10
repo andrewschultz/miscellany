a66b280b84735f6b0a74a633dbdd265c25624598
andrewschultz <schultz.andrew@sbcglobal.net>
2018-03-09 20:41:44 -0600

    added unit tests to llmod

--- llmod.py
+++ llmod.py
@@ -1,28 +1,59 @@
 # llmod.py: gets rid of lalaland (or other default room) references
 
 from filecmp import cmp
+import sys
 import os
 import re
 
 f1 = 'story.ni'
 f2 = 'story.ni2'
 
+def usage():
+    print("-u = unit tests")
+    print("-r = new default room")
+    exit()
+
 def mootify(a):
-	ll = re.sub(r"move (.*) to lalaland", "moot $1", a, 0, re.IGNORECASE)
-	ll = re.sub(r"now (.*) is in lalaland", "moot $1", ll, 0, re.IGNORECASE)
+	ll = re.sub(r"move (.*) to lalaland", r"moot \1", a, 0, re.IGNORECASE)
+	ll = re.sub(r"now (.*) is in lalaland", r"moot \1", ll, 0, re.IGNORECASE)
 	ll = re.sub("not in " + discard_room, "not moot", ll, 0, re.IGNORECASE)
 	ll = re.sub("in " + discard_room, "moot", ll, 0, re.IGNORECASE)
 	return ll
 
-fout = open(f2, "w", newline='\n')
+def u_trans(a):
+    print(a, "/", mootify(a))
 
+def unit_tests(bail = True):
+    print("Running unit tests...")
+    u_trans("now stuff is in lalaland;")
+    u_trans("move stuff to lalaland;")
+    u_trans("if th is in lalaland, yes; [ic]")
+    if bail: exit()
+    return
+
+count = 1
 discard_room = "lalaland"
-
-print(mootify("now stuff is in lalaland;"))
-print(mootify("move stuff to lalaland;"))
 
 difs = 0
 line_count = 0
+do_unit_tests = False
+
+while count < len(sys.argv):
+    arg = sys.argv[count].lower()
+    if arg[0] == '-':
+        arg = arg[1:]
+    if arg == 'u':
+        do_unit_tests = True
+    elif arg == 'r':
+        discard_room = sys.argv[count+1].lower()
+        count = count + 1
+    else:
+        usage()
+    count = count + 1
+
+fout = open(f2, "w", newline='\n')
+
+unit_tests(do_unit_tests)
 
 with open(f1) as file:
 	for line in file:
