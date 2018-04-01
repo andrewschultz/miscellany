b4d715bdcdd411092607a1b59193f37ec33232bf
andrewschultz <schultz.andrew@sbcglobal.net>
2018-03-31 19:20:22 -0500

    duptab changes

--- duptab.py
+++ duptab.py
@@ -2,39 +2,72 @@
 #
 # searches for duplicate-ish table entries
 #
+# todo: specify project
+# todo: allow write-over
 
 from collections import defaultdict
 import re
+import sys
 
 dup_yet = defaultdict(int)
+dup_no_space = defaultdict(str)
 t2d = defaultdict(int)
+
+# options
+check_spaceless = True
 
 table_file = 'c:\Program Files (x86)\Inform 7\Inform7\Extensions\Andrew Schultz\Ailihphilia Tables.i7x'
 
+def usage():
+    print("-ns/-sn removes spaceless checks e.g. No Ton ~ Not On")
+    print("-s allows spaceless checks (default)")
+    exit()
+
 dupes = 0
+dupe_without_spaces = 0
 in_table = False
 line_num = 0
 cur_table = "(none)"
 
+count = 1
+
+while count < len(sys.argv):
+    arg = sys.argv[count].lower()
+    if arg[0] == '-': arg = arg[1:]
+    if arg == 'ns' or arg == 'sn':
+        check_spaceless = False
+    elif arg == 's':
+        check_spaceless = True
+    else:
+        print("Bad argument", arg)
+        usage()
+    count = count + 1
+
 with open(table_file) as file:
-	for line in file:
-		line_num = line_num + 1
-		if line.startswith('table'):
-			in_table = True
-			cur_table = line.lower().strip()
-			continue
-		if not line.strip():
-			in_table = False
-			continue
-		if in_table:
-			ll = line.lower().strip().split("\t")
-			if not ll[0].startswith('"'): continue
-			l0 = re.sub("\"", "", ll[0])
-			l0 = re.sub("[^a-z ]", "", l0)
-			if l0 in dup_yet.keys():
-				print("Uh oh, line", line_num, "/", cur_table, "has", l0, "which duplicates line", dup_yet[l0], "/", t2d[l0])
-				dupes = dupes + 1
-			dup_yet[l0] = line_num
-			t2d[l0] = cur_table
+    for line in file:
+        line_num = line_num + 1
+        if line.startswith('table'):
+            in_table = True
+            cur_table = line.lower().strip()
+            continue
+        if not line.strip():
+            in_table = False
+            continue
+        if in_table:
+            ll = line.lower().strip().split("\t")
+            if not ll[0].startswith('"'): continue
+            l0 = re.sub("\"", "", ll[0])
+            l0 = re.sub("[^a-z ]", "", l0)
+            l1 = re.sub("[^a-z]", "", l0)
+            if l0 in dup_yet.keys():
+                print("Uh oh, line", line_num, "/", cur_table, "has", l0, "which duplicates line", dup_yet[l0], "/", t2d[l0])
+                dupes = dupes + 1
+            elif check_spaceless and l1 in dup_no_space.keys():
+                print("Dup-without-spaces", l1, l0, "from line", dup_no_space[l1])
+                dupe_without_spaces = dupe_without_spaces + 1
+            dup_no_space[l1] = "{:d}/{:s}".format(line_num, l0)
+            dup_yet[l0] = line_num
+            t2d[l0] = cur_table
 
 print(dupes, "total duplicates.")
+if dupe_without_spaces: print(dupe_without_spaces, "dupes without spaces e.g. Not On vs No Ton.")
