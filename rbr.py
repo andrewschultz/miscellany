a23443ce46901f5281c2546ce39214ce6e3581cd
andrewschultz <schultz.andrew@sbcglobal.net>
2018-04-30 23:40:04 -0500

    rbr now allows c/e arguments to open source and cfg file

--- rbr.py
+++ rbr.py
@@ -5,26 +5,44 @@ import i7
 from collections import defaultdict
 
 def_file = defaultdict(str)
+times = defaultdict(int)
 
 in_file = ""
 in_dir = os.getcwd()
 
-def get_file(x):
+def get_file(fname):
+    dupe_val = 1
+    last_cmd = ""
     file_list = []
     file_array = []
     line_count = 0
-    print("Poking at", x)
-    with open(x) as file:
+    dupe_file_name = ""
+    try:
+        dupe_file = open("hello.txt", "r")
+        dupe_file.close()
+    except:
+        pass
+    print("Poking at", fname)
+    with open(fname) as file:
         for line in file:
             line_count = line_count + 1
-            if len(file_array) == 0:
-                file_array = line.lower().strip().split(',')
+            if line.startswith("dupefile="):
+                dupe_file_name = re.sub(".*=", "", line.lower().strip())
+                dupe_file = open(dupe_file_name, "w")
+                continue
+            if line.startswith("files="):
+                file_array = re.sub(".*=", "", line.lower().strip()).split(',')
                 actives = [False] * len(file_array)
                 for x in file_array:
                     f = open(x, 'w')
                     file_list.append(f)
                 continue
             if len(actives) == 0: continue
+            if line.startswith(">"):
+                last_cmd = line.lower().strip()
+            if line.startswith("===a"):
+                actives = [True] * len(actives)
+                continue
             if line.startswith("==+"):
                 ll = line.lower().strip()[3:]
                 for x in ll.split(','):
@@ -45,13 +63,27 @@ def get_file(x):
             for ct in range(0, len(file_list)):
                 if actives[ct]:
                     file_list[ct].write(line)
+            if actives[dupe_val]:
+                dupe_file.write(line)
+                if 'by one point' in line:
+                    reps = 1
+                    if times[last_cmd[1:]] > 1: reps = times[last_cmd[1:]]
+                    for x in range(0, reps):
+                        dupe_file.write("\n" + last_cmd + "\n")
+                        dupe_file.write("/by one point\n")
     for ct in range(0, len(file_array)):
         file_list[ct].close()
-    print("Wrote files:", ', '.join(file_array), 'from', x)
+    dupe_file.close()
+    print("Wrote files:", ', '.join(file_array), 'from', fname)
 
 with open('c:/writing/scripts/rbr.txt') as file:
     for line in file:
         if line.startswith(';'): break
+        if line.lower().startswith('dupe'):
+            j = re.sub(r'^dupe([:=])?', "", line.strip().lower())
+            ja = j.split("\t")
+            times[ja[0]] = int(ja[1])
+            continue
         if line.lower().startswith('default'):
             j = re.sub(r'^default([:=])?', "", line.strip().lower())
             def_proj = j
@@ -69,7 +101,12 @@ count = 1
 
 while count < len(sys.argv):
     arg = sys.argv[count].lower()
-    print(arg, i7.i7x[arg])
+    if arg == 'c':
+        i7.open_source()
+        exit()
+    if arg == 'e':
+        os.system("rbr.txt")
+        exit()
     if arg in i7.i7x.keys():
         in_proj = i7.i7x[arg]
         in_file = i7.sdir(arg) + "/" + def_file[in_proj]
