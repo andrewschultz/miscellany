c8399fb6f1e1e4d3aa2f2a4ed93bb1604fcd3444
andrewschultz <schultz.andrew@sbcglobal.net>
2018-05-28 11:06:32 -0500

    m6 now does linear equations better

--- puzzles/m6.py
+++ puzzles/m6.py
@@ -7,12 +7,22 @@ x = numpy.zeros(shape=(4, 3))
 solv = []
 lc = 0
 dumbstr = ""
-my_col = 0
+my_col = -1
+
+horiz = []
+verts = []
 
 my_puz = "6" if len(sys.argv) == 1 else sys.argv[1]
 puz_str = "puz" + my_puz
 
 puzzled = False
+
+to_solve = []
+known_eq = []
+sols = []
+
+def abc_2_vars(x):
+    return [ x.count('a'), x.count('b'), x.count('c') ]
 
 with open("m6.txt") as file:
     for line in file:
@@ -20,19 +30,59 @@ with open("m6.txt") as file:
         if line.startswith(puz_str):
             puzzled = True
             continue
+        if puzzled and line.startswith('puz'): break
         if not puzzled: continue
-        if line.startswith("col"):
-            my_col = int(line[4:]) - 1
+        if lc == 4:
+            sols = line.strip().split(',')
             continue
-        temp = [line.count('a'), line.count('b'), line.count('c')]
-        dumbstr = dumbstr + line[my_col]
-        x[lc] = temp
-        solv.append(int(line[5:].strip()))
+        if lc > 5: sys.exit("You can only have 5 lines for the 4x4.")
+        temp = abc_2_vars(line)
+        horiz.append(line[:4].strip())
+        if '?' in line:
+            to_solve.append(temp)
+        else:
+            q = int(line[5:])
+            temp.append(q)
+            print("Adding", temp)
+            known_eq.append(temp)
         lc = lc + 1
-        if lc == 4: break
 
 if not puzzled: raise Exception("Couldn't find " + puz_str)
 if not puzzled: sys.exit("Didn't find " + puz_str)
+
+for a in range(0, 4):
+    vert = abc_2_vars([x[a] for x in horiz])
+    if sols[a].isdigit():
+        vert.append(int(sols[a]))
+        known_eq.append(vert)
+    else:
+        to_solve.append(vert)
+    print(vert)
+
+print ('to solve', to_solve)
+print ('known', known_eq)
+
+my_matrix = []
+
+solve_matrix = []
+
+for x in known_eq:
+    print(x)
+    solve_matrix.append(x)
+    if numpy.linalg.matrix_rank(numpy.row_stack(solve_matrix)) != len(solve_matrix):
+        solve_matrix.pop()
+        continue
+    print(numpy.linalg.matrix_rank(numpy.row_stack(solve_matrix)), len(solve_matrix), solve_matrix)
+
+vals = []
+for j in solve_matrix:
+    vals.append(j.pop())
+
+q = numpy.linalg.solve(solve_matrix, vals)
+print(q)
+for x in to_solve:
+    print(q, '*', x, '=', numpy.dot(q, x))
+exit()
 
 got_one = False
 
