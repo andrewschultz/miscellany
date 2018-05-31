6e47db84875143cad73719f50c4cabb9d3be228c
andrewschultz <schultz.andrew@sbcglobal.net>
2018-05-31 00:20:07 -0500

    added fractions to debug

--- puzzles/m14.py
+++ puzzles/m14.py
@@ -1,7 +1,7 @@
 import sys
 
+from fractions import Fraction
 from itertools import permutations
-from enum import Enum
 
 op_str = [ '+', '-', '*', '/' ]
 
@@ -32,17 +32,18 @@ class equat():
         self.adj_array = [self.val_array[x] for x in self.rotate]
     def print_eq(self): print(eq_str())
     def eq_str(self):
-        return ("seed {:d}: ({:s}) {:s}{:d} {:s} {:s}{:d}{:s} {:s} {:d}{:s} = {:f}".format(self.seed, str(self.val_array), '' if self.paren23 else '(', self.adj_array[0], op_str[self.op1], '(' if self.paren23 else '', self.adj_array[1], '' if self.paren23 else ')', op_str[self.op2], self.adj_array[2], ')' if self.paren23 else '', self.eval_expr()))
+        return ("seed {:d}: ({:s}) {:s}{:d} {:s} {:s}{:d}{:s} {:s} {:d}{:s} = {:s}".format(self.seed, str(self.val_array), '' if self.paren23 else '(', self.adj_array[0], op_str[self.op1], '(' if self.paren23 else '', self.adj_array[1], '' if self.paren23 else ')', op_str[self.op2], self.adj_array[2], ')' if self.paren23 else '', str(Fraction(self.eval_expr()).limit_denominator())
+        ))
         # print("{:s}{:d} {:s} {:s}{:d}{:s} {:s} {:d}{:s} = {:d}".format('' if self.paren23 else '(', self.val_array[0], op_str[self.op1], '' if not self.paren23 else '(', self.val_array[1], ')' if self.paren23 else '', op_str[self.op2], self.val_array[2]), ')' if self.paren23 else '', self.eval_expr())
     def eval_expr(self):
         try:
             if not self.paren23:
                 temp = shuf(self.adj_array[0], self.op1, self.adj_array[1])
-                return shuf(temp, self.op2, self.adj_array[2])
+                return float(shuf(temp, self.op2, self.adj_array[2]))
             temp = shuf(self.adj_array[1], self.op2, self.adj_array[2])
-            return shuf(self.adj_array[0], self.op1, temp)
+            return float(shuf(self.adj_array[0], self.op1, temp))
         except:
-            return 9999
+            return float(9999)
 
 while count < len(sys.argv):
     arg = sys.argv[count]
@@ -74,6 +75,7 @@ with open("m14.txt") as file:
 
 only_one = False
 got_one = False
+print_all = False
 
 for q in range(0,192):
     j = equat(q)
@@ -87,12 +89,15 @@ for q in range(0,192):
         cur_result = res
         print_str += j.eq_str() + "\n"
     if okay:
+        if not got_one: print("Got one!")
+        else: print("Got another!")
+        got_one = True
         j.make_new_vals(to_solve)
         print(print_str + j.eq_str())
-        got_one = True
         if only_one:
             exit()
+    elif print_all:
+        j.make_new_vals(to_solve)
+        print(print_str + j.eq_str())
 
 if not got_one: print("No patterns found. Rats.")
-
-exit()
