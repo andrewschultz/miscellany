f8808775baa0b0758c3e2e078f41ce02dc1af2c9
andrewschultz <schultz.andrew@sbcglobal.net>
2018-08-09 19:38:43 -0500

    first swipe at seeing differences

--- python/lanv.py
+++ python/lanv.py
@@ -5,14 +5,49 @@
 # usage: (no arguments yet, just looks at story.ni)
 
 import i7
+import re
+from collections import defaultdict
 
 c = 0
+
+ignore_verbs = defaultdict(int)
+lv_entries = defaultdict(int)
+understand_entries = defaultdict(int)
 
 lasts = [ '' ] * 6
 cur_nfr = 0
 cur_lev = 0
 
-with open("story.ni") as file:
+proj_name = "ailihphilia"
+file_name = "story.ni"
+ignore_file = "c:/writing/scripts/lanv.txt"
+
+def read_language_verb(f):
+    got_lv_yet = False
+    with open(f) as file:
+        for (line_count, line) in enumerate(file, 1):
+            if '[ LanguageVerb i;' in line: got_lv_yet = True
+            if not got_lv_yet: continue
+            if 'after "Language.i6t".' in line: break
+            if "'" in line:
+                j = re.compile("'([a-z]+)[\\\/]*'")
+                for q in j.findall(line):
+                    if q in lv_entries.keys(): print("WARNING", q, "appears in", lv_entries[q], "and is repeated at", line_count)
+                    else: lv_entries[q] = line_count
+                print(j.findall(line))
+    if not got_lv_yet: sys.exit("{:s} has no LanguageVerb replacement function. Bailing.")
+    return
+
+def read_ignore_file():
+    with open(ignore_file) as file:
+        for (line_count, line) in enumerate(file, 1):
+            if line.startswith(';'): break
+            if line.startswith('#'): next
+
+read_language_verb(file_name)
+read_ignore_file()
+
+with open(file_name) as file:
     for (line_count, line) in enumerate(file, 1):
         inl = i7.new_lev(line)
         if inl:
@@ -29,5 +64,17 @@ with open("story.ni") as file:
         if cur_lev > cur_nfr:
             if line.startswith("understand") and 'as something new' in line:
                 c += 1
-                print(cur_lev, cur_nfr, c, outline_str, line_count, line.strip())
+                j = re.compile('"([a-z ]+)"')
+                for q in j.findall(line):
+                    if q in understand_entries.keys(): print("WARNING", q, "appears in", understand_entries[q], "and is repeated at", line_count)
+                    else: understand_entries[q] = line_count
+                #print(cur_lev, cur_nfr, c, outline_str, line_count, line.strip())
                 continue
+
+x = sorted(list(set(understand_entries.keys()).union(set(lv_entries.keys())).difference(set(ignore_verbs.keys()))))
+
+for y in x:
+    if y not in lv_entries.keys(): print(y, understand_entries[y], "needs LV entry or needs to be in the ignore file.")
+
+for y in x:
+    if y not in understand_entries.keys(): print(y, lv_entries[y], "needs UNDERSTAND/AS line or needs to be in the ignore file.")
