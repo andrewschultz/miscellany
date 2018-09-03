2f4c97419eb21d1858fda06bfbbce0a906e827f2
andrewschultz <schultz.andrew@sbcglobal.net>
2018-09-02 21:42:51 -0500

    genc.py now tracks list of strings

--- i7/py/genc.py
+++ i7/py/genc.py
@@ -11,8 +11,9 @@ M_GENDER = 1
 F_GENDER = 2
 
 genders = NO_GENDER
+ai_gen = "lug,lover,nob"
 default_gender_string = "lug"
-gender_string = ""
+gender_strings = []
 flags = re.IGNORECASE
 
 count = 0
@@ -24,19 +25,20 @@ while acount < len(sys.argv):
     if arg == 'm': genders = M_GENDER
     elif arg == 'n': genders = NO_GENDER
     elif arg == 'f': genders = F_GENDER
-    elif gender_string:
-        sys.exit("Tried to define 2 search strings.")
-    else:
-        gender_string = arg
+    elif arg == 'ai': gender_strings = ai_gen.split(",")
+    elif len(gender_strings): sys.exit("Tried to define 2 search strings.")
+    else: gender_strings = re.sub("-", " ", arg).split(",")
     acount += 1
 
-if not gender_string:
+if not gender_strings:
     print("Going with default string", default_gender_string)
-    gender_string = default_gender_string
+    gender_strings = [ default_gender_string ]
+
+grex = r"\b({:s})\b".format("|".join(gender_strings))
 
 with open("story.ni") as file:
     for (line_count, line) in enumerate(file, 1):
-        if re.search(r'\b{:s}\b'.format(gender_string), line, re.IGNORECASE):
+        if re.search(grex, line, re.IGNORECASE):
             line = re.sub("\[[^\]*]\]", "", line)
             if genders == NO_GENDER and re.search(r'\b(he|him|his|her|hers|she)\b', line.lower(), re.IGNORECASE):
                 count += 1
@@ -48,4 +50,4 @@ with open("story.ni") as file:
                 count += 1
                 print(count, line_count, line.strip())
 
-if not count: print("Found nothing for", gender_string)
+if not count: print("Found nothing for", grex)
