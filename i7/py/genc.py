5527d71958c3325a65da25f59e5fd9aaf0bcb7b4
andrewschultz <schultz.andrew@sbcglobal.net>
2018-09-05 23:59:45 -0500

    more general genc.py

--- i7/py/genc.py
+++ i7/py/genc.py
@@ -2,52 +2,81 @@
 #
 # cursory check for obvious gender switches/mistakes in code
 #
+# usage
+#   genc.py ai
+#   genc.py ailihphilia
 
+from collections import defaultdict
+import i7
 import sys
 import re
 
-NO_GENDER = 0
-M_GENDER = 1
-F_GENDER = 2
 
-genders = NO_GENDER
-ai_gen = "lug,lover,nob"
-default_gender_string = "lug"
-gender_strings = []
+gender_file = "c:/writing/scripts/genc.txt"
+
+genregx = defaultdict(str)
+
+MALE=0
+FEMALE=1
+BOTH=2
+
+genregx[BOTH] = r'\b(he|him|his|her|hers|she)\b'
+genregx[MALE] = r'\b(her|hers|she)\b'
+genregx[FEMALE] = r'\b(him|his|he)\b'
+
+default_gender_proj = "ailihphilia"
+gender_dict = defaultdict(lambda: defaultdict(str))
 flags = re.IGNORECASE
+
+gender_strings = ""
 
 count = 0
 acount = 1
 
+gender_tests = defaultdict(str)
+
+def to_num(a):
+    al = a.lower()
+    if al == 'm': return MALE
+    if al == 'f': return BOTH
+    if al == 'n' or al == 'b': return FEMALE
+    sys.exit(a + " is not recognized. Try m n f or b.")
+
+with open(gender_file) as file:
+    for (line_count, line) in enumerate(file, 1):
+        if line.startswith('#'): continue
+        if line.startswith(';'): break
+        try:
+            l0 = line.lower().strip().split(":")
+            l1 = l0[1].split("=")
+            gender_dict[l1[0]][to_num(l0[0])] = l1[1]
+            print(l1[0], to_num(l0[0]), l1[1])
+        except:
+            print(line_count, line)
+            sys.exit("Need something of the form m:ailihphilia=a,b,c")
+
 while acount < len(sys.argv):
     arg = sys.argv[acount].lower()
     if arg[0] == '-': arg = arg[1:]
-    if arg == 'm': genders = M_GENDER
-    elif arg == 'n': genders = NO_GENDER
-    elif arg == 'f': genders = F_GENDER
-    elif arg == 'ai': gender_strings = ai_gen.split(",")
+    if arg in gender_tests.keys():
+        pass
+        # gender_strings = gender_tests[arg].split(",")
+    elif i7.proj_exp(arg) in gender_tests.keys(): current_project = i7.proj_exp(arg)
     elif len(gender_strings): sys.exit("Tried to define 2 search strings.")
     else: gender_strings = re.sub("-", " ", arg).split(",")
     acount += 1
 
-if not gender_strings:
-    print("Going with default string", default_gender_string)
-    gender_strings = [ default_gender_string ]
-
-grex = r"\b({:s})\b".format("|".join(gender_strings))
+if not gender_dict.keys():
+    sys.exit("No gender dict for " + default_gender_string)
 
 with open("story.ni") as file:
     for (line_count, line) in enumerate(file, 1):
-        if re.search(grex, line, re.IGNORECASE):
-            line = re.sub("\[[^\]*]\]", "", line)
-            if genders == NO_GENDER and re.search(r'\b(he|him|his|her|hers|she)\b', line.lower(), re.IGNORECASE):
-                count += 1
-                print(count, line_count, line.strip())
-            if genders == M_GENDER and re.search(r'\b(her|hers|she)\b', line.lower(), re.IGNORECASE):
-                count += 1
-                print(count, line_count, line.strip())
-            if genders == F_GENDER and re.search(r'\b(he|him|his)\b', line.lower(), re.IGNORECASE):
-                count += 1
-                print(count, line_count, line.strip())
+        line = re.sub("\[[^\]]*\]", "/", line.lower().strip())
+        for q in [MALE, FEMALE, BOTH]:
+            if q in gender_dict[default_gender_proj].keys():
+                if re.search(genregx[q], line, re.IGNORECASE) and re.search(gender_dict[default_gender_proj][q], line):
+                    count += 1
+                    print(q, genregx[q], gender_dict[default_gender_proj][q])
+                    print(count, line_count, line.strip())
 
-if not count: print("Found nothing for", grex)
+if not count: print("Found nothing for", default_gender_proj)
