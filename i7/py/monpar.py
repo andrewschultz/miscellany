94d4d59f435aeb37429c178fe7af916404fbaa90
andrewschultz <schultz.andrew@sbcglobal.net>
2018-09-06 22:44:18 -0500

    added clipboard ability for monpar.py

--- i7/py/monpar.py
+++ i7/py/monpar.py
@@ -1,39 +1,66 @@
+#
+# monpar.py: monty-parse
+#
+# file name or -c for clipboard
+#
 
 from difflib import Differ
+import pyperclip
 import sys
 import re
-
-file_name = sys.argv[1]
 
 last_test_text = []
 cur_test_text = []
 
 in_test_text = False
+read_file = True
+
+file_name = ""
+count = 1
+
+while count < len(sys.argv):
+    arg = sys.argv[count]
+    if arg[0] == '-': arg = arg[1:]
+    if arg == 'c': read_file = False
+    else: file_name = sys.argv[1]
+    count += 1
 
 d = Differ()
 
-with open(file_name) as file:
-    for (line_count, line) in enumerate(file, 1):
-        if line.startswith(">"):
-            if not last_test_text and not cur_test_text:
-                print(line.strip(), "(no test text before)")
-                continue
-            if not cur_test_text:
-                print(line.strip(), "No current test. Maybe this is a test command or an out of world action.")
-                continue
-            elif cur_test_text == last_test_text:
-                print("No change in test text.")
-            else:
-                print("Changed text.")
-                print("\n".join([x.strip() for x in list(d.compare(last_test_text, cur_test_text)) if re.search("^[\+\?-]", x)]))
-            last_test_text = list(cur_test_text)
-            cur_test_text = []
-            print(line.strip())
-        if "=START TESTS" in line:
-            in_test_text = True
+if read_file:
+    if not file_name: sys.exit("Need file name.")
+    f = open(file_name, "r")
+    rl = [ q.strip() for q in f.readlines() ]
+    print("Reading", file_name, len(rl), "lines")
+else:
+    f1 = pyperclip.paste()
+    rl = re.split("(\r?)\n", f1)
+    print("Reading from clipboard", len(rl), "lines")
+
+#print(rl[5])
+#exit()
+
+for line in rl:
+    if line.startswith(">"):
+        if not last_test_text and not cur_test_text:
+            print(line, "(no test text before)")
             continue
-        if "=END TESTS" in line:
-            in_test_text = False
+        if not cur_test_text:
+            print(line, "No current test. Maybe this is a test command or an out of world action.")
             continue
-        if in_test_text: cur_test_text.append(line)
+        elif cur_test_text == last_test_text:
+            print("No change in test text.")
+        else:
+            print("Changed text.")
+            print("\n".join([x for x in list(d.compare(last_test_text, cur_test_text)) if re.search("^[\+\?-]", x)]))
+        last_test_text = list(cur_test_text)
+        cur_test_text = []
+        print(line)
+    if "=START TESTS" in line:
+        in_test_text = True
+        continue
+    if "=END TESTS" in line:
+        in_test_text = False
+        continue
+    if in_test_text: cur_test_text.append(line)
 
