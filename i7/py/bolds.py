8e7aeb747a2a833840e0ff448e40255c78eaa36b
andrewschultz <schultz.andrew@sbcglobal.net>
2018-10-07 23:57:53 -0500

    in quotes check to bolds.py + readability tweaks

--- i7/py/bolds.py
+++ i7/py/bolds.py
@@ -1,8 +1,15 @@
+# bolds.py
+# looks for misplaced bold in story.ni locally
+# todo: see about opening at first err line
+
 import re
+import i7
+import os
+import sys
 
 from collections import defaultdict
 
-caps = [ "THINK", "TIP IT", "DEVED", "DEV ED", "SMH MS", "GUY UG", "TOOLS LOOT", "NI WIN", "N I WIN", "META", "SHUTTUHS", "LO VOL", "LOVE VOL", "DEEP SPEED", "REV OVER", "ROT", "REI", "REV", "MM", "GRAMMAR G" ]
+caps = [ "THINK", "TIP IT", "DEVED", "DEV ED", "SMH MS", "GUY UG", "TOOLS LOOT", "NI WIN", "N I WIN", "META", "SHUTTUHS", "LO VOL", "LOVE VOL", "DEEP SPEED", "REV OVER", "ROT", "REI", "REV", "MM", "GRAMMAR G", "VERB", "VERBS", "ABOUT" ]
 
 caps_par = "|".join(caps)
 
@@ -36,7 +43,9 @@ def sophisticated():
     countall = 0
     with open("story.ni") as file:
         for (line_count, line) in enumerate (file, 1):
-            q = re.findall(r"(?<!(\[b\]))\b({:s})\b(?!(\[r\]))".format(caps_par), line)
+            l2 = i7.in_quotes(line)
+            #print(l2.rstrip())
+            q = re.findall(r"(?<!(\[b\]))\b({:s})\b(?!(\[r\]))".format(caps_par), l2)
             if q:
                 if skipit(line): continue
                 count += 1
@@ -47,7 +56,7 @@ def sophisticated():
     if count == 0: print("NO ERRORS! Yay!")
     else:
         for q in sorted(finerr.keys(), key=lambda x: (len(finerr[x]), finerr[x])):
-            print(q, finerr[q])
+            print("{:10s}".format(q), finerr[q])
 
 def find_caps():
     capfind = defaultdict(int)
@@ -57,7 +66,8 @@ def find_caps():
     with open("story.ni") as file:
         for (line_count, line) in enumerate (file, 1):
             if skipit(line): continue
-            q = re.findall(r"([A-Z])([A-Z ]*[A-Z])".format(caps_par), line)
+            l2 = i7.in_quotes(line)
+            q = re.findall(r"([A-Z])([A-Z ]*[A-Z])".format(caps_par), l2)
             for l in q: capfind[l[0]+l[1]] += 1
     for q in sorted(capfind.keys(), key=capfind.get):
         print(q, capfind[q])
@@ -76,6 +86,9 @@ def check_bold_italic():
                 imbalances += 1
     if imbalances: print(imbalances, "imbalances")
     else: print("No bold/italic/regular imbalances found.")
+
+if not os.path.exists("story.ni"): sys.exit("Need a directory with story.ni.")
+
 #find_caps()
 #bruteforce()
 sophisticated()
