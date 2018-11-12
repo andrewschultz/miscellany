57836a0218a981f42880984953a8d3abd036d59c
andrewschultz <schultz.andrew@sbcglobal.net>
2018-11-12 05:25:21 -0600

    i7h is now smarter. It has a default project and checks the current dir.

--- i7/py/i7h.py
+++ i7/py/i7h.py
@@ -7,15 +7,35 @@ import os
 import sys
 import i7
 
+usage_str = "You need a header file or abbreviation, or you need a project/header file combination."
+
 my_stuff = sys.argv[1:]
 
-if my_stuff[1] in i7.i7x.keys() and my_stuff[0] in i7.i7hxf.keys():
-    print("Going header-project instead of project-header")
-    my_stuff.reverse()
+sl = len(my_stuff)
 
-hdr = re.sub("-", " ", i7.proj_exp(sys.argv[1]))
+if sl == 0: sys.exit(usage_str)
 
-to_open = "{:s}\\{:s} {:s}.i7x".format(i7.extdir, hdr.title(), i7.th_exp(sys.argv[2]))
+if len(my_stuff) == 1:
+    x = i7.dir2proj(os.getcwd())
+    if x: print("You're in project {:s}, so we'll use that to look for header files.".format(x))
+    else:
+        x = i7.curdef
+        print("Going with default project", x)
+    my_stuff = [x] + my_stuff
+elif len(my_stuff) == 2:
+    if my_stuff[1] in i7.i7x.keys() and my_stuff[0] in i7.i7hfx.keys():
+        print("Going header-project instead of project-header")
+        my_stuff.reverse()
+else:
+    print("Too many arguments.")
+    sys.exit(usage_str)
+
+if not i7.proj_exp(my_stuff[0]): sys.exit("{:s} is not a valid project or abbreviation.".format(my_stuff[0]))
+if my_stuff[1] not in i7.i7hfx.keys() and my_stuff[1] not in i7.i7hfx.values(): sys.exit("{:s} is not a valid header file name or abbreviation.".format(my_stuff[1]))
+
+hdr = re.sub("-", " ", i7.proj_exp(my_stuff[0]))
+
+to_open = "{:s}\\{:s} {:s}.i7x".format(i7.extdir, hdr.title(), i7.th_exp(my_stuff[1]))
 
 if os.path.exists(to_open):
     print("Opening", to_open)
