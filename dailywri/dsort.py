cab5d16b6952bbf5bb66b0ab91053a5d99fa4b70
andrewschultz <schultz.andrew@sbcglobal.net>
2018-11-29 18:26:36 -0600

    added double space checking option -bc to dsort.py

--- dailywri/dsort.py
+++ dailywri/dsort.py
@@ -42,12 +42,54 @@ stderr_warnings = False
 to_temp_only = True
 return_after_first_bug = False
 
-def show_section_add():
+blank_counter = False
+
+def show_section_add(): # only used for debugging
     to = sorted(text_out.keys())
     for q in to:
         print(q, "====")
         print(text_out[q] + "===")
     print("Sections added:", ", ".join(to))
+
+def search_for_spaces(my_f):
+    if 'hthws' in my_f: return
+    if 'sb.otl' in my_f: return
+    last_blank = 0
+    my_f_full = to_temp(my_f, base_dir)
+    cur_section = ""
+    skip_next = False
+    with open(my_f_full) as file:
+        for (line_count, line) in enumerate(file, 1):
+            # print(line_count, q, line[:30], sep="-/-")
+            if line.startswith("[") and line.rstrip().endswith("]"):
+                print("Ignoring comment at line", line_count, "in", my_f_full)
+                skip_next = True
+                continue
+            if skip_next:
+                skip_next = False
+                continue
+            if not line.strip():
+                if not cur_section and line_count - last_blank > 1:
+                    print("Extra blank line at", line_count, "file", my_f_full)
+                last_blank = line_count
+                cur_section = ""
+                continue
+            q = space_section(line)
+            if q:
+                if cur_section: print("Double defined section at", line_count, "file", my_f_full)
+                # else: print("New section", q, "at", line_count)
+                cur_section = q
+
+def space_section(x):
+    x1 = x.rstrip()
+    if x1.startswith("\\"):
+        return re.sub("=.*", "", x1[1:].lower())
+    if x1.startswith("H=") or x.startswith("h="):
+        temp = re.sub("^.*?\"", "", x)
+        return re.sub("\"", "", temp)
+    if x1.startswith("= V4"):
+        return "Intro-outline"
+    return ""
 
 def usage():
     print("U# = upper bound")
@@ -111,6 +153,7 @@ def read_hash_file():
         print("Couldn't find a FILES: in {:s}.".format(idea_hash))
         print("SUGGESTION:")
         sys.exit("FILES:{:s}".format(','.join(sorted(stuff.keys()))))
+    return file_list
 
 def compare_hash_with_files():
     with open(idea_hash) as file:
@@ -165,10 +208,14 @@ while count < len(sys.argv):
     if arg.startswith("-"): arg = arg[1:]
     if arg == 'wm': wm_diff = True
     elif arg == 'nw' or arg == 'wn': wm_diff = False
-    if arg.startswith('u'):
+    elif arg == 'bc':
+        blank_counter = True
+    elif arg.startswith('u'):
         upper_bound = arg[1:]
-    if len(upper_bound) < 8: upper_bound += '9' * (8 - len(upper_bound))
-    elif arg.startswith('l'): lower_bound = arg[1:]
+        if len(upper_bound) < 8: upper_bound += '9' * (8 - len(upper_bound))
+    elif arg.startswith('l'):
+        lower_bound = arg[1:]
+        if len(lower_bound) < 8: upper_bound += '0' * (8 - len(upper_bound))
     elif arg.startswith('tf'):
         try:
             total_files = int(arg[2:])
@@ -192,10 +239,14 @@ if days_back_start or days_back_end:
 
 if lower_bound > upper_bound: sys.exit("Lower bound > upper bound")
 
+my_files = read_hash_file()
+
+if blank_counter:
+    for mf in my_files: search_for_spaces(mf)
+    exit()
+
 os.chdir(daily_dir)
 readdir = [x for x in os.listdir(daily_dir) if os.path.isfile(x)]
-
-read_hash_file()
 
 daily_files_processed = 0
 
