5208ef60e3557d0c9d1920d4bb964c1046a74c27
andrewschultz <schultz.andrew@sbcglobal.net>
2018-11-29 18:26:19 -0600

    dsort.py first successful runthrough to temp

--- dailywri/dsort.py
+++ dailywri/dsort.py
@@ -3,6 +3,7 @@
 # python extension of former perl dsort updater
 #
 
+import i7
 import sys
 import re
 import os
@@ -15,20 +16,23 @@ file_name = defaultdict(str)
 file_comment = defaultdict(lambda:defaultdict(str))
 cfg_line = defaultdict(lambda:defaultdict(str))
 modified_files = defaultdict(bool)
-done_with = defaultdict(bool)
+daily_done_with = defaultdict(bool)
 or_dict = defaultdict(str)
 text_out = defaultdict(str)
 
 need_tabs = defaultdict(bool)
 
-default_section_name = "not"
+default_section_name = "ide"
 
 #def sort_backslash_sections()
+base_dir = "c:/writing"
 daily_dir = "c:/writing/daily"
 done_dir = "c:/writing/daily/done"
 temp_dir = "c:/writing/temp"
 backup_dir = "c:/writing/backup"
 idea_hash = "c:/writing/idea-tab.txt"
+
+wm_diff = False
 
 copy_over = True
 to_temp_only = True
@@ -38,11 +42,19 @@ stderr_warnings = False
 to_temp_only = True
 return_after_first_bug = False
 
+def show_section_add():
+    to = sorted(text_out.keys())
+    for q in to:
+        print(q, "====")
+        print(text_out[q] + "===")
+    print("Sections added:", ", ".join(to))
+
 def usage():
     print("U# = upper bound")
     print("L# = lower bound")
     print("B/D# = days back")
     print("Numbers are right after the letters, with no spaces.")
+    print("WM = show WinMerge differences, WN/NW = turn it off, default = {:s}".format(i7.on_off[wm_diff]))
     print("? = this")
     exit()
 
@@ -134,10 +146,11 @@ def get_stuff_from_one_file(x):
                     # print(line_count, section_name, ll, sep="-/-")
             loc_text_out[section_name] += line
     for y in loc_text_out.keys():
+        if not file_name[y]: sys.exit("Could not find file name for section {:s}.".format(y))
         modified_files[file_name[y]] = True
         if y in need_tabs.keys(): loc_text_out[y] = "\t" + loc_text_out[x]
         text_out[y] += loc_text_out[y]
-    done_with[x] = True
+    daily_done_with[x] = True
     print("Got stuff from", x)
 
 lower_bound = "20170101.txt"
@@ -150,6 +163,8 @@ total_files = 0
 while count < len(sys.argv):
     arg = sys.argv[count]
     if arg.startswith("-"): arg = arg[1:]
+    if arg == 'wm': wm_diff = True
+    elif arg == 'nw' or arg == 'wn': wm_diff = False
     if arg.startswith('u'):
         upper_bound = arg[1:]
     if len(upper_bound) < 8: upper_bound += '9' * (8 - len(upper_bound))
@@ -198,28 +213,44 @@ for file in readdir:
 
 if not daily_files_processed: sys.exit("Nothing processed. I am copying nothing over.")
 
-sys.exit(copy_over)
+show_section_add()
 
 if copy_over: # maybe we should put this into a function
+    cur_out = ""
     last_backslash = 0
-    for x in modified_files:
-        f = to_temp(x)
-        with open(x) as file:
+    for x in modified_files.keys():
+        x2 = to_temp(x, base_dir)
+        x3 = to_temp(x, temp_dir)
+        if not os.path.exists(x2):
+            print("Could not find path for", x, "/", x2)
+            exit()
+        with open(x2) as file:
+            f = open(x3, "w")
             for (line_count, line) in enumerate(file, 1):
                 if line.startswith("\\"):
-                    if cur_out: sys.exit("Ruh roh two backslashes in one section {:d} {:d} in file {:s}.".format(line_count, last_backslash, x))
+                    if cur_out: sys.exit("Ruh roh two backslashes without a line break curline={:d}/{:s} {:d} in file {:s}.".format(line_count, line.strip(), last_backslash, x))
                     cur_out = re.sub("=.*", "", line[1:].strip())
                     last_backslash = line_count
                     f.write(line)
                     continue
-                if cur_out and not line.strip() and cur_out in text_out.keys():
-                    f.write(text_out[cur_out])
-                    print("Added {:d} lines to section {:s} in file {:s}.".format(text_out[cur_out].count("\n"), cur_out, x))
-    if not to_temp_only:
-        for x in changed_files.keys():
+                if cur_out:
+                    if not line.strip():
+                        if cur_out in text_out.keys():
+                            crs = text_out[cur_out].count("\n")
+                            f.write(text_out[cur_out])
+                            print("Added {:d} line{:s} to section {:s} ({:d}-{:d}) in file {:s}.".format(crs, i7.plur(crs), cur_out, last_backslash, line_count, x))
+                        cur_out = ""
+                f.write(line)
+    if to_temp_only:
+        print("Look in", temp_dir, "for", '/'.join(modified_files.keys()))
+    else:
+        for x in changed_out_files.keys():
+            if wm_diff:
+                os.system("wm \"{:s}\" \"{:s]\"")
             the_temp = to_temp(x)
-            copy(the_temp, x)
+            print(the_temp, x)
+            # copy(the_temp, x)
             os.remove(the_temp)
-    for j in done_with.keys():
-            os.move(j, to_temp(j))
-
+        for j in daily_done_with.keys():
+                print("Move", j, to_temp(j))
+                # os.move(j, to_temp(j))
