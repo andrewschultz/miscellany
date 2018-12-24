73d779c6686e257523c1d2a7261d149af4b3e614
andrewschultz <schultz.andrew@sbcglobal.net>
2018-12-23 23:57:00 -0600

    added l flag for GIT LOG after running logm.py to change commit date

--- python/logm.py
+++ python/logm.py
@@ -15,18 +15,24 @@ def usage(arg = ""):
     if arg: print("Invalid command", arg)
     else: print("USAGE")
     print("=" * 50)
-    print("r or x executes the command")
+    print("r or x executes the command, l at the end runs git log too")
     print("a number specifies the days back to look.")
 
 
 days = 0
 count = 1
 run_cmd = False
+run_log = False
 
 while count < len(sys.argv):
     arg = sys.argv[count].lower()
     if arg[0] == '-': arg = arg[1:]
-    if arg == 'r' or arg == 'x': run_cmd = True
+    if re.search("^[rxl]+", arg):
+        run_cmd = 'r' in arg or 'x' in arg
+        cmd_counts = arg.count('r') + arg.count('x')
+        run_log = 'l' in arg
+        if cmd_counts == 0: print("WARNING an L without an R or X means nothing.")
+        elif cmd_counts > 1: print("WARNING extra r/x in the argument to run the command mean nothing.")
     elif arg.isdigit(): days = int(arg)
     elif arg == '?': usage()
     else: usage()
@@ -59,5 +65,8 @@ print(out_string)
 
 if run_cmd:
     os.system(out_string)
+    if run_log:
+        time.sleep(4)
+        os.system("git log")
 else:
     print("Use -r to run.")
