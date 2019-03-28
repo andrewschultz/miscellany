7b4a7e2bdeb73b1cc67973344ebbdf336d002095
andrewschultz <schultz.andrew@sbcglobal.net>
2019-03-28 01:57:00 -0500

    dgrab.py now can edit cfg file warnings or open cfg file

--- dgrab.py
+++ dgrab.py
@@ -13,6 +13,11 @@ default_sect = ""
 my_sect = ""
 default_by_dir = i7.dir2proj(to_abbrev = True)
 
+dg_cfg = "c:/writing/scripts/dgrab.txt"
+flat_cfg = os.path.basename(dg_cfg)
+dg_temp = "c:/writing/temp/dgrab-temp.txt"
+flat_temp = os.path.basename(dg_temp)
+
 mapping = defaultdict(str)
 regex_to = defaultdict(str)
 notes_to_open = defaultdict(int)
@@ -31,6 +36,7 @@ def usage(header="GENERAL USAGE"):
     print("# = maximum number of files to process")
     print("d/db(#) = days before to ignore")
     print("o = open notes after, no/on = don't")
+    print("e = edit cfg file")
     exit()
 
 def file_len(fname):
@@ -43,7 +49,6 @@ def send_mapping(sect_name, file_name, change_files = False):
     temp_time = os.stat(file_name)
     fn = os.path.basename(file_name)
     time_delta = time.time() - temp_time.st_ctime
-    dgtemp = "c:/writing/temp/dgrab-temp.txt"
     my_reg = regex_to[sect_name]
     found_sect_name = False
     in_sect = False
@@ -95,25 +100,13 @@ def send_mapping(sect_name, file_name, change_files = False):
 # start main program
 #
 
-cmd_count = 1
-while cmd_count < len(sys.argv):
-    arg = sys.argv[cmd_count].lower()
-    if arg[0] == '-': arg = arg[1:]
-    if arg.isdigit(): max_process = arg
-    elif re.search("^d(b)?[0-9]+$", arg):
-        temp = re.sub("^d(b)?", "", arg)
-        days_before_ignore = int(temp)
-    elif arg == 'd' or arg == 'db': days_before_ignore = 0
-    elif arg == 'dt' or arg == 't': max_process = -1
-    elif arg == 'o': open_notes_after = True
-    elif arg == 'no' or arg == 'on': open_notes_after = False
-    elif arg == '?': usage()
-    else: usage("BAD PARAMETER {:s}".format(sys.argv[cmd_count]))
-    cmd_count += 1
+#
+# need to read CFG file first as defaults may be in there
+#
 
-os.chdir("c:/writing/scripts")
+cfg_edit_line = 0
 
-with open("dgrab.txt") as file:
+with open(dg_cfg) as file:
     for (line_count, line) in enumerate(file, 1):
         if line.startswith("#"): continue
         if line.startswith(";"): break
@@ -125,13 +118,53 @@ with open("dgrab.txt") as file:
                 mapping[q] = lary[1]
                 regex_to[q] = my_regex
         elif line.startswith("DEFAULT="): default_sect = lary[0]
-        else: print("Unrecognized command line", line_count, line.strip())
+        elif line.startswith("OPENONWARN="):
+            open_on_warn = int(lary[0])
+        elif line.startswith("MAXPROC="):
+            try:
+                max_process = int(lary[0])
+                if max_process < -1:
+                    print("Setting default max_process to -1. Maybe fix line", line_count)
+                    max_process = -1
+            except:
+                print("MAXPROC= went wrong at line", line_count)
+                cfg_edit_line = line_count
+        else:
+            print("Unrecognized command line", line_count, line.strip())
+            cfg_edit_line = line_count
+
+if cfg_edit_line:
+    if not open_on_warn:
+        for q in sys.argv:
+            if q == 'e' or q == '-e': open_on_warn = True
+    if open_on_warn: i7.npo(dg_cfg, cfg_edit_line)
+    else: print("Put in an OPENONWARN in {:s} to open the CFG file, or type e/-e on the command line.")
+
+cmd_count = 1
+while cmd_count < len(sys.argv):
+    arg = sys.argv[cmd_count].lower()
+    if arg[0] == '-': arg = arg[1:]
+    if arg.isdigit(): max_process = arg
+    elif re.search("^d(b)?[0-9]+$", arg):
+        temp = re.sub("^d(b)?", "", arg)
+        days_before_ignore = int(temp)
+    elif arg == 'd' or arg == 'db': days_before_ignore = 0
+    elif arg == 'dt' or arg == 't': max_process = -1
+    elif arg == 'e':
+        os.system(dg_cfg)
+        exit()
+    elif arg == 'o': open_notes_after = True
+    elif arg == 'no' or arg == 'on': open_notes_after = False
+    elif arg == '?': usage()
+    else:
+        usage("BAD PARAMETER {:s}".format(sys.argv[cmd_count]))
+    cmd_count += 1
 
 x = glob.glob("c:/writing/daily/20*.txt")
 
 if not my_sect:
     if not default_by_dir or default_by_dir not in mapping:
-        print("Going with default section defined in dgrab.txt: {:s}.{:s}".format(default_sect, " {:s} is defined by the PWD but is not in the dgrab.txt mapping.".format(default_by_dir) if default_by_dir else ""))
+        print("Going with default section defined in {:s}: {:s}.{:s}".format(flat_cfg, default_sect, (" {:s} is defined by the PWD but is not in the {:s} mapping.".format(default_by_dir, flat_cfg) if default_by_dir else "")))
         my_sect = default_sect
     else:
         print("Going with default section defined by directory you're in {:s}.".format(default_by_dir))
