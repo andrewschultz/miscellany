314a9d733dda961fadcd60f8fce85fbef697fffb
andrewschultz <schultz.andrew@sbcglobal.net>
2019-06-24 23:35:38 -0500

    running on AI and other directories turned up tweaks. Also, rigorous checking flag

--- i7/py/isvis.py
+++ i7/py/isvis.py
@@ -17,31 +17,41 @@ max_vis = 0
 
 ary = []
 
+check_rigorous = True
+
 def usage():
-    print("USAGE: m# for max # of visibles, or write in a project.")
+    print("USAGE:")
+    print("m# for max # of visibles")
+    print("rc/cr/r/ry/yr turns on rigor check, currently", i7.oo[check_rigorous])
+    print("nr/rn turns it off")
+    print("Or write in a project.")
     exit()
 
 def viscap(x):
     return re.sub(r'\bvisible\b', "VISIBLE", x)
 
-def num_valid_visibles(l):
+def num_valid_visibles(l, debug = False):
     q = ' '.join(re.findall(r'\[[^\]]*\]', l))
     syn = ' '.join(l.split('"')[::2])
-    print(q, q.count('visible'), "quotes")
-    print(syn, syn.count('visible'), "syntax")
+    if debug:
+        print(q, q.count('visible'), "quotes")
+        print(syn, syn.count('visible'), "syntax")
     return q.count('visible') + syn.count('visible')
 
 def ignore_vis(l):
     if "[v]" in l: return True
     if "[v:]" in l: return True #I can do this manually
     if l.startswith("["): return True
-    if l.endswith("]") and not "[" in l: return True
-    if 'applying to' in l and ('one visible thing' in l) or ('two visible things' in l): return True #actions definitions are okay
+    if l.endswith("]"):
+        if not "[" in l: return True
+        ls = re.sub(r'( *\[([^\]])*\])+', '', l.strip())
+        if 'visible' not in ls: return True
+    if 'applying to' in l and (('one visible thing' in l) or ('two visible things' in l)): return True #actions definitions are okay
 
 def find_vis(proj_name):
     visibles = 0
     count_idx = 0
-    ignored = 0
+    ignored = []
     x = i7.main_src(proj_name)
     if not os.path.exists(x):
         print("Bad project name/abbreviation", proj_name)
@@ -54,22 +64,31 @@ def find_vis(proj_name):
             ll = line.lower().strip()
             if 'visible' in ll and re.search(r'\bvisible\b', ll, re.IGNORECASE):
                 if ignore_vis(ll):
-                    ignored += 1
+                    ignored.append(line_count)
                     continue
-                tv = num_valid_visibles(ll)
+                if check_rigorous:
+                    tv = num_valid_visibles(ll)
+                else:
+                    tv = len(re.findall(r'\bvisible\b', ll, re.IGNORECASE))
                 if not tv: continue
-                # OLD: tv = len(re.findall(r'\bvisible\b', ll, re.IGNORECASE))
                 visibles += tv
                 count_idx += 1
                 print("Line", line_count, "Incidence", count_idx)
                 print('    l{:s}'.format("" if tv == 1 else " +{:d}".format(tv)), visibles, 'tot', last_rule if line.startswith("\t") else "", viscap(line.lower().strip())) #ask codereview about a better way to do this?
-    print(count_idx, "total instances of visible for story.ni in project", proj_name)
-    if ignored: print(ignored, "instances of VISIBLE ignored in project", proj_name)
+    if not count_idx:
+        print('HOORAY! No instances of "visible" to check.' + '' if check_rigorous else ' Use -nr for nonrigorous to pick up more bugs')
+    else:
+        print(count_idx, "total instance{:s} of visible for story.ni in project".format(i7.plur(count_idx)), proj_name)
+    if len(ignored): print(len(ignored), "instance{:s} of VISIBLE ignored in project".format(i7.plur(ignored)), proj_name, ", ".join([str(x) for x in ignored]))
 
 cmd_count = 1
 while cmd_count < len(sys.argv):
     arg = sys.argv[cmd_count]
-    if arg[0] == 'm' and arg[1:].isdigit(): max_vis = int(arg[1:])
+    if arg == 'r' or arg == 'rc' or arg == 'cr' or arg == 'ry' or arg == 'yr':
+        check_rigorous = True
+    elif arg == 'rn' or arg == 'nr' or arg == 'n':
+        check_rigorous = False
+    elif arg[0] == 'm' and arg[1:].isdigit(): max_vis = int(arg[1:])
     elif i7.proj_exp(arg, return_nonblank = False):
         if i7.proj_exp(arg, return_nonblank = False) in ary:
             print("Duplicate entry", arg, "at position", cmd_count)
@@ -84,7 +103,9 @@ cwp = i7.dir2proj(os.getcwd())
 
 if cwp and not len(ary):
     by_default = True
-    if os.path.exists(os.path.join(cwp, "story.ni")):
+    story_path = os.path.join(i7.proj2dir(cwp), "story.ni")
+    print("Verifying", story_path)
+    if os.path.exists(story_path):
         ary = [ i7.dir2proj(os.getcwd()) ]
     else:
         sys.exit("You're in a potentially valid default project directory, but its games/inform directory has no story.ni. Bailing.")
