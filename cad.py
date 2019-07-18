16c85c98bf4ffcefbae745adef07877de644c75f
andrewschultz <schultz.andrew@sbcglobal.net>
2019-07-18 12:24:55 -0500

    cad.py now allows for pendulum/nightly/naive time checking

--- cad.py
+++ cad.py
@@ -3,10 +3,16 @@ import re
 import sys
 import time
 import winsound
+import pendulum
 
 from urllib.request import Request, urlopen
 
+use_pendulum = True
 log_file = "ca-todo.htm"
+
+auto_launch = False
+ignore_download = False
+cmd_count = 0
 
 def launch_html_note(err_str, beep_freq, beep_dur):
     f = open(log_file, "w")
@@ -16,27 +22,53 @@ def launch_html_note(err_str, beep_freq, beep_dur):
     winsound.Beep(beep_freq, beep_dur)
     os.system(log_file)
 
-def see_if_days_match(dy):
+def process_day_deltas(actual_days, expected_days, extra_seconds):
+    print('should have', expected_days, 'days in a row')
+    print("Current streak =", actual_days, "days with", extra_seconds, "extra seconds")
+    if expected_days > actual_days: launch_html_note("Need something today", 500, 1500)
+    elif expected_days == actual_days:
+        print("Everything is square{0}!".format(", but launching to make sure" if auto_launch else ""))
+        if auto_launch:
+            launch_html_note("All square", 1000, 500)
+    else: launch_html_note("Oops! You did too much, somehow", 2000, 500)
+    exit()
+
+def see_if_days_match_pendulum(dy):
+    this_day = pendulum.now()
+    naive = this_day.naive()
+    start_day = pendulum.from_format("2017-08-19 00:00", "YYYY-MM-DD HH:SS").naive()
+    day_period = naive - start_day
+    day_streak_expected = day_period.days
+    extra_seconds = day_period.seconds
+    process_day_deltas(dy, day_streak_expected, extra_seconds)
+    exit()
+
+def see_if_days_match_gmt(dy):
     streak_start = 1503187200 - 86400 # 2017-08-19 19:00:00
     eptime = int(time.time())
     days = (eptime - streak_start) // 86400
     rem = (eptime - streak_start) % 86400
-    print('should have', days, 'days in a row', 'extra seconds', rem, 'of 86400')
-    print("Current streak =", dy, "days")
-    if days > dy: launch_html_note("Need something today", 500, 1500)
-    elif days == dy and auto_launch: launch_html_note("All square", 1000, 500)
-    else: launch_html_note("Oops! You did too much, somehow", 2000, 500)
+    process_day_deltas(dy, days, extra_seconds)
     exit()
 
-auto_launch = False
-ignore_download = False
-cmd_count = 0
+def see_if_days_match_generic(dy):
+    if use_pendulum:
+        see_if_days_match_pendulum(dy)
+    else:
+        see_if_days_match_gmt(dy)
+
 while cmd_count < len(sys.argv):
     arg = sys.argv[cmd_count].lower()
     if arg == 'i':
         ignore_download = True
     elif arg == 'a':
         auto_launch = True
+    elif arg == 'p' or arg == 'pe' or arg == 'pen':
+        use_pendulum = True
+    elif arg == 'g' or arg == 'gm' or arg == 'gmt' or arg == 'np' or arg == 'pn' or arg == 'npe':
+        use_pendulum = False
+    elif arg.isdigit() and int(arg) > 100:
+        see_if_days_match_generic(int(arg))
     cmd_count += 1
 
 doc_file = 'c:\coding\perl\proj\doc.html'
@@ -61,5 +93,5 @@ for li in lines:
         q = re.sub("<\/.*", "", q)
         q = re.sub(".*>", "", q)
         dy = int(q)
-        see_if_days_match(dy)
+        see_if_days_match_generic(dy)
     last = li
