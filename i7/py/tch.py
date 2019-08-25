dd37432b21c548f4a92cffdea13bff7705534956
andrewschultz <schultz.andrew@sbcglobal.net>
2019-08-24 23:57:00 -0500

    tch.py can now detect/switch projects and open first error

--- i7/py/tch.py
+++ i7/py/tch.py
@@ -5,12 +5,16 @@
 # makes sure tabs are consistent
 #
 
+import sys
 import re
 import i7
+import mytools as mt
 
 show_short_columns = False
 
-project = 'roi'
+project = ''
+default_project = 'roi'
+dir_project = i7.dir2proj()
 
 def tab_sort(q):
     get_tabs = False
@@ -43,17 +47,38 @@ def tab_sort(q):
                     print(err_count, "Culprit:", line.strip())
                     print("END TABS REMOVED:", l2)
                     columns = 0
+                    mt.add_postopen_file_line(q, line_count)
                 elif ll < columns and show_short_columns:
                     err_count += 1
                     print(q, my_table, line_count, "has", ll, "columns, should have", columns)
                     print(err_count, "Culprit:", line.strip())
+                    mt.add_postopen_file_line(q, line_count)
+
+cmd_count = 1
+
+while cmd_count < len(sys.argv):
+    arg = sys.argv[cmd_count]
+    if arg in i7.i7x.keys():
+        if project: sys.exit("Tried to define 2 projects at once.")
+        project = i7.i7x[arg]
+        if not project: sys.exit("No such project/shortcut {}.".format(project))
+    cmd_count += 1
 
 if project in i7.i7x.keys():
     print(project, "=>", i7.i7x[project])
     project = i7.i7x[project]
+
+if not project:
+    if dir_project:
+        print("Current dir project", dir_project)
+        project = dir_project
+    elif default_project:
+        print("Default project", default_project)
 
 if project in i7.i7f.keys():
     for q in i7.i7f[project]:
         tab_sort(q)
 else:
     tab_sort(i7.src(project))
+
+mt.postopen_files()
