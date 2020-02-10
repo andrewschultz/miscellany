460e9d63c54c2be6c2e3c020a9ffc98c04eedfd0
andrewschultz <schultz.andrew@sbcglobal.net>
2020-02-09 23:57:00 -0600

    rewriting how to send alerts in habqs

--- hab/habqs.py
+++ hab/habqs.py
@@ -5,6 +5,11 @@
 # option to print what is pasted to the clipboard (pp/npp)
 # put our_zones into the config file
 
+bye_message = '''Hello, looks like you missed 4 quests in a row. That's the minimum participation level for the group, so I've removed you. But don't worry, you can join back up if you want! I think we all understand life can (and should) get in the way of fun small web games. And if Habitica isn't working for you any more, that's okay, too--I hope you find something that has!
+
+So, if you've moved on for good, thanks for all your participation in the group, and happy trails in whatever you do, in Habitica or life!
+'''
+
 import re
 from collections import defaultdict
 import pyperclip
@@ -14,27 +19,29 @@ import os
 
 launch_html = False
 
-write_to_quest_file = False
-
 quest_hours = quest_default_hours = 24
+preview_hours = 2
 
 timezone_list = defaultdict(list)
 
 quest_details = defaultdict(str)
 quest_nag = "c:/scripts/start-quest.htm"
 
+# these are the 3 main options
 overwrite_html = False
 reset_task = False
+write_to_quest_file = False
 
 # it's okay to have duplicate-ish time zones below.
 our_zones = [ 'America/Chicago', 'America/New_York', 'America/Denver', 'America/Los_Angeles', 'Europe/Sofia', 'Australia/Sydney', "America/El_Salvador" ]
 
 def usage():
-    print("The main thing to know is, -f turns full options on, which is useful for setting a quest.")
     print("Options include w / nw / wn to write to a quest file or not. The default is not to write to it.")
     print("You can also enter a quest name e.g. cow. The program will try to match anything over 1 character long.")
     print("x / nx / xn = execute the command or set up a task")
     print("l launches the task command")
+    print()
+    print("The main thing to know is, -f turns full options on, which is useful for setting a quest.")
     exit()
 
 def extract_copy():
@@ -59,6 +66,25 @@ def slash_join(ary):
 def hab_format(tm, tz):
     temp = tm.in_timezone(tz)
     return temp.format("dddd MMMM DD HH:mm (h:mm A)")
+
+def task_sched(sched_time, sched_name, reset_task, rewrite_html = False, run_the_command = False, verbose = False):
+    alert_date = sched_time.format("MM/DD/YYYY")
+    alert_time = sched_time.format("HH:mm")
+    task_set_cmd = 'schtasks /f /create /sc ONCE /tn {} /tr "c:\\scripts\\start-quest.htm" /st {} /sd {}'.format(sched_name, alert_time, alert_date)
+    print("Task set command =", task_set_cmd)
+    if reset_task:
+        print("Setting with schtasks...")
+        os.system(task_set_cmd)
+    else:
+        print("Not setting with schtasks... use t/tr/rt.")
+    if not overwrite_html: return
+    if rewrite_html:
+        f = open(quest_nag, "w")
+        f.write("<html>\n<title>\nStart quest nag</title>\n<body>\n<center>\nSTART QUEST NAG AT {} {}</center>\n</body>\n</html>\n".format(alert_time, alert_date))
+        f.close()
+        print("Rewrote", quest_nag)
+    else:
+        print("Did not overwrite html. Do so with -w or with -f = full go")
 
 os.chdir("c:/scripts")
 
@@ -105,12 +131,21 @@ while cmd_count < len(sys.argv):
     elif arg == 'xc':
         extract_copy()
         exit()
+    elif arg == 'bye':
+        pyperclip.copy(bye_message)
+        print("Copied goodbye message to clipboard.")
+        exit()
     elif arg == 't':
         reset_task = True
     elif arg == 'nt' or arg == 'tn':
         reset_task = False
     elif arg == 'lh':
         launch_html = True
+        exit()
+    elif arg == 'ft' or arg == 'tf':
+        print("Forcing task ... ")
+        os.system("schtasks /run /tn hman")
+        # schtasks /query /tn hman"
         exit()
     elif arg == '?':
         usage()
@@ -138,6 +173,7 @@ paste_string = ""
 
 my_time = pendulum.now()
 quest_start_time = my_time.add(hours=quest_hours)
+quest_warn_time = my_time.add(hours=quest_hours - preview_hours)
 
 for x in our_zones:
     try:
@@ -152,7 +188,7 @@ for x in our_zones:
     timezone_list[y].append(x)
 
 for x in poss_match:
-    paste_string += "Habitica {} quest ({} pet) invites sent {}. Quest starts by time zone:\n".format(x, quest_details[x], hab_format(my_time, our_zones[0]))
+    paste_string += "Habitica {} quest ({} pet) invites sent {}. Quest starts by time zone:\n".format(quest_details[x], x, hab_format(my_time, our_zones[0]))
 
 for q in sorted(timezone_list): # the sorting is so times will be sorted from the international date line (top) east around the globe. This seems like the best way to do things, though it doesn't feel perfect.
     paste_string += "* {} for the {} timezone{}\n".format(q, slash_join(timezone_list[q]), "" if len(timezone_list[q]) == 1 else "s")
@@ -162,18 +198,8 @@ pyperclip.copy(paste_string)
 print("PASTED===================")
 print(paste_string)
 
-alert_date = quest_start_time.format("MM/DD/YYYY")
-alert_time = quest_start_time.format("HH:mm")
-
-my_cmd = 'schtasks /f /create /sc ONCE /tn hman /tr "c:\\scripts\\start-quest.htm" /st {} /sd {}'.format(alert_time, alert_date)
-
-if overwrite_html:
-    f = open(quest_nag, "w")
-    f.write("<html>\n<title>\nStart quest nag</title>\n<body>\n<center>\nSTART QUEST NAG AT {} {}</center>\n</body>\n</html>\n".format(alert_time, alert_date))
-    f.close()
-    print("Rewrote", quest_nag)
-else:
-    print("Did not overwrite html. Do so with -w or with -f = full go")
+task_sched(quest_start_time, "hman", reset_task, rewrite_html = True, verbose = True)
+task_sched(quest_warn_time, "hmanp", reset_task, rewrite_html = False, verbose = False)
 
 if launch_html:
     os.system(quest_nag)
@@ -182,9 +208,5 @@ if write_to_quest_file:
     with open("c:/scripts/habqlog.txt", "a") as file:
         file.write(paste_string)
 
-print(my_cmd)
-if reset_task:
-    print("Running task-set command.")
-    os.system(my_cmd)
-else:
-    print("You didn't task-set the command. Do so with -t or with -f = full go.")
+if not (write_to_quest_file and overwrite_html and reset_task):
+    print("Remember, -f does the works: rewrite HTML file, put to log file, and set a task.")
