4185069c3e90d03c46008a2d35e3d38fc37a87ca
Andrew <blurglecruncheon@gmail.com>
2020-07-28 23:57:00 -0500

    twid.py can now process separate projects, default is hard coded

--- dailywri/twid.py
+++ dailywri/twid.py
@@ -14,18 +14,20 @@ overall_comparisons = True
 
 section_text = defaultdict(str)
 
-regex_pattern = defaultdict(str)
-from_file = defaultdict(str)
-to_file = defaultdict(str)
-priority = defaultdict(int)
+regex_pattern = defaultdict(lambda:defaultdict(str))
+from_file = defaultdict(lambda:defaultdict(str))
+to_file = defaultdict(lambda:defaultdict(str))
+priority = defaultdict(lambda:defaultdict(int))
 
-from_temp = defaultdict(str)
-to_temp = defaultdict(str)
+from_temp = defaultdict(lambda:defaultdict(str))
+to_temp = defaultdict(lambda:defaultdict(str))
 
 my_twiddle_config = "c:/writing/scripts/twid.txt"
 my_twiddle_dir = "c:/writing/twiddle"
 
 from_and_to = []
+
+my_project = "spo"
 
 def usage(arg = "general usage"):
     print(arg)
@@ -36,25 +38,32 @@ def usage(arg = "general usage"):
     exit()
 
 def get_twiddle_mappings():
+    current_project = ""
     with open(my_twiddle_config) as file:
         for (line_count, line) in enumerate (file, 1):
             if line.startswith(";"): break
             if line.startswith("#"): continue
+            if line.startswith("project:"):
+                current_project = re.sub("^.*?:", "", line.strip())
+                continue
+            if not current_project:
+                sys.exit("Need current project defined.")
             ary = line.strip().split(",")
-            priority[ary[1]] = int(ary[0])
-            from_file[ary[1]] = ary[2]
-            to_file[ary[1]] = ary[3]
-            regex_pattern[ary[1]] = ary[4]
+            priority[current_project][ary[1]] = int(ary[0])
+            from_file[current_project][ary[1]] = ary[2]
+            to_file[current_project][ary[1]] = ary[3]
+            regex_pattern[current_project][ary[1]] = ary[4]
     global from_and_to
-    from_and_to = list(set(from_file.values()) | set(to_file.values()))
-    for q in from_and_to:
-        poss_temp = os.path.basename(q)
-        dupe_index = 1
-        while poss_temp in from_temp:
-            poss_temp = "{}-{}".format(dupe_index, os.path.basename(q))
-            dupe_index += 1
-        to_temp[q] = poss_temp
-        from_temp[poss_temp] = q
+    for proj in priority:
+        from_and_to = list(set(from_file[proj].values()) | set(to_file[proj].values()))
+        for q in from_and_to:
+            poss_temp = os.path.basename(q)
+            dupe_index = 1
+            while poss_temp in from_temp[proj]:
+                poss_temp = "{}-{}".format(dupe_index, os.path.basename(q))
+                dupe_index += 1
+            to_temp[proj][q] = poss_temp
+            from_temp[proj][poss_temp] = q
 
 def twiddle_of(my_file):
     return os.path.join(my_twiddle_dir, os.path.basename(my_file))
@@ -62,7 +71,7 @@ def twiddle_of(my_file):
 def write_out_files(my_file):
     in_section = False
     current_section = ""
-    twiddle_file = twiddle_of(to_temp[my_file])
+    twiddle_file = twiddle_of(to_temp[my_project][my_file])
     f = open(twiddle_file, "w")
     with open(my_file) as file:
         for (line_count, line) in enumerate (file, 1):
@@ -92,8 +101,8 @@ def write_out_files(my_file):
         mt.compare_alphabetized_lines(my_file, twiddle_file)
 
 def pattern_check(my_line):
-    for x in sorted(regex_pattern, key=lambda x: (-priority[x])):
-        if re.search(regex_pattern[x], my_line, re.IGNORECASE):
+    for x in sorted(regex_pattern[my_project], key=lambda x: (-priority[my_project][x])):
+        if re.search(regex_pattern[my_project][x], my_line, re.IGNORECASE):
             return x
     return ""
 
@@ -125,7 +134,7 @@ while cmd_count < len(sys.argv):
 
 get_twiddle_mappings()
 
-for x in to_temp.values():
+for x in to_temp[my_project]:
     with open(x) as file:
         current_section = ""
         for (line_count, line) in enumerate (file, 1):
@@ -142,19 +151,19 @@ for x in to_temp.values():
                 section_text[current_section] += line
                 continue
             section_text['blank'] += line
-            for q in regex_pattern:
-                if re.search(regex_pattern[q], line):
+            for q in regex_pattern[my_project]:
+                if re.search(regex_pattern[my_project][q], line):
                     section_text[q] += line
-                    print(line, "matches with", q, "pattern", regex_pattern[q])
+                    print(line, "matches with", q, "pattern", regex_pattern[my_project][q])
 
 print("FROM AND TO:", from_and_to)
 
 for x in from_and_to:
     write_out_files(x)
-    print("TWIDDLY", twiddle_of(to_temp[x]), x)
+    print("TWIDDLY", twiddle_of(to_temp[my_project][x]), x)
     if print_stats:
         print("Orig:", os.stat(x).st_size, x)
-        print("New:", os.stat(twiddle_of(to_temp[x])).st_size, twiddle_of(to_temp[x]))
+        print("New:", os.stat(twiddle_of(to_temp[my_project][x])).st_size, twiddle_of(to_temp[my_project][x]))
 
 if not copy_over:
     sys.exit("-co to copy over")
@@ -162,7 +171,7 @@ if not copy_over:
 changed = unchanged = 0
 
 for x in from_and_to:
-    twid_from = twiddle_of(to_temp[x])
+    twid_from = twiddle_of(to_temp[my_project][x])
     if cmp(x, twid_from):
         print("Skipping", x, twid_from, "no changes.")
         unchanged += 1
