d2b9119bedac564b99031b26a457410c9153b70a
Andrew <blurglecruncheon@gmail.com>
2021-02-11 23:57:00 -0600

    change zups(proj_candidate) to shorthand curzip in zup.py

--- zup.py
+++ zup.py
@@ -4,8 +4,7 @@
 # zip up project utility, to bring all files into a zip file
 #
 # todo:
-# 1 zups[proj] -> something equivalent?
-# 2 switch statement in CFG reader
+# 2 switch statement in CFG reader (if possible)
 
 import re
 import mytools as mt
@@ -26,7 +25,7 @@ class zip_project:
         self.version = 1
         self.max_specific_file_size = defaultdict(int)
         self.min_specific_file_size = defaultdict(int)
-        self.out_name = '{}.zip'.format(name)
+        self.out_name = ''
         self.command_buffer = []
         self.dropbox_location = ''
         self.size_compare = defaultdict(tuple)
@@ -61,7 +60,7 @@ def read_zup_txt():
                 continue
             if line.startswith(">>"):
                 print("Deprecated >> should be converted to cmd: at line", line_count)
-                zups[proj_candidate].command_buffer.append(line[2:].strip())
+                curzip.command_buffer.append(line[2:].strip())
                 continue
             try:
                 (prefix, data) = mt.cfg_data_split(line.strip())
@@ -73,29 +72,29 @@ def read_zup_txt():
             # keep the below alphabetized
 
             if prefix == 'cmd':
-                zups[proj_candidate].command_buffer.append(data)
+                curzip.command_buffer.append(data)
                 continue
             elif prefix == 'dl':
-                zups[proj_candidate].dropbox_location = data
+                curzip.dropbox_location = data
             elif prefix == 'f':
                 file_array = data.split("\t")
                 if len(file_array) == 1:
-                    zups[proj_candidate].file_map[file_array[0]] = os.path.basename(file_array[0])
+                    curzip.file_map[file_array[0]] = os.path.basename(file_array[0])
                 elif len(file_array) == 2:
-                    zups[proj_candidate].file_map[file_array[0]] = file_array[1]
+                    curzip.file_map[file_array[0]] = file_array[1]
                 else:
                     print("Badly split file line at {} has {} entr(y/ies).".format(line_count, len(file_array)))
                 current_file = file_array[0]
             elif prefix == 'min':
                 if current_file:
-                    zups[proj_candidate].min_specific_file_size[current_file] = int(data)
+                    curzip.min_specific_file_size[current_file] = int(data)
                 else:
-                    zups[proj_candidate].min_zip_size = int(data)
+                    curzip.min_zip_size = int(data)
             elif prefix == 'max':
                 if current_file:
-                    zups[proj_candidate].max_specific_file_size[current_file] = int(data)
+                    curzip.max_specific_file_size[current_file] = int(data)
                 else:
-                    zups[proj_candidate].max_zip_size = int(data)
+                    curzip.max_zip_size = int(data)
             elif prefix == 'proj' or prefix == 'projx':
                 accept_alt_proj_name = (prefix == 'projx')
                 if cur_zip_proj:
@@ -111,16 +110,17 @@ def read_zup_txt():
                     flag_cfg_error(line_count, "BAILING redefining zip project at line {} with {}/{}.".format(line_count, proj_read_in, proj_candidate))
                 else:
                     zups[proj_candidate] = zip_project(proj_candidate)
+                    print("Switching project to", proj_candidate)
+                    curzip = zups[proj_candidate]
             elif prefix == 'time':
                 time_array = re.split("[<>]", data)
                 if len(time_array) != 2:
                     print("Bad timing line {} needs exactly one < or >.".format(line_count))
                     continue
                 if '>' in data:
-                    zups[proj_candidate].time_compare.append((time_array[0], time_array[1]))
+                    curzip.time_compare.append((time_array[0], time_array[1]))
                 else:
-                    zups[proj_candidate].time_compare.append((time_array[1], time_array[0]))
-                zups[proj_candidate] = zip_project(proj_candidate)
+                    curzip.time_compare.append((time_array[1], time_array[0]))
             elif prefix == 'v':
                 zups[proj_candidate].version = int(data)
             else:
