7963789f26bcdaf66b996495471628609dd41fcd
Andrew <blurglecruncheon@gmail.com>
2021-04-26 22:49:23 -0500

    gq.py can now read continuous user input

--- gq.py
+++ gq.py
@@ -28,6 +28,7 @@ my_cfg = "c:/writing/scripts/gqcfg.txt"
 
 # options only on cmd line
 
+user_input = False
 fast_match = True
 view_history = False
 post_open_matches = False
@@ -51,11 +52,11 @@ create_new_history = False
 
 # variables not in CFG file/cmd line
 
+first_loop = True
 found_overall = 0
 
 frequencies = defaultdict(int)
 
-cmd_count = 1
 match_string_array = []
 default_from_cwd = i7.dir2proj()
 default_from_cfg =  ""
@@ -234,76 +235,97 @@ def related_projects(my_proj):
     except:
         return [my_proj]
 
+def read_args(my_arg_array):
+    cmd_count = 0
+    global verbose
+    global user_input
+    global post_open_matches
+    global all_similar_projects
+    global max_in_file
+    global max_overall
+    global view_history
+    global verbose
+    global quote_status
+    global create_new_history
+    global modify_line
+    global include_notes
+    global fast_match
+    global match_string_array
+    global my_proj
+    while cmd_count < len(my_arg_array):
+        arg = mt.nohy(my_arg_array[cmd_count])
+        arg_orig = my_arg_array[cmd_count]
+        if arg_orig in i7.i7x:
+            my_proj = i7.i7x[arg_orig]
+        elif arg_orig in i7.i7xr: # this is because we may have a dash- flag going to the same as a project name, so let's have a way to look at any project
+            my_proj = arg_orig
+        elif arg == 'r':
+            print("Using super-shortcut 'r' for A Roiling Original.")
+            my_proj = "roiling"
+        elif arg == 'npo' or arg == 'pon':
+            post_open_matches = False
+        elif arg == 'po':
+            post_open_matches = True
+        elif arg == 'o':
+            all_similar_projects = False
+        elif arg == 'a':
+            all_similar_projects = True
+        elif arg[:2] == 'mf' and arg[2:].isdigit():
+            max_in_file = int(arg[2:])
+        elif arg[:2] == 'mo' and arg[2:].isdigit():
+            max_overall = int(arg[2:])
+        elif arg == 'vh':
+            view_history = True
+        elif arg == 'v':
+            verbose = True
+        elif arg == 'q':
+            verbose = False
+        elif arg == 'qo' or arg == 'oq':
+            quote_status = OUTSIDE
+        elif arg == 'qi' or arg == 'iq':
+            quote_status = INSIDE
+        elif arg == 'qa' or arg == 'aq':
+            quote_status = ALL
+        elif arg == 'newhist':
+            create_new_history = True
+        elif arg == 'e' or arg == 'ec' or arg == 'ce':
+            mt.npo(my_cfg)
+        elif arg == 'ml' or arg == 'lm':
+            modify_line = True
+        elif sorted(arg) == 'lmn': #no modify line
+            modify_line = False
+        elif arg[:2] == 'mn':
+            try:
+                user_specified_matches_needed = int(arg[2:])
+                if user_specified_matches_needed < 1:
+                    sys.exit("-mn must define a positive number.")
+            except:
+                print("mn needs a valid number after it.")
+        elif arg == 'ny' or arg == 'yn':
+            include_notes = True
+        elif arg == 'nn':
+            include_notes = False
+        elif arg == 'fm':
+            fast_match = True
+        elif arg == 'nfm' or arg == 'fmn':
+            fast_match = False
+        elif arg == 'ui':
+            user_input = True
+        elif arg == '?':
+            usage()
+        else:
+            if verbose:
+                print("Adding searchable string", arg)
+            arg = arg.replace('/', '[^a-z]*')
+            arg = arg.replace('`', '')
+            match_string_array.append(arg)
+        cmd_count += 1
+
 ######################################main file below
 
 read_cfg()
 
-while cmd_count < len(sys.argv):
-    arg = mt.nohy(sys.argv[cmd_count])
-    arg_orig = sys.argv[cmd_count]
-    if arg_orig in i7.i7x:
-        my_proj = i7.i7x[arg_orig]
-    elif arg_orig in i7.i7xr: # this is because we may have a dash- flag going to the same as a project name, so let's have a way to look at any project
-        my_proj = arg_orig
-    elif arg == 'r':
-        print("Using super-shortcut 'r' for A Roiling Original.")
-        my_proj = "roiling"
-    elif arg == 'npo' or arg == 'pon':
-        post_open_matches = False
-    elif arg == 'po':
-        post_open_matches = True
-    elif arg == 'o':
-        all_similar_projects = False
-    elif arg == 'a':
-        all_similar_projects = True
-    elif arg[:2] == 'mf' and arg[2:].isdigit():
-        max_in_file = int(arg[2:])
-    elif arg[:2] == 'mo' and arg[2:].isdigit():
-        max_overall = int(arg[2:])
-    elif arg == 'vh':
-        view_history = True
-    elif arg == 'v':
-        verbose = True
-    elif arg == 'q':
-        verbose = False
-    elif arg == 'qo' or arg == 'oq':
-        quote_status = OUTSIDE
-    elif arg == 'qi' or arg == 'iq':
-        quote_status = INSIDE
-    elif arg == 'qa' or arg == 'aq':
-        quote_status = ALL
-    elif arg == 'newhist':
-        create_new_history = True
-    elif arg == 'e' or arg == 'ec' or arg == 'ce':
-        mt.npo(my_cfg)
-    elif arg == 'ml' or arg == 'lm':
-        modify_line = True
-    elif sorted(arg) == 'lmn': #no modify line
-        modify_line = False
-    elif arg[:2] == 'mn':
-        try:
-            user_specified_matches_needed = int(arg[2:])
-            if user_specified_matches_needed < 1:
-                sys.exit("-mn must define a positive number.")
-        except:
-            print("mn needs a valid number after it.")
-    elif arg == 'ny' or arg == 'yn':
-        include_notes = True
-    elif arg == 'nn':
-        include_notes = False
-    elif arg == 'fm':
-        fast_match = True
-    elif arg == 'nfm' or arg == 'fmn':
-        fast_match = False
-    elif arg == '?':
-        usage()
-    else:
-        if verbose:
-            print("Adding searchable string", arg)
-        arg = arg.replace('/', '[^a-z]*')
-        arg = arg.replace('`', '')
-        match_string_array.append(arg)
-    cmd_count += 1
+read_args(my_arg_array = sys.argv[1:])
 
 if not my_proj:
     if not default_from_cwd:
@@ -316,78 +338,91 @@ if not my_proj:
         print("Using default project", default_from_cwd, "from current directory")
         my_proj = default_from_cwd
 
-#file_list = i7.i7com[default_from_cwd]
-if all_similar_projects:
-    proj_umbrella = related_projects(my_proj)
-else:
-    proj_umbrella = [my_proj]
+while first_loop or user_input:
+    if user_input:
+        frequencies.clear()
+        from_user = input("search string (current project {}) >>".format(my_proj))
+        if not from_user:
+            sys.exit("Ok, that's all.")
+        read_args(from_user.strip().split(" "))
 
-history_file = hist_file_of(my_proj)
+    first_loop = False
 
-if view_history:
-    print(history_file)
-    mt.npo(history_file)
-
-if not len(match_string_array):
-    sys.exit("You need to specify text to find.")
-
-matches_needed = len(match_string_array)
-if user_specified_matches_needed:
-    if user_specified_matches_needed > matches_needed:
-        print("mn was bigger than the number of matches.")
-    elif user_specified_matches_needed == matches_needed:
-        print("mn was bigger than the number of matches.")
+    if all_similar_projects:
+        proj_umbrella = related_projects(my_proj)
     else:
-        matches_needed = user_specified_matches_needed
+        proj_umbrella = [my_proj]
 
-print("Searching for string{}: {}".format(mt.plur(match_string_array), ' / '.join(match_string_array)))
+    history_file = hist_file_of(my_proj)
 
-for proj in proj_umbrella:
-    if proj not in i7.i7f:
-        if os.path.exists(i7.main_src(proj)):
-            print("No project exists for {}. But there is a story file. So I am using that.".format(proj))
-            my_array = [ i7.main_src(proj) ]
+    if view_history:
+        print(history_file)
+        mt.npo(history_file)
+
+    if not len(match_string_array):
+        if user_input:
+            print("OK, you may only have changed options. If you wish to exit, enter a blank line.")
+            continue
+        sys.exit("You need to specify text to find.")
+
+    matches_needed = len(match_string_array)
+    if user_specified_matches_needed:
+        if user_specified_matches_needed > matches_needed:
+            print("mn was bigger than the number of matches.")
+        elif user_specified_matches_needed == matches_needed:
+            print("mn was bigger than the number of matches.")
         else:
-            print("WARNING", proj, "does not have a project file array associated with it. It may not be a valid inform project.")
-            continue
-    else:
-        my_array = i7.i7f[proj]
-        if proj in i7.i7aux:
-            my_array.extend(i7.i7aux[proj])
-    for projfile in my_array:
-        if not os.path.exists(projfile):
-            if 'story.ni' in projfile:
-                print("Skipping nonexistent story file for {}, probably due to 'only' parameter.".format(proj))
+            matches_needed = user_specified_matches_needed
+
+    print("Searching for string{}: {}".format(mt.plur(match_string_array), ' / '.join(match_string_array)))
+
+    for proj in proj_umbrella:
+        if proj not in i7.i7f:
+            if os.path.exists(i7.main_src(proj)):
+                print("No project exists for {}. But there is a story file. So I am using that.".format(proj))
+                my_array = [ i7.main_src(proj) ]
+            else:
+                print("WARNING", proj, "does not have a project file array associated with it. It may not be a valid inform project.")
                 continue
-            print("Uh oh,", projfile, "does not exist. It probably should. Skipping.")
-            continue
-        if i7.inform_short_name(projfile) in frequencies:
-            continue
-        frequencies[i7.inform_short_name(projfile)] = find_text_in_file(match_string_array, projfile)
-    notes_file = i7.notes_file(proj)
-    if include_notes:
-        if not os.path.exists(notes_file):
-            print("Skipping absent notes file for", proj)
-            continue
-        if i7.inform_short_name(notes_file) in frequencies: # STS files overlap
-            continue
-        frequencies[i7.inform_short_name(notes_file)] = find_text_in_file(match_string_array, notes_file)
-    elif os.path.exists(notes_file):
-            print("Ignoring notes file {}. Toggle with yn/ny.".format(notes_file))
-    notes_file = i7.notes_file(proj)
+        else:
+            my_array = i7.i7f[proj]
+            if proj in i7.i7aux:
+                my_array.extend(i7.i7aux[proj])
+        for projfile in my_array:
+            if not os.path.exists(projfile):
+                if 'story.ni' in projfile:
+                    print("Skipping nonexistent story file for {}, probably due to 'only' parameter.".format(proj))
+                    continue
+                print("Uh oh,", projfile, "does not exist. It probably should. Skipping.")
+                continue
+            if i7.inform_short_name(projfile) in frequencies:
+                continue
+            frequencies[i7.inform_short_name(projfile)] = find_text_in_file(match_string_array, projfile)
+        notes_file = i7.notes_file(proj)
+        if include_notes:
+            if not os.path.exists(notes_file):
+                print("Skipping absent notes file for", proj)
+                continue
+            if i7.inform_short_name(notes_file) in frequencies: # STS files overlap
+                continue
+            frequencies[i7.inform_short_name(notes_file)] = find_text_in_file(match_string_array, notes_file)
+        elif os.path.exists(notes_file):
+                print("Ignoring notes file {}. Toggle with yn/ny.".format(notes_file))
+        notes_file = i7.notes_file(proj)
 
-print("    ---- total matches printed:", found_overall)
-for x in sorted(frequencies, key=frequencies.get, reverse=True):
-    if frequencies[x] < 1: continue
-    print("    ---- {} match{} in {}".format(frequencies[x], 'es' if frequencies[x] > 1 else '', i7.inform_short_name(x)))
+    print("    ---- total matches printed:", found_overall)
+    for x in sorted(frequencies, key=frequencies.get, reverse=True):
+        if frequencies[x] < 1: continue
+        print("    ---- {} match{} in {}".format(frequencies[x], 'es' if frequencies[x] > 1 else '', i7.inform_short_name(x)))
 
-temp_array = [i7.inform_short_name(x) for x in frequencies if frequencies[x] == 0]
-if len(temp_array):
-    print("No matches for", ', '.join(temp_array))
+    temp_array = [i7.inform_short_name(x) for x in frequencies if frequencies[x] == 0]
+    if len(temp_array):
+        print("No matches for", ', '.join(temp_array))
 
-temp_array = [i7.inform_short_name(x) for x in frequencies if frequencies[x] == -1]
-if len(temp_array):
-    print("Left untested:", ', '.join(temp_array))
+    temp_array = [i7.inform_short_name(x) for x in frequencies if frequencies[x] == -1]
+    if len(temp_array):
+        print("Left untested:", ', '.join(temp_array))
+    write_history(history_file, ' '.join(sorted(match_string_array)), create_new_history)
 
-write_history(history_file, ' '.join(sorted(match_string_array)), create_new_history)
-mt.post_open()
+    mt.post_open()
+    match_string_array = []
