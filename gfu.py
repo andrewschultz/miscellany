fd5f7be8b1f91490471b282980d77b0666f01828
Andrew <blurglecruncheon@gmail.com>
2021-06-22 05:38:22 -0500

    gfu.py allow m=/f=

--- gfu.py
+++ gfu.py
@@ -73,6 +73,7 @@ def usage(my_msg = "Usage for gfu.py"):
     print("d to delete stuff. Otherwise, you need entries for commit message and file selection.")
     print("nt/t toggles file trimming.")
     print("# number adds to a file (#) days ahead, n adds to next open day.")
+    print("The program scans for spaces in arguments, so use quotes. You can also use f=files, m=messages, d=dates.")
     print()
     print("The path argument must have . or * in it. The commit must have a space.")
     print()
@@ -81,11 +82,12 @@ def usage(my_msg = "Usage for gfu.py"):
 
 while cmd_count < len(sys.argv):
     arg = mt.nohy(sys.argv[cmd_count])
-    if ' ' in arg:
+    (arg_prefix, arg) = mt.prefix_div(arg, '=')
+    if ' ' in arg or arg_prefix == 'm':
         if my_msg:
             sys.exit("Two messages.")
         my_msg = sys.argv[cmd_count]
-    elif '.' in arg or '*' in arg:
+    elif '.' in arg or '*' or arg_prefix == 'f':
         if my_files:
             sys.exit("Two file specs")
         my_files = sys.argv[cmd_count]
@@ -98,11 +100,13 @@ while cmd_count < len(sys.argv):
     elif arg in ( 'nt', 'tn' ):
         trim_files = False
     elif arg.isdigit():
-        days_ahead = int(arg)
+        sys.exit("d/days= needs an integer after.")
     elif arg == 'n':
         days_ahead = next_from_batches()
         print("First open day is", days_ahead, "day{} ahead".format(mt.plur(days_ahead)))
     elif arg == '?':
+        if arg_prefix:
+            print("WARNING unrecognized prefix", arg_prefix)
         usage()
     else:
         usage("Bad argument {}".format(sys.argv[cmd_count]))
@@ -142,10 +146,13 @@ system_cmd = "schtasks /create /f /tn {} /tr \"{}\" /sc Once /sd {} /st 00:01".f
 
 if trim_files:
     print("Trimming whitespace...")
-    x = git_wildcard(my_files)
+    file_array = git_wildcard(my_files)
     for f in file_array:
-        os.system("ttrim \"{}\"".format(os.path.realpath(f)))
+        os.system("ttrim.py \"{}\"".format(os.path.realpath(f)))
 
 print(system_cmd)
 os.system(system_cmd)
+print("To undo this, try:")
+print("    schtasks /DELETE /F /TN {}".format(task_name))
+print("    erase {}".format(batch_file))
 
