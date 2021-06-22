6a23d34e87d80aaf3cb52caa7dab841c4baa7ec0
Andrew <blurglecruncheon@gmail.com>
2021-06-22 06:15:33 -0500

    gfu.py now can check if commit worked

--- gfu.py
+++ gfu.py
@@ -11,6 +11,8 @@ import os
 import sys
 import mytools as mt
 import win32com.client
+import subprocess
+import ctypes
 
 #schtasks /create /f /tn "Test" /tr "\"c:\program files\test.bat\" arg1 'arg 2 with spaces' arg3" /sc Daily /st 00:00
 
@@ -22,9 +24,13 @@ days_ahead = 1
 trim_files = True
 
 def git_wildcard(my_files):
-    import subprocess
     x = subprocess.check_output(['git', 'ls-files', my_files]).decode()
+    return [y.strip() for y in x.split("\n") if y.strip()]
     return x.strip().split("\n")
+
+def git_staged():
+    x = subprocess.check_output(['git', 'diff', '--name-only', '--cached']).decode()
+    return [y.strip() for y in x.split("\n") if y.strip()]
 
 def delete_tasks_and_batches():
     scheduler = win32com.client.Dispatch('Schedule.Service')
@@ -83,7 +89,21 @@ def usage(my_msg = "Usage for gfu.py"):
 while cmd_count < len(sys.argv):
     arg = mt.nohy(sys.argv[cmd_count])
     (arg_prefix, arg) = mt.prefix_div(arg, '=')
-    if ' ' in arg or arg_prefix == 'm':
+    if arg_prefix == 'l':
+        files_to_check = git_wildcard(sys.argv[cmd_count][2:])
+        staged_array = git_staged()
+        popup_string = ""
+        for x in files_to_check:
+            if x in staged_array:
+                popup_string += "{}\n".format(os.path.abspath(x))
+        if popup_string:
+            popup_string = pendulum.now().format("MM/DD/YYYY HH:mm:ss") + "\n\n" + popup_string
+            messageBox = ctypes.windll.user32.MessageBoxW
+            messageBox(None, popup_string, "Still staged after git commit {}".format(sys.argv[cmd_count][2:]), 0x0)
+        else:
+            print("Everything that should be committed, is.")
+        sys.exit()
+    elif ' ' in arg or arg_prefix == 'm':
         if my_msg:
             sys.exit("Two messages.")
         my_msg = sys.argv[cmd_count]
@@ -91,6 +111,8 @@ while cmd_count < len(sys.argv):
         if my_files:
             sys.exit("Two file specs")
         my_files = sys.argv[cmd_count]
+        if arg_prefix:
+            my_files = my_files[2:]
         if len(git_wildcard(my_files)) == 0:
             sys.exit("No wildcard found for file argument{}. Bailing.".format(my_files))
     elif arg == 'd':
@@ -132,23 +154,37 @@ task_name = "\"future-gfu-{}\"".format(tomorrow_readable)
 
 batch_file = "c:\\writing\\temp\\sched-{}.bat".format(tomorrow_readable)
 
-f = open(batch_file, "a")
+file_array = git_wildcard(my_files)
 
-f.write("\necho off\n\n")
-f.write("cd {}\\{}\n\n".format(i7.gh_dir, my_proj))
-f.write("git add {}\n\n".format(my_files))
-f.write("git commit -m \"{}\"\n\n".format(my_msg))
-f.write("echo GIT PUSH on your own time, especially if experimenting with this script and its products!\n\n")
-
-f.close()
-
-system_cmd = "schtasks /create /f /tn {} /tr \"{}\" /sc Once /sd {} /st 00:01".format(task_name, batch_file, tomorrow_date)
+full_file_array = [os.path.realpath(x) for x in file_array]
 
 if trim_files:
     print("Trimming whitespace...")
-    file_array = git_wildcard(my_files)
     for f in file_array:
         os.system("ttrim.py \"{}\"".format(os.path.realpath(f)))
+
+for f in full_file_array:
+    os.system("attrib +r \"{}\"".format(f))
+
+fout = open(batch_file, "a")
+
+fout.write("\necho off\n\n")
+fout.write("cd {}\\{}\n\n".format(i7.gh_dir, my_proj))
+fout.write("git add {}\n\n".format(my_files))
+fout.write("git commit -m \"{}\"\n\n".format(my_msg))
+fout.write("gfu.py \"l={}\"\n\n".format(my_files))
+fout.write("echo GIT PUSH on your own time, especially if experimenting with this script and its products!\n\n")
+
+for f in full_file_array:
+    fout.write("attrib -r \"{}\"\n\n".format(f))
+
+fout.close()
+
+mt.npo(batch_file)
+
+sys.exit()
+
+system_cmd = "schtasks /create /f /tn {} /tr \"{}\" /sc Once /sd {} /st 00:01".format(task_name, batch_file, tomorrow_date)
 
 print(system_cmd)
 os.system(system_cmd)
