07f4fe1beca7892f380c7cbd6df69e1b2a95b731
Andrew <blurglecruncheon@gmail.com>
2021-06-23 15:30:03 -0500

    rz.py can now look through multiple words

--- wrifi/rz.py
+++ wrifi/rz.py
@@ -22,6 +22,59 @@ def url_of(this_word, batch_convert = False):
         my_url = my_url.replace("&", "^&")
     return my_url
 
+def send_to_cache(w):
+    url = url_of(w)
+
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html, features="html.parser")
+
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
+
+    # get text
+    text = soup.get_text()
+    f = open(rz_cache, "w")
+    f.write(text)
+    f.close()
+
+def process_cache(my_word, reset = False):
+    outputting = "w" if reset else "a"
+    f = open(rz_cache, "r")
+    text = f.read()
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+
+    read_syllables = False
+    any_syllables = False
+    out_string = ''
+
+    for x in chunks:
+        if re.search("^[0-9] syllable", x):
+            read_syllables = True
+            any_syllables = True
+        if 'almost rhyme' in x.lower() or 'more ideas' in x.lower():
+            read_syllables = False
+        if 'words and phrases that' in x.lower():
+            out_string += "\n" + x
+        if read_syllables:
+            if 'syllable' in x:
+                out_string += "\n" + x
+            else:
+                out_string += x.replace(',', ', ')
+
+    if not any_syllables:
+        sys.exit("Failed to read anything.")
+
+    out_string = '=========================Rhymes for {}=========================\n'.format(my_word) + out_string.lstrip()
+
+    f = open(rz_out, outputting)
+    f.write(out_string.strip() + "\n")
+    f.close()
+
 while cmd_count < len(sys.argv):
     arg = nohy(sys.argv[cmd_count])
     if arg == 'w':
@@ -50,59 +103,17 @@ if use_cache:
     if not my_word:
         print("Warning could not find rhymable word in cache.")
         my_word = "UNDEFINED"
-else:
-    try:
-        my_word = words_to_rhyme[0]
-        url = url_of(my_word)
-    except:
-        sys.exit("I need a word to rhyme.")
+    process_cache(my_word, reset = True)
+    sys.exit()
 
-    if len(words_to_rhyme) == 0:
-        sys.exit("No rhymes given.")
-
-    html = urlopen(url).read()
-    soup = BeautifulSoup(html, features="html.parser")
-
-    # kill all script and style elements
-    for script in soup(["script", "style"]):
-        script.extract()    # rip it out
-
-    # get text
-    text = soup.get_text()
-    f = open(rz_cache, "w")
-    f.write(text)
-    f.close()
-
-# break into lines and remove leading and trailing space on each
-lines = (line.strip() for line in text.splitlines())
-# break multi-headlines into a line each
-chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-# drop blank lines
-
-read_syllables = False
-any_syllables = False
-out_string = ''
-
-for x in chunks:
-    if re.search("^[0-9] syllable", x):
-        read_syllables = True
-        any_syllables = True
-    if 'almost rhyme' in x.lower() or 'more ideas' in x.lower():
-        read_syllables = False
-    if 'words and phrases that' in x.lower():
-        out_string += "\n" + x
-    if read_syllables:
-        if 'syllable' in x:
-            out_string += "\n" + x
-        else:
-            out_string += x.replace(',', ', ')
-
-if not any_syllables:
-    sys.exit("Failed to read anything.")
-
-out_string = '=========================Rhymes for {}=========================\n'.format(my_word) + out_string.lstrip()
+if len(words_to_rhyme) == 0:
+    sys.exit("No rhymes given.")
 
 f = open(rz_out, "w")
-f.write(out_string.strip())
 f.close()
+
+for w in words_to_rhyme:
+    send_to_cache(w)
+    process_cache(w, reset = False)
+
 npo(rz_out)
