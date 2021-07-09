aa1623ded497d8ce87befbed6b38f77cbde79206
Andrew <blurglecruncheon@gmail.com>
2021-07-09 06:34:41 -0500

    verb.py allow for copy vs. print text

--- verb.py
+++ verb.py
@@ -4,6 +4,8 @@ from collections import defaultdict
 import sys
 
 verb_types = defaultdict(str)
+copy_text = True
+print_text = False
 
 def add_clipboard_text(prefix, data):
     ary = data.split(",")
@@ -35,13 +37,34 @@ with open("c:/writing/scripts/verbdata.txt") as file:
 clip_text = ""
 
 cmd_count = 1
+
 while cmd_count < len(sys.argv):
     arg = sys.argv[cmd_count]
     (prefix, data) = mt.cfg_data_split(arg)
-    if not prefix or not data:
-        sys.exit("Badly formed argument {}".format(arg))
-    clip_text += add_clipboard_text(prefix, data)
+    if arg in ( 'pc', 'cp' ):
+        copy_text = print_text = True
+    elif arg == 'c':
+        copy_text = True
+        print_text = False
+    elif arg == 'p':
+        copy_text = False
+        print_text = True
+    elif arg == '?':
+        print("Verb types and results:")
+        for x in verb_types:
+            print(x, verb_types[x])
+        sys.exit()
+    elif not prefix:
+        sys.exit("Badly formed argument {}: there is no default prefix, so we need to specify w= for out of world, etc.".format(arg))
+    elif not data:
+        sys.exit("You need data after the =/: in {}.".format(arg))
+    else:
+        clip_text += add_clipboard_text(prefix, data)
     cmd_count += 1
 
-pyperclip.copy(clip_text)
-print(clip_text)
+if copy_text:
+    print("Copied source text to clipboard.")
+    pyperclip.copy(clip_text)
+
+if print_text:
+    print(clip_text)
