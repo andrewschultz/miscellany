5e11cdc5a97a37d0615f313f1819a64dec61c030
Andrew <blurglecruncheon@gmail.com>
2021-07-18 06:10:33 -0500

    regv.py can now detect missing verbs

--- i7/py/regv.py
+++ i7/py/regv.py
@@ -1,9 +1,19 @@
 # regv.py : (crude) regex testing verb generator
 
+from collections import defaultdict
+import os
 import re
 import sys
 import i7
 import mytools as mt
+import glob
+
+debug = False
+
+def valid_understand(my_line):
+    if not my_line.startswith('understand'): return False
+    if ' as something new' in my_line: return False
+    return True
 
 def expand_verbs(my_string):
     # print("Expanding", my_string)
@@ -19,6 +29,64 @@ def expand_verbs(my_string):
         break
     return temp_ary
 
+def verbs_from_line(my_line):
+    ret_ary = []
+    ary = my_line.strip().split('"')[1::2]
+    for a in ary:
+        ret_ary.extend(expand_verbs(a))
+    return ret_ary
+
+def find_verbs(file_list):
+    argless = defaultdict(bool)
+    witharg = defaultdict(bool)
+    for f in file_list:
+        bn = os.path.basename(f)
+        with open(f) as file:
+            for (line_count, line) in enumerate (file, 1):
+                if not valid_understand(line):
+                    continue
+                for q in verbs_from_line(line):
+                    if '[' in q:
+                        q = re.sub(" *\[.*", "", q)
+                        witharg[q] = False
+                    else:
+                        argless[q] = False
+    return (argless, witharg)
+
+def process_misses(my_dict, list_desc):
+    this_list = sorted([x for x in my_dict if not my_dict[x]])
+    if len(this_list) == 0:
+        print("Hooray! Nothing missing in {}.".format(list_desc))
+    else:
+        print("Missing {} entries in {}.".format(len(this_list), list_desc))
+        print(' / '.join(this_list))
+                
+def look_up_test_cases(my_proj, my_list):
+    regs = i7.proj2dir(my_proj)
+    reg_glob = os.path.normpath(os.path.join(regs, "reg-*.txt"))
+    g = glob.glob(regs + "/reg-*.txt")
+    (argless, witharg) = find_verbs(my_list)
+    for testfile in g:
+        tb = os.path.basename(testfile)
+        with open(testfile) as file:
+            for (line_count, line) in enumerate (file, 1):
+                if not line.startswith(">"):
+                    continue
+                ary = line[1:].strip().lower().split(' ')
+                verb_candidate = ary[0].lower()
+                if verb_candidate in argless:
+                    if len(ary) == 1:
+                        if debug and not argless[verb_candidate]:
+                            print("Found argless", verb_candidate, tb, line_count)
+                        argless[verb_candidate] = True
+                if verb_candidate in witharg:
+                    if len(ary) == 2:
+                        if debug and not witharg[verb_candidate]:
+                            print("Found with-arg", verb_candidate, tb, line_count)
+                        witharg[verb_candidate] = True
+    process_misses(argless, "verbs without arguments")
+    process_misses(witharg, "verbs with arguments")
+    sys.exit()
 
 def crank_out_verb_tests(this_file):
     next_break = False
@@ -46,15 +114,13 @@ def crank_out_verb_tests(this_file):
                     wrongo_verb = ''
                     look_for_say = False
                     continue
-            if not line.startswith('understand'): continue
-            if ' as something new' in line: continue
+            if not valid_understand(line): continue
             look_for_say = True
-            ary = line.strip().split('"')[1::2]
-            for a in ary:
-                big_ary.extend(expand_verbs(a))
+            big_ary.extend(verbs_from_line(line))
 
 user_project = ''
 cmd_count = 1
+lookup_cases = False
 
 while cmd_count < len(sys.argv):
     arg = mt.nohy(sys.argv[cmd_count])
@@ -62,6 +128,12 @@ while cmd_count < len(sys.argv):
         if user_project:
             sys.exit("Redefining user project from {} to {}.".format(user_project, arg))
         user_project = i7.i7x[arg]
+    elif arg == 'l':
+        lookup_cases = True
+    elif arg == 'd':
+        debug = True
+    elif arg in ( 'ld', 'dl' ):
+        lookup_cases = debug = True
     else:
         sys.exit("Could not find project for {}.".format(arg))
     cmd_count += 1
@@ -74,9 +146,11 @@ if not user_project:
 else:
     my_proj = user_project
 
-if my_proj not in i7.i7f:
-    crank_out_verb_tests(i7.main_src(my_proj))
+project_file_list = i7.i7f[my_proj] if my_proj in i7.i7f else [ i7.main_src(my_proj) ]
+
+if lookup_cases:
+    look_up_test_cases(my_proj, project_file_list)
     sys.exit()
 
-for x in i7.i7f[my_proj]:
+for x in project_file_list:
     crank_out_verb_tests(x)
