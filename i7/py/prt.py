b10c65fdcfc907e51bf30b5c65cec0e76f86e951
Andrew <blurglecruncheon@gmail.com>
2021-10-31 23:57:00 -0500

    prt.py is faster than/can replace prt.pl

---
+++ i7/py/prt.py
@@ -0,0 +1,58 @@
+import shutil
+import os
+import glob
+import sys
+import i7
+import filecmp
+
+try:
+    my_proj = i7.long_name(sys.argv[1])
+except:
+    my_proj = i7.dir2proj()
+
+mp = i7.proj2dir(my_proj)
+
+if not my_proj:
+    sys.exit("Need a project or valid directory.")
+
+changes = 0
+news = 0
+
+for x in glob.glob(os.path.join(mp, "reg-*.txt")):
+    x0 = os.path.join(i7.prt, os.path.basename(x))
+    if not os.path.exists(x0):
+        print("New file", x0)
+        news += 1
+    if filecmp.cmp(x, x0):
+        continue
+    else:
+        changes += 1
+        print(x, "has changed. Copying over.")
+        shutil.copy(x, x0)
+
+build_dir = i7.proj2dir(my_proj, my_subdir = "Build")
+
+y0 = glob.glob(os.path.join(build_dir, "output.*"))
+
+if len(y) == 0:
+    sys.exit("No binary found in {}".format(build_dir))
+elif len(y) > 1:
+    sys.exit("Multiple binaries found in {}: {}".format(build_dir, y))
+
+my_copy_file = y[0]
+
+(_, my_ext) = os.path.splitext(my_copy_file)
+
+file_dest = os.path.join(i7.prt, "debug-{}{}".format(i7.long_name(my_proj), my_ext))
+
+if os.path.exists(file_dest) and filecmp.cmp(my_copy_file, file_dest):
+    print("No binary file change.")
+else:
+    print("New binary file needed! Copying {} to {}.".format(my_copy_file, file_dest))
+    shutil.copy(y[0], file_dest)
+
+if not changes:
+    print("No test scripts changed.")
+
+if not news:
+    print("No new test scripts.")
