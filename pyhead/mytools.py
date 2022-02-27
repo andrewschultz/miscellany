bffd1fafc974046d97b05b92e9d0d0984bcf93ce
Andrew <blurglecruncheon@gmail.com>
2022-02-27 13:01:27 -0600

    mytools text_to_browser file name modified, bail option added

--- pyhead/mytools.py
+++ pyhead/mytools.py
@@ -1034,14 +1034,17 @@ def browser_or_native(file_name, print_action = True, bail = False, return_to_or
 
 open_in_browser = file_in_browser = file_in_browser_conditional = gib = g_i_b = graphics_in_browser = tib = t_i_b = text_in_browser = fib = f_i_b = browser_or_native
 
-def text_to_browser(my_text, delete_immediately = True):
-    file_name = pendulum.now().format("YYYY-MM-DD-HH-mm-SS.txt")
+def text_to_browser(my_text, delete_immediately = True, bail = False):
+    file_name = pendulum.now().format("YYYY-MM-DD-HH-mm-SS") + '.txt'
     f = open(file_name, "w")
     f.write(my_text)
     f.close()
     browser_or_native(file_name)
     if delete_immediately:
+        time.sleep(.1)
         os.remove(file_name)
+    if bail:
+        sys.exit()
 
 def comment_combine(my_lines, cr_at_end = True):
     if type(cr_at_end) != bool and type(cr_at_end) != int:
