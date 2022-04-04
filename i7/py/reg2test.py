1feac3ab158cfbaf1830e0aa9acadc927165f2e2
Andrew <blurglecruncheon@gmail.com>
2022-04-04 13:58:25 -0500

    reg2test.py added truncate-hyphens option

--- i7/py/reg2test.py
+++ i7/py/reg2test.py
@@ -7,15 +7,23 @@ import glob
 
 ignorables = [ 'score', 'thisalt' ]
 
+truncate_hyphens = True
+
 try:
     wild_card = sys.argv[1]
 except:
     print("You need an argument for the wildcard.")
 
+def usage(my_header = 'USAGE FOR REG2TEST'):
+    print(my_header
+    sys.exit()
+
 def convert_reg2test(my_file):
     command_list = []
     test_out_name = my_file.replace(".txt", "").replace("reg-", "")
     test_out_name = re.sub("^.*?-", "", test_out_name)
+    if truncate_hyphens:
+        test_out_name = test_out_name.replace('-', '')
     with open(my_file) as file:
         for (line_count, line) in enumerate (file, 1):
             if not line.startswith(">"):
@@ -31,6 +39,17 @@ def convert_reg2test(my_file):
                 continue
             command_list.append(cmd)
     return('test {} with "{}"'.format(test_out_name, '/'.join(command_list)))
+
+cmd_count = 1
+while cmd_count < len(sys.argv):
+    arg = mt.nohy(sys.argv)
+    if arg in ( 'nh', 'hn', 'th', 'ht' ):
+        truncate_hyphens = True
+    elif arg in ( 'h', 'hy', 'yh' ):
+        truncate_hyphens = False
+    else:
+        usage('illegal command {}".format(arg))
+    cmd_count += 1
 
 my_files = glob.glob("reg-*.txt")
 
