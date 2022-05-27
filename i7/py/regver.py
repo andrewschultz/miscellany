f3b210ca2e2d80432c0ad963ac735e739b668402
Andrew <blurglecruncheon@gmail.com>
2022-05-26 23:57:00 -0500

    regver.py now tracks running total for pre-commit scripts, better logging of results

--- i7/py/regver.py
+++ i7/py/regver.py
@@ -2,11 +2,14 @@
 # regver.py: verify that important tests are not commented out in reg-* files
 #
 
+import i7
+import colorama
 import mytools as mt
 import os
 import sys
 import glob
 from collections import defaultdict
+from shutil import copy
 
 open_after = True
 edit_files = False
@@ -17,11 +20,14 @@ temp_regver = "c:/writing/temp/regver-py.txt"
 
 default_wild_card = "reg-*-lone-*.txt"
 
+my_proj = ''
+
 def usage(header = "usage for ".format(__file__)):
     print('=' * 20, header, '=' * 20)
     print("m# = max files")
     print("o = open after, no/on = don't open after")
-    print("You can specify a file or a wild card to test with an asterisk. Default wild card is {}.".format(default_wild_card))
+    print("You can specify a file or a wild card to test with an asterisk or long (4+ chars) string name.")
+    print("Default wild card is {}.".format(default_wild_card))
     sys.exit()
 
 def to_wild(my_text):
@@ -45,9 +51,13 @@ def check_one_file(my_file, edit_the_file = True):
             if line.startswith(">{include}"):
                 my_entry = line[10:].strip()
                 includes[line[10:].strip()] = line_count
+    color_highlight = colorama.Fore.GREEN if edit_the_file else colorama.Fore.RED
     for x in commented_sections:
         if x not in includes:
-            print("{}: {} is commented at line {} of but is not in {{include}}s.".format(my_file, x, commented_sections[x]))
+            if not ret_val:
+                mbase = os.path.basename(my_file)
+                print("==========================", mbase)
+            print(color_highlight + "    {} section {} is commented at line {} but is not in any {{include}}s.".format(mbase, x, commented_sections[x]) + colorama.Style.RESET_ALL)
             ret_val = True
             lines_to_edit.append(commented_sections[x])
             if open_after:
@@ -62,7 +72,7 @@ def check_one_file(my_file, edit_the_file = True):
             f.write(line)
     f.close()
     copy(temp_regver, my_file)
-    os.path.delete(temp_regver)
+    os.remove(temp_regver)
     return ret_val
 
 files = []
@@ -87,6 +97,8 @@ while cmd_count < len(sys.argv):
         max_files = num
     elif os.path.exists(arg):
         files.append(arg)
+    elif i7.long_name(arg):
+        my_proj = arg
     elif '*' in arg or len(arg) > 4:
         wild_cards.append(to_wild(arg))
     elif arg == '?':
@@ -94,6 +106,9 @@ while cmd_count < len(sys.argv):
     else:
         usage(header = 'invalid argument: {}'.format(arg))
     cmd_count += 1
+
+if my_proj:
+    i7.go_proj(my_proj)
 
 if not len(files) and not len(wild_cards):
     print("Going with default {}.".format(default_wild_card))
@@ -104,22 +119,27 @@ for this_wild in wild_cards:
 
 files = sorted(set(files))
 
-running_total = 0
+if not len(files):
+    sys.exit("No files were specified.")
+
+running_total = 0 # this is the running total of files to edit
 
 for f in files:
     running_total += check_one_file(f, edit_the_file = edit_files)
 
 if not running_total:
     if len(files) > 1:
-        print(len(files), "files passed!")
+        print(len(files), "files {} passed!".format('both' if len(files) == 2 else 'all'))
     else:
-        print(files[0], "passed!")
+        print(files[0], "file passed!")
 else:
-    print(running_total, "files failed,", len(files) - running_total, "files failed")
+    print("{} file{} {} of {} total".format(running_total, mt.plur(running_total), 'rewritten' if edit_files else 'failed', len(files)))
 
 if running_total and not edit_files:
     print("-e edits files")
 
 if open_after:
     print("no/on turns opening files off, en/ne edits but does not open files.")
-    mt.post_open()
+    mt.post_open(bail_after = False)
+
+sys.exit(running_total)
