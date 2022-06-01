dda1572bbfab092eec7f0041b1dd19aa1f581cdc
Andrew <blurglecruncheon@gmail.com>
2022-05-31 23:57:00 -0500

    sas.py now scans story and extension files

--- i7/py/sas.py
+++ i7/py/sas.py
@@ -11,6 +11,9 @@ my_string = "aqueduct"
 find_regex = False
 print_full_rule = False
 
+track_story_files = True
+track_extension_files = True
+
 try:
     my_string = sys.argv[1]
     if my_string.startswith('r:'):
@@ -41,14 +44,18 @@ def look_for_string(my_string, this_file):
             if in_rule and print_full_rule:
                 print(line_count, this_file, line.strip())
 
-ary = glob.glob("c:/games/inform/*.inform")
+if track_story_files:
+    ary = glob.glob("c:/games/inform/*.inform")
+    for d in ary:
+        if not os.path.isdir(d):
+            continue
+        my_ni_file = os.path.join(d, "source", "story.ni")
+        if not os.path.exists(my_ni_file):
+            print("WARNING no story file", my_ni_file)
+            continue
+        look_for_string(my_string, my_ni_file)
 
-for d in ary:
-    if not os.path.isdir(d):
-        continue
-    my_ni_file = os.path.join(d, "source", "story.ni")
-    if not os.path.exists(my_ni_file):
-        print("WARNING no story file", my_ni_file)
-        continue
-    look_for_string(my_string, my_ni_file)
-
+if track_extension_files:
+    ary = glob.glob("C:/Program Files (x86)/Inform 7/Inform7/Extensions/Andrew Schultz/*.i7x")
+    for a in ary:
+        look_for_string(my_string, a)
