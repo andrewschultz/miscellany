53098ea61a27989113225d4f241e8b79fd52f1a7
Andrew <blurglecruncheon@gmail.com>
2022-07-12 13:07:21 -0500

    rotrot.py now uses glob instead of hard coded file name

--- python/rotrot.py
+++ python/rotrot.py
@@ -1,3 +1,8 @@
+#
+# rotrot.py: this takes a text file and goes through the Caesar Ciphers from 1 to 25.
+#
+
+import glob
 import os
 from string import ascii_uppercase
 from string import ascii_lowercase
@@ -18,42 +23,45 @@ def rotate_string(original_string, shift_by):
     return return_string
 
 rotate_delta = 1
-current_rotate = 0
-am_rotating = False
-am_writing = False
-ever_wrote = False
 
-if not os.path.exists("prerot-walkthrough.txt"):
+def convert_prerot(file_name):
+    current_rotate = 0
+    am_rotating = False
+    am_writing = False
+    ever_wrote = False
+    with open(file_name) as file:
+        for (line_count, line) in enumerate (file, 1):
+            if line.startswith("FILE="):
+                file_to_write = line[5:].strip()
+                f = open(file_to_write, "w")
+                ever_wrote = True
+                am_writing = True
+                continue
+            if line.lower().startswith("==start-rotate"):
+                am_rotating = True
+                continue
+            if line.lower().startswith("==end-rotate"):
+                am_rotating = False
+                continue
+            if am_writing:
+                if not am_rotating or not line.strip():
+                    f.write(line)
+                    continue
+                current_rotate += rotate_delta
+                if current_rotate > 26:
+                    current_rotate -= 26
+                f.write(rotate_string(line, current_rotate))
+        f.close()
+        if not ever_wrote:
+            print("No FILE= command in the pre-file, so I don't know where to write to.")
+            return
+        os.system(file_to_write)
+
+my_glob = glob.glob("prerot*")
+
+if len(my_glob) == 0:
     sys.exit("You need a prerot-walkthrough file in this directory.")
 
-with open("prerot-walkthrough.txt") as file:
-    for (line_count, line) in enumerate (file, 1):
-        if line.startswith("FILE="):
-            file_to_write = line[5:].strip()
-            f = open(file_to_write, "w")
-            ever_wrote = True
-            am_writing = True
-            continue
-        if line.lower().startswith("==start-rotate"):
-            am_rotating = True
-            continue
-        if line.lower().startswith("==end-rotate"):
-            am_rotating = False
-            continue
-        if am_writing:
-            if not am_rotating or not line.strip():
-                f.write(line)
-                continue
-            current_rotate += rotate_delta
-            if current_rotate > 26:
-                current_rotate -= 26
-            f.write(rotate_string(line, current_rotate))
+for f in my_glob:
+    convert_prerot(f)
 
-if not ever_wrote:
-    sys.exit("No FILE= command in the pre-file, so I don't know where to write to.")
-
-f.close()
-os.system(file_to_write)
-
-for x in range(1, 26):
-    print(rotate_string("This is a test!", x))
