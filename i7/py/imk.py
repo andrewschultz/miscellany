295d4e5b7ecccec58f6718e430644eea7ae8c072
Andrew <blurglecruncheon@gmail.com>
2022-07-14 07:43:24 -0500

    redid imk.py so you could redirect new header to a different repository

--- i7/py/imk.py
+++ i7/py/imk.py
@@ -41,76 +41,84 @@ def check_valid_combo(my_abbrev, abbrev_class, dict_of_abbrevs):
     if my_abbrev in dict_of_abbrevs.values(): return
     sys.exit("You may need to define a valid {} abbreviation--{} turned up nothing.".format(abbrev_class, my_abbrev))
 
+to_create = []
+
 while count < len(sys.argv):
     arg = mt.nohy(sys.argv[count])
-    if arg in ( 'o', 'of', 'fo'): overwrite = True
+    if arg in ( 'o', 'of', 'fo'):
         print("Overwrite option is on.")
+        overwrite = True
     elif arg in ( 'n', 'on', 'no' ):
         print("Not opening post-conversion.")
         open_post_conversion = False
     else:
-        if ' ' in arg:
-            file_args.append(sys.argv[count])
+        if to_create:
+            sys.exit("You can't create more than one file per run.")
+        if ',' not in arg:
+            if count == 1:
+                to_create = sys.argv[1:]
+                break
         else:
-            file_args.append(arg)
+            to_create = sys.argv[1].split(',')
     count += 1
 
-if len(file_args) == 0:
-    sys.exit("I need a valid project and/or header. I have neither.")
+if len(to_create) < 2 or len(to_create) > 3:
+    sys.exit("You need to specify project, header, (optional other project directed to).")
 
-if len(file_args) == 1:
-    temp = i7.dir2proj()
-    if not temp:
-        sys.exit("You need to provide a valid project (abbreviation) and header (abbreviation) or be in a product directory and specify a header (abbreviation).")
-    print("Assuming default project", temp)
-    file_args.insert(0, temp)
+if to_create[1] in i7.i7x and to_create[0] not in i7.i7x:
+    print("It looks like you put the project name second, so I am flipping the arguments. No big deal.")
+    to_create[0], to_create[1] = to_create[1], to_create[0]
 
-if file_args[1] in i7.i7x and file_args[0] not in i7.i7x:
-    print("It looks like you put the project name first, so I am flipping the arguments. No big deal.")
-    file_args = file_args[::-1]
+if len(to_create) == 2:
+    print("Assuming project-to is project-from.")
+    to_create.append(to_create[0])
 
-if len(file_args) == 1 and ' ' in file_args[0]:
-    base_file_noxt = file_args[0].strip()
-    if base_file_noxt.lower().endswith('.i7x'): base_file_noxt = base_file_noxt[-4]
-elif len(file_args) != 2:
-    print("You need 2 file arguments: 1st the project, 2nd the module description. You have {:d}.".format(len(file_args)))
-    print("Though it is okay to use 1 argument if it has a space in it. That space can be at the beginning or end if there is only one word.");
-    usage()
+combos_and_projects = list(i7.i7x)
+combos_and_projects.extend(i7.i7com)
 
-check_valid_combo(file_args[0], 'project', i7.i7x)
-check_valid_combo(file_args[1], 'header file', i7.i7hfx)
+check_valid_combo(to_create[0], 'project', combos_and_projects)
+check_valid_combo(to_create[1], 'header file', i7.i7hfx)
+check_valid_combo(to_create[2], 'project', combos_and_projects)
 
-if not file_args[1] in i7.i7hfx.keys() and not file_args[1] in i7.i7hfx.values():
-    sys.exit("You may need to define a valid header file abbreviation--{} turned up nothing.".format(file_args[1]))
+orig_file = i7.hdr(to_create[0], to_create[1])
+base_file = i7.hdr(to_create[0], to_create[1], base = True)
+base_file_noxt = re.sub("\..*", "", base_file)
 
-if not base_file_noxt:
-    my_proj = i7.proj_exp(file_args[0], False)
-    if not my_proj: sys.exit("You need to define a valid project or project abbreviation.")
-    base_file_noxt = '{:s} {:s}'.format(i7.proj_exp(file_args[0], False), i7.hf_exp(file_args[1])).title().replace('-', ' ')
+github_dir = i7.proj2dir(to_create[2], to_github = True)
+github_file = github_dir + "\\" + base_file
+link_command = "mklink \"{}\" \"{}\"".format(orig_file, github_file)
 
-base_file = base_file_noxt + ".i7x"
-nongit_file = i7.extdir + "\\" + base_file # can't use os.path.join since the file may not be there
-my_git_proj = i7.i7gx[i7.main_abbr(my_proj)] if my_proj in i7.i7xr else my_proj
-git_file = i7.gh_dir + "\\" + my_git_proj + "\\" + base_file
+print(base_file_noxt)
+print(orig_file)
+print(github_file)
+print(link_command)
 
-link_command = "mklink \"{}\" \"{}\"".format(nongit_file, git_file)
+if os.path.exists(orig_file) and not overwrite:
+    sys.exit("{} exists. Delete it or use the -o flag to overwrite.".format(orig_file))
 
-if os.path.exists(git_file) and not overwrite:
+if os.path.exists(github_file) and not overwrite:
     if not open_post_conversion: sys.exit("With open post conversion set to off, there is nothing to do here. Bailing.")
-    print(nongit_file, "exists. Opening and not creating.")
-    mt.npo(nongit_file)
+    print(github_file, "exists. Opening and not creating. Use the -o flag to overwrite.")
+    mt.npo(github_file)
+
+if overwrite:
+    print(github_file, "exists but overwriting.")
 else:
-    if overwrite:
-        print(git_file, "exists but overwriting.")
-    else:
-        print(git_file, "does not exist. Creating.")
-    now = datetime.datetime.now()
-    f = open(git_file, "w")
-    f.write("Version 1/{:02d}{:02d}{:02d} of {:s} by {:s} begins here.\n\n\"This should briefly describe the purpose of {:s}.\"\n\n".format(now.year % 100, now.month, now.day, base_file_noxt, i7.auth, base_file_noxt))
-    f.write("{:s} ends here.\n\n---- DOCUMENTATION ----\n".format(base_file_noxt))
-    f.close()
+    print(github_file, "does not exist. Creating.")
+
+now = datetime.datetime.now()
+f = open(github_file, "w")
+f.write("Version 1/{:02d}{:02d}{:02d} of {:s} by {:s} begins here.\n\n\"This should briefly describe the purpose of {:s}.\"\n\n".format(now.year % 100, now.month, now.day, base_file_noxt, i7.auth, base_file_noxt))
+f.write("{:s} ends here.\n\n---- DOCUMENTATION ----\n".format(base_file_noxt))
+f.close()
 
 print("Running link command", link_command)
-os.system(link_command)
 
-if open_post_conversion: i7.npo(nongit_file)
+try:
+    os.system(link_command)
+    print("Linking from {} to {} was successful.".format(orig_file, github_file))
+except:
+    print("Run this command from a prompt with administrative rights:")
+    print(link_command)
+
+if open_post_conversion: i7.npo(github_file)
