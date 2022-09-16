bfc73a82eb8c8c9427f20e7c13b8f570f4facb8f
Andrew <blurglecruncheon@gmail.com>
2022-09-15 23:52:12 -0500

    dglo.py tweaks: duplicate checking, more global definitions checked

--- i7/py/dglo.py
+++ i7/py/dglo.py
@@ -1,5 +1,6 @@
 # dglo.py: sends variables to global or definitions file
 
+import re
 import os
 import i7
 import mytools as mt
@@ -7,21 +8,39 @@ import mytools as mt
 globals = []
 main_file = []
 
-global_from_temp = "c:/writing/temp/dglo-from.txt"
-global_to_temp = "c:/writing/temp/dglo-to.txt"
+new_main_temp = "c:/writing/temp/dglo-from.txt"
+new_global_temp = "c:/writing/temp/dglo-to.txt"
 
 def valid_variable(my_line):
     if 'is a truth state that varies' in my_line:
         return True
+    if 'is a number that varies' in my_line:
+        return True
+    if 'is a person that varies' in my_line:
+        return True
+    if 'is a room that varies' in my_line:
+        return True
+    if re.search("is a list of [a-z ]+ variable\.", my_line):
+        return True
+    if re.search("^[a-z-]+ is [0-9]+\.", my_line):
+        return True
     return False
 
-def track_globals(my_file, global_file):
-    with open(my_file) as file:
+def track_globals(main_file, global_file):
+    globals = []
+    non_globals = []
+    dupe_set = set()
+    with open(main_file) as file:
         for (line_count, line) in enumerate (file, 1):
             if valid_variable(line):
-                globals.append(line)
+                x = re.sub(" .*", "", line.strip())
+                if x in dupe_set:
+                    print("WARNING DUPLICATE {} {}".format(x, line_count))
+                else:
+                    dupe_set.add(x)
+                    globals.append(line)
             else:
-                main_file.append(line)
+                non_globals.append(line)
     if not len(globals):
         return
     if not os.path.exists(global_file):
@@ -29,23 +48,32 @@ def track_globals(my_file, global_file):
             print(x, end='')
         print("You may need to create a global file for", global_file)
         return
-    fin = open(global_file, "w")
-    in_lines = fin.readlines()
-    fout = open(global_to_temp, "w")
+    mout = open(new_main_temp, "w")
+    gin = open(global_file, "r")
+    global_in_lines = gin.readlines()
+    gout = open(new_global_temp, "w")
     written_yet = False
-    for i in in_lines:
-        fout.write(i)
+    for i in global_in_lines:
+        gout.write(i)
         if ' unsorted' in i:
             for x in globals:
-                fout.write(x)
+                gout.write(x)
             written_yet = True
     if not written_yet:
+        print("write-over ops usually require UNSORTED.")
         for x in globals:
-            fout.write(x)
-    fin.close()
-    fout.close()
-    mt.wm(my_file, global_from_temp)
-    mt.wm(global_file, global_to_temp)
+            gout.write(x)
+    last_blank = False
+    for i in non_globals:
+        if last_blank and not i.strip():
+            continue
+        mout.write(i)
+        last_blank = not i.strip()
+    gin.close()
+    gout.close()
+    mout.close()
+    mt.wm(main_file, new_main_temp)
+    mt.wm(global_file, new_global_temp)
 
 x = i7.dir2proj()
 
