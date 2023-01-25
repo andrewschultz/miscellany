2c483fafbc264fa1d9287fe438130dcc7a533a17
Andrew <blurglecruncheon@gmail.com>
2023-01-24 23:57:00 -0600

    24/03/10 00:51:13 get main source with T in ni.py

--- ni.py
+++ ni.py
@@ -3,7 +3,11 @@
 # they will be renamed niold.bat and ghold.bat
 
 import sys
+import os
 import mytools as mt
+import i7
+
+to_project = i7.dir2proj()
 
 def usage(header="Generic usage writeup"):
     mt.okay(header)
@@ -21,8 +25,16 @@ if len(sys.argv) == 1:
 
 while cmd_count < len(sys.argv):
     arg = sys.argv[cmd_count]
-    if arg == '?':
+    if arg == 't':
+        get_main_source = True
+    elif arg == '?':
         usage("USAGE")
     else:
         usage("Bad parameter {}.".format(arg))
     cmd_count += 1
+
+if not to_project:
+    mt.bailfail("Could not find project to act on.")
+
+if get_main_source:
+    os.system(i7.main_src(to_project))
