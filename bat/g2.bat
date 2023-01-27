4bc33827c130c13b0c3c50f05dd10e2eeb69b454
Andrew <blurglecruncheon@gmail.com>
2023-01-26 23:57:00 -0600

    24/03/10 01:16:04 ni.py auxiliary batch files n2 and g2 -- to be renamed later

---
+++ bat/g2.bat
@@ -0,0 +1,7 @@
+@echo off
+
+REM g2.bat
+REM longterm replacement for gh.bat
+REM It uses ni.py to potentially create a temporary batch file
+
+call ni.py %*
---
+++ bat/n2.bat
@@ -0,0 +1,7 @@
+@echo off
+
+REM n2.bat
+REM longterm replacement for ni.bat
+REM It uses ni.py to potentially create a temporary batch file
+
+call ni.py %*
