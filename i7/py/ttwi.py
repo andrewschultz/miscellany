3952c1bc968bed8e93db2693d2d83f67985e7116
Andrew <blurglecruncheon@gmail.com>
2023-02-11 23:57:00 -0600

    24/03/11 12:23:36 ttwi.py now allows string input for shuffle-array, with warnings if it doesn't work on a table header

--- i7/py/ttwi.py
+++ i7/py/ttwi.py
@@ -17,7 +17,7 @@ from filecmp import cmp
 write_for_compare = True
 ttwi_temp = "c:/writing/temp/ttwi-after.txt"
 
-switch_array = []
+switch_array_base = []
 table_to_find = ""
 table_regex = ""
 clipboard_string = ""
@@ -38,7 +38,7 @@ out_newline = '\n'
 def print_whats_missing():
     print("Your input was missing something...")
     if not table_to_find: print("Need to give me a table to find--that can be any text.")
-    if not switch_array: print("You need a CSV of numbers.")
+    if not switch_array_base: print("You need a CSV of numbers.")
     sys.exit()
 
 def print_examples():
@@ -62,6 +62,21 @@ def zero_to_n_shuffle(q):
     q2 = list(range(0, len(q)))
     return Counter(q2) == Counter(q)
 
+def ordering_of(my_base, this_header):
+    int_check = this_header[0].isdigit()
+    return_array = []
+    for x in range(1, len(this_header)):
+        if this_header[x].isdigit() != int_check:
+            return [] # here, we make sure everything is an integer
+    for m in my_base:
+        m = re.sub(" *\(.*", "", m)
+        try:
+            return_array.append(this_header.index(m))
+        except:
+            print(m, "not in array", this_header)
+            return []
+    return return_array
+
 if len(sys.argv) == 1:
     print("No command. Printing usage.")
     usage()
@@ -81,6 +96,10 @@ while count < len(sys.argv):
         story_file = True
     elif arg.startswith ('/'):
         table_regex = arg[1:]
+    elif ',' in arg:
+        if switch_array_base:
+            mt.bailfail("Two arrays.")
+        switch_array_base = arg.split(',')
     elif re.search("[a-z]", arg):
         if arg.startswith("table-of-"):
             print("No need to start the table name with table-of-.")
@@ -88,22 +107,17 @@ while count < len(sys.argv):
         else:
             table_to_find = ("table of " + arg).lower()
         table_to_find = re.sub("-", " ", table_to_find)
-    elif re.search("[0-9]", arg):
-        switch_array = [int(x) for x in arg.split(",")]
     elif arg == '?':
         usage()
     elif arg == '??':
         print_examples()
     count += 1
 
-if not switch_array:
+if not switch_array_base:
     print_whats_missing()
 
 if (not table_to_find) == (not table_regex):
     mt.bailfail("You need exactly one of absolute string or a regex.")
-
-if not zero_to_n_shuffle(switch_array):
-    mt.bailfail("You need the switch-array to be a permutation of 0, ..., n.")
 
 if not project_name:
     project_name = i7.dir2proj()
@@ -153,7 +167,14 @@ with open(my_file) as file:
             lma = re.sub("( *\[[^\]]*\])*$", "", ll)
         else:
             #ll = re.sub(" ?\([^\)]*\)", "", ll)
-            cols = len(line.split("\t"))
+            table_headers = [re.sub(" *\(.*", "", x) for x in ll.lower().split("\t")]
+            cols = len(table_headers)
+            switch_array = ordering_of(switch_array_base, table_headers)
+            if not switch_array:
+                mt.warn("Line {} table header couldn't be matched up with switch-array. You have either mixed up integers and strings, or the base array has a string the in-file array doesn't.".format(line_count))
+                in_table = False
+                file_out_string += line
+                continue
             if cols < len(switch_array):
                 mt.warn("Line {} switch array is too big, {:d} vs {:d}.".format(line_count, len(switch_array), cols))
                 in_table = False
@@ -180,12 +201,12 @@ with open(my_file) as file:
                 if re.search(" [a-z]", q.lower()): print("WARNING space in header", q)
         new_ar = []
         for x in switch_array: new_ar.append(lm[x])
-        this_row_string = "\t".join(new_ar)
+        this_row_string = "\t".join(new_ar) + "\n"
         file_out_string += this_row_string
         if to_clipboard:
             clipboard_string += this_row_string + '\n'
-        else:
-            print(line_count, this_row_string)
+        elif not write_for_compare:
+            print(this_row_string.rstrip())
 
 if not got_table:
     mt.bailfail("Could not find {} {:s} in story.ni.".format('absolute text' if table_to_find else 'regex', table_to_find if table_to_find else table_regex))
@@ -195,7 +216,6 @@ if write_for_compare:
     f.write(file_out_string)
     f.close()
     if cmp(my_file, ttwi_temp):
-        mt.wm(my_file, ttwi_temp)
         mt.warn("No changes.")
     else:
         mt.wm(my_file, ttwi_temp)
