15bd6c7facaef4e5fc8cd3afff2bada93b99e256
Andrew <blurglecruncheon@gmail.com>
2023-09-24 23:57:00 -0500

    25/09/22 23:59:16 trivial adds to n2.bat

--- bat/n2.bat
+++ bat/n2.bat
@@ -1,11 +1,23 @@
 @echo off
 
-REM n2.bat
-REM longterm replacement for ni.bat
-REM It uses ni.py to potentially create a temporary batch file
+REM ni.bat
+REM replaces ni-old.bat (much longer)
+REM It is a wrapper around ni.py, which creates a temporary batch file
+
+if "%1" EQU "aa" (
+  cd \games\inform\andac
+  goto end
+)
+
+if "%1" EQU "gt" (
+  c:\users\andrew\documents\github\git-tips.txt
+  goto end
+)
 
 call ni.py %*
 
 if EXIST "c:\writing\temp\ni-temp.bat" (
   call c:\writing\temp\ni-temp.bat
 )
+
+:end
