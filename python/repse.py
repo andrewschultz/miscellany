3ee2400844d2010cce2d72d06be7f437a8b22de8
Andrew <blurglecruncheon@gmail.com>
2023-12-24 23:57:00 -0600

    25/09/24 07:39:40 repse.py replacer script now replaces more than just default

--- python/repse.py
+++ python/repse.py
@@ -37,6 +37,17 @@ while cmd_count < len(sys.argv):
         search_end = False
     elif arg in ( 'b', 'es', 'se' ):
         search_end = search_start = True
+    else:
+        if arg[0] == '.':
+            arg = arg[1:]
+        if not my_in:
+            print("Assuming non_control parameter {} is input--use commas to make sure".format(arg))
+            my_in = arg
+        elif not my_out:
+            print("Assuming non_control parameter {} is output--use commas to make sure".format(arg))
+            my_out = arg
+        else:
+            sys.exit("Too many non-control parameters. Type ? for usage.")
     cmd_count += 1
 
 if not my_in or not my_out:
@@ -55,7 +66,7 @@ with open("c:/writing/dict/brit-1word.txt") as file:
         x = line.lower().strip()
         wbl[len(x)].add(x)
 
-delta = len(default_out) - len(default_in)
+delta = len(my_out) - len(my_in)
 
 the_range = range(3, max(wbl) + 1)
 
@@ -65,12 +76,12 @@ for y in the_range:
         continue
     for w in wbl[y]:
         if search_end:
-            if w.endswith(default_in):
-                w0 = w.replace(default_in, default_out, -1)
+            if w.endswith(my_in):
+                w0 = w.replace(my_in, my_out, -1)
                 if w0 in wbl[z]:
                     print(w, w0)
         if search_start:
-            if w.startswith(default_in):
-                w0 = w.replace(default_in, default_out, 1)
+            if w.startswith(my_in):
+                w0 = w.replace(my_in, my_out, 1)
                 if w0 in wbl[z]:
                     print(w, w0)
