49ff01349fad13dfccd73d4e4d7d49a25808a8b0
Andrew <blurglecruncheon@gmail.com>
2024-01-08 23:57:00 -0600

    25/09/24 21:49:32 tcp.py allow user to assign break-after-x-lines

--- i7/py/tcp.py
+++ i7/py/tcp.py
@@ -10,12 +10,14 @@ import pyperclip
 temp_file = "c:\\writing\\temp\\from_webpage_transcript.txt"
 
 max_width = 100
-breaks_after = 5
+default_breaks_after = 7
+assigned_max_width = 0
+assigned_breaks_after = 0
 
 x = pyperclip.paste()
 ary = x.split("\r\n")
 
-def spacing_stuff(word_array, our_max_width):
+def spacing_stuff(word_array, our_max_width, our_breaks_after):
     this_line = ''
     big_string = ''
     break_counter = 0
@@ -28,7 +30,7 @@ def spacing_stuff(word_array, our_max_width):
             else:
                 big_string += this_line + "\n"
                 break_counter += 1
-                if break_counter == breaks_after:
+                if break_counter == our_breaks_after:
                     break_counter = 0
                     big_string += "\n"
                 this_line = w
@@ -47,13 +49,19 @@ while cmd_count < len(sys.argv):
         max_width = int(arg)
     elif (arg.replace('b', '').isdigit()):
         my_num = int(arg.replace('b', ''))
-        breaks_after = my_num
+        if assigned_breaks_after:
+            mt.warn("Reassigned max_width from {} to {}.".format(assigned_breaks_after, my_num))
+        assigned_breaks_after = my_num
     elif (arg.replace('w', '').isdigit()):
         max_width = int(arg)
         print("TRIVIA: You don't need a w before or after a number to establish width. It is chosen by default.")
     else:
         sys.exit("Need a # for max_width or #b/b# for carriage returns before extra break.")
     cmd_count += 1
+
+if not assigned_breaks_after:
+    mt.warn("Going with default max-width of", default_breaks_after)
+    assigned_breaks_after = default_breaks_after
 
 transcript_next = False
 to_file = True
@@ -74,7 +82,7 @@ for a in ary:
         copy_string_array.extend(a.split(' '))
         continue
 
-copy_string = spacing_stuff(copy_string_array, max_width)
+copy_string = spacing_stuff(copy_string_array, assigned_max_width, assigned_breaks_after)
 
 if to_file:
     f = open(temp_file, "w")
