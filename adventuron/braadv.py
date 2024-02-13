c3a74a6c203496014acf9495b94dc1506b073b3b
Andrew <blurglecruncheon@gmail.com>
2024-02-12 23:57:00 -0600

    26/01/23 22:30:23 braadv.py adventuron bracket checker

---
+++ adventuron/braadv.py
@@ -0,0 +1,33 @@
+import sys
+bracket_count = 0
+last_line = 0
+
+debug = False
+see_all = True
+
+try:
+    if sys.argv[1] == 'd':
+        debug = True
+except:
+    debug = False
+
+from collections import defaultdict
+
+bracket_last_line = defaultdict(int)
+
+with open("source_code.adv") as file:
+    for (line_count, line) in enumerate (file, 1):
+        bracket_count += (line.count('{') - line.count('{('))
+        bracket_count -= (line.count('}') - line.count(')}'))
+        if bracket_count == 0:
+            last_line = line_count
+        if debug:
+            print(line_count, bracket_count, line.rstrip())
+        if 'subroutine' in line or see_all:
+            bracket_last_line[bracket_count] = line_count
+
+if bracket_count:
+    for s in sorted(bracket_last_line):
+        print("Last line with {} bracket{}: {}".format(s, 's' if s > 1 else '', line_count))
+else:
+    print("Brackets all match!")
