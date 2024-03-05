57d8ac6a4af92ad77d72ee0da0796d6988277067
Andrew <blurglecruncheon@gmail.com>
2024-03-04 23:57:00 -0600

    26/04/14 18:25:52 shalf rewrite so cmd line parsed better

--- adventuron/shalf.py
+++ adventuron/shalf.py
@@ -3,25 +3,42 @@ import sys
 if len(sys.argv) < 3:
 	sys.exit("Need 2 words.")
 
-spaces_before = 3
+default_indents = 3
+indents = 3
 
-if len(sys.argv) == 4:
-    spaces_before = int(sys.argv[3])
+cmd_words = []
 
-spaces_before *= 3
+cmd_count = 1
 
-sbstring = ' ' * spaces_before
+while cmd_count < len(sys.argv):
+    arg = sys.argv[cmd_count]
+    if arg.isdigit():
+        if indents:
+            sys.exit("Re-redefined default 3-spaces before.")
+        indents = arg
+    else:
+        cmd_words.append(arg)
+    cmd_count += 1
 
-my_point = "point_{}_{}".format(sys.argv[1], sys.argv[2])
+if len(cmd_words) != 2:
+    sys.exit("Need 2 command words. You have {}, {}".format(len(cmd_words), ' '.join(cmd_words)))
+
+if not indents:
+    print("Going with default indents", default_indents)
+    indents = default_indents
+
+sbstring = ' ' * (3 * indents)
+
+my_point = "point_{}_{}".format(cmd_words[0], cmd_words[1])
 
 # no need for spaces
 print("   {} : boolean \"false\" ;".format(my_point))
 print()
 
-print("   puz_{} : dynamic_string {{( opt_color_coding ? \"<{} {}<#f80>>\" : \"{} {}\" )}} ;".format(my_point, sys.argv[1], sys.argv[2], sys.argv[1], sys.argv[2]))
+print("   puz_{} : dynamic_string {{( opt_color_coding ? \"<{} {}<#f80>>\" : \"{} {}\" )}} ;".format(my_point, cmd_words[0], cmd_words[1], cmd_words[0], cmd_words[1]))
 print()
 
-print(sbstring + ": match \"{} {}\" {{".format(sys.argv[1], sys.argv[2]))
+print(sbstring + ": match \"{} {}\" {{".format(cmd_words[0], cmd_words[1]))
 print(sbstring + "   : if ({}) {{".format(my_point))
 print(sbstring + "      : print \"Already done!\" ;")
 print(sbstring + "      : done ;")
@@ -31,12 +48,12 @@ print(sbstring + "   : set_true \"{}\" ;".format(my_point))
 print(sbstring + "   : gosub \"add_point\" ;")
 print(sbstring + "   : done ;")
 print(sbstring + "}")
-#print(sbstring + ": if (footnotes_available && !bonus_point_{}_{}) {{".format(sys.argv[1], sys.argv[2]))
-print(sbstring + ": if (!{}) {{".format(my_point, sys.argv[1], sys.argv[2]))
-print(sbstring + "   : match \"{} _;_ {}\" {{".format(sys.argv[1], sys.argv[2]))
+#print(sbstring + ": if (footnotes_available && !bonus_point_{}_{}) {{".format(cmd_words[0], cmd_words[1]))
+print(sbstring + ": if (!{}) {{".format(my_point, cmd_words[0], cmd_words[1]))
+print(sbstring + "   : match \"{} _;_ {}\" {{".format(cmd_words[0], cmd_words[1]))
 print(sbstring + "      : set_integer var=\"tempint\" \"x\" ;")
 print(sbstring + "      : set_false \"first_half\" ;")
-print(sbstring + "      : match \"{} _\" {{".format(sys.argv[1]))
+print(sbstring + "      : match \"{} _\" {{".format(cmd_words[0]))
 print(sbstring + "         : set_true \"first_half\" ;")
 print(sbstring + "      }")
 print(sbstring + "      : gosub \"say_half\" ;")
